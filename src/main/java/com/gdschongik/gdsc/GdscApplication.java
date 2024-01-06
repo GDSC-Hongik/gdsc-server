59caaabab5fda3a65dbd0e363233b8cdadba05f3
uwoobeat <desktop3711@gmail.com>
2024-01-06 21:39:10 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/gdschongik/gdsc/GdscApplication.java
@@ -0,0 +1,12 @@
+package com.gdschongik.gdsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gdschongik/gdsc/GdscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdschongik.gdsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
