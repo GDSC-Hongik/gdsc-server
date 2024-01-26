b4a0065cce91c90286394bba177f81b973513182
chosw1002@naver.com <chosw1002@naver.com>
2024-01-26 16:39:28 +0900

    feat: MemberRole 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/member/domain/MemberRole.java
@@ -0,0 +1,14 @@
+package com.gdschongik.gdsc.domain.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberRole {
+
+	USER("ROLE_USER"),
+	ADMIN("ROLE_ADMIN");
+
+	private final String value;
+}
