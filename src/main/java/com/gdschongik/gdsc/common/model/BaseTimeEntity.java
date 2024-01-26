4ce1dc76a3f0ca4f5b51fdc73503c73a5966508d
chosw1002@naver.com <chosw1002@naver.com>
2024-01-26 16:38:36 +0900

    feat: BaseTimeEntity 추가

---
+++ src/main/java/com/gdschongik/gdsc/common/model/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.gdschongik.gdsc.common.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+	@Column(updatable = false)
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@Column
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+}
