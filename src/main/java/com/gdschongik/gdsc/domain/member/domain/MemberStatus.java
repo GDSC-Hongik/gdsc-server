e9feb848bd07a1a305287bbbb8c0d9eddabc97bf
chosw1002@naver.com <chosw1002@naver.com>
2024-01-26 16:40:20 +0900

    feat: MemberStatus 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/member/domain/MemberStatus.java
@@ -0,0 +1,15 @@
+package com.gdschongik.gdsc.domain.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberStatus {
+
+	NORMAL("NORMAL"),
+	DELETED("DELETED"),
+	FORBIDDEN("FORBIDDEN");
+
+	private final String value;
+}
