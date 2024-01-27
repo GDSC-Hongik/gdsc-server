3c7cd1d9985c50619cfcbcc1e6bf9ae8b7dc647d
uwoobeat <desktop3711@gmail.com>
2024-01-27 19:15:17 +0900

    feat: 시큐리티 관련 상수 클래스 추가

---
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
@@ -0,0 +1,10 @@
+package com.gdschongik.gdsc.global.common.constant;
+
+public class SecurityConstant {
+
+    public static final String REGISTRATION_REQUIRED_HEADER = "Registration-Required";
+    public static final String ACCESS_TOKEN_COOKIE_NAME = "accessToken";
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+
+    private SecurityConstant() {}
+}
