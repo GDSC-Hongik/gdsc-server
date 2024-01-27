0431cf3f27d85a14a7af6a8c1872a5efe0376195
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-01-27 16:56:45 +0900

    feat: 멤버 도메인 구현 (#13)
    * feat: BaseTimeEntity 추가
    
    * feat: MemberRole 추가
    
    * feat: MemberStatus 추가
    
    * feat: Member 도메인 추가
    
    * feat: MemberRole에 Guest 추가
    
    * feat: Member에 GuestMember 생성 정적 메서드 추가
    
    * feat: security config 추가
    
    * test: 멤버 회원가입 테스트 추가
    
    * feat: JpaConfig 추가
    
    * style: spotless 적용

---
+++ src/test/java/com/gdschongik/gdsc/domain/member/domain/MemberTest.java
@@ -0,0 +1,32 @@
+package com.gdschongik.gdsc.domain.member.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    @Test
+    void 회원가입시_MemberRole은_GUEST이다() {
+        // given
+        Member member = Member.createGuestMember("testOauthId");
+
+        // when
+        MemberRole role = member.getRole();
+
+        // then
+        assertThat(role).isEqualTo(MemberRole.GUEST);
+    }
+
+    @Test
+    void 회원가입시_MemberStatus는_NORMAL이다() {
+        // given
+        Member member = Member.createGuestMember("testOauthId");
+
+        // when
+        MemberStatus status = member.getStatus();
+
+        // then
+        assertThat(status).isEqualTo(MemberStatus.NORMAL);
+    }
+}
