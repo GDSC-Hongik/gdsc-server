adae5bdb60f6712879fb16bd82ee8f5b6dcd6a94
uwoobeat <desktop3711@gmail.com>
2024-01-27 21:07:54 +0900

    style: spotless 적용

--- src/main/java/com/gdschongik/gdsc/domain/auth/dto/AccessTokenDto.java
+++ src/main/java/com/gdschongik/gdsc/domain/auth/dto/AccessTokenDto.java
@@ -2,9 +2,4 @@ package com.gdschongik.gdsc.domain.auth.dto;
 
 import com.gdschongik.gdsc.domain.member.domain.MemberRole;
 
-public record AccessTokenDto(
-	Long memberId,
-	MemberRole memberRole,
-	String tokenValue
-) {
-}
+public record AccessTokenDto(Long memberId, MemberRole memberRole, String tokenValue) {}
