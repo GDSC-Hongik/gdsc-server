13377ab6c121d123a14c03a15e720986af5b5df0
uwoobeat <desktop3711@gmail.com>
2024-01-27 17:01:32 +0900

    refactor: 패키지 구조 변경

--- src/main/java/com/gdschongik/gdsc/global/config/JpaConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.gdschongik.gdsc.common.config;
+package com.gdschongik.gdsc.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
--- src/main/java/com/gdschongik/gdsc/global/config/WebSecurityConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.gdschongik.gdsc.common.config;
+package com.gdschongik.gdsc.global.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
