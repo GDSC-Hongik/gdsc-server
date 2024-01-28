8279899301627e3d34198e6e049af4daf5f6ed23
uwoobeat <desktop3711@gmail.com>
2024-01-28 15:37:47 +0900

    fix: 생성자 사용하도록 수정

--- src/main/java/com/gdschongik/gdsc/global/property/RedisProperty.java
+++ src/main/java/com/gdschongik/gdsc/global/property/RedisProperty.java
@@ -1,11 +1,11 @@
 package com.gdschongik.gdsc.global.property;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
-@Setter
+@AllArgsConstructor
 @ConfigurationProperties(prefix = "spring.data.redis")
 public class RedisProperty {
 
