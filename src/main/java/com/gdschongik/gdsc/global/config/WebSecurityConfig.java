ea34e5e1ea2ac227e296d11c9afe030c6a1635ba
uwoobeat <desktop3711@gmail.com>
2024-01-28 15:15:02 +0900

    chore: 시큐리티 설정 추가

--- src/main/java/com/gdschongik/gdsc/global/config/WebSecurityConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/WebSecurityConfig.java
@@ -1,25 +1,62 @@
 package com.gdschongik.gdsc.global.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import static org.springframework.security.config.Customizer.*;
+
+import com.gdschongik.gdsc.domain.auth.application.JwtService;
+import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
+import com.gdschongik.gdsc.global.security.CustomSuccessHandler;
+import com.gdschongik.gdsc.global.security.CustomUserService;
+import com.gdschongik.gdsc.global.security.JwtFilter;
+import com.gdschongik.gdsc.global.util.CookieUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final MemberRepository memberRepository;
+    private final JwtService jwtService;
+    private final CookieUtil cookieUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+        httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .authorizeHttpRequests(authorize ->
-                        authorize.requestMatchers(PathRequest.toH2Console()).permitAll())
-                .build();
+                .cors(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        httpSecurity.oauth2Login(
+                oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userService(customUserService(memberRepository)))
+                        .successHandler(customSuccessHandler(jwtService, cookieUtil)));
+
+        httpSecurity.addFilterBefore(jwtFilter(jwtService, cookieUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CustomUserService customUserService(MemberRepository memberRepository) {
+        return new CustomUserService(memberRepository);
+    }
+
+    @Bean
+    public CustomSuccessHandler customSuccessHandler(JwtService jwtService, CookieUtil cookieUtil) {
+        return new CustomSuccessHandler(jwtService, cookieUtil);
+    }
+
+    @Bean
+    public JwtFilter jwtFilter(JwtService jwtService, CookieUtil cookieUtil) {
+        return new JwtFilter(jwtService, cookieUtil);
     }
 }
