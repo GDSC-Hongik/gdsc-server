b24421b03fb3aa51ec218df091584a1516060913
chosw1002@naver.com <chosw1002@naver.com>
2024-01-30 11:10:08 +0900

    feat: ErrorCode 추가

---
+++ src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.gdschongik.gdsc.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
