fdfcce28cb0580a2e575261dfc7568665500be3c
chosw1002@naver.com <chosw1002@naver.com>
2024-02-01 01:30:14 +0900

    feat: 어드민 멤버 조회 기능 구현

--- src/main/java/com/gdschongik/gdsc/domain/member/api/AdminMemberController.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/api/AdminMemberController.java
@@ -1,11 +1,30 @@
 package com.gdschongik.gdsc.domain.member.api;
+
 import com.gdschongik.gdsc.domain.member.application.MemberService;
+import com.gdschongik.gdsc.domain.member.dto.response.AdminMemberFindAllResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/admin/members")
 @RequiredArgsConstructor
 public class AdminMemberController {
+
     private final MemberService memberService;
+
+    @GetMapping
+    public ResponseEntity<Page<AdminMemberFindAllResponse>> getMembers(
+            @RequestParam(value = "keyword", required = false) String keyword,
+            @RequestParam(value = "type", required = false) String type,
+            Pageable pageable) {
+
+        Page<AdminMemberFindAllResponse> response = memberService.findAll(keyword, type, pageable);
+        return ResponseEntity.ok().body(response);
+    }
 }
