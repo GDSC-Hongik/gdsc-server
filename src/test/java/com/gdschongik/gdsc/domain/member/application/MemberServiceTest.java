7f4cbb84febdf376dbc4bd90f1912f28b9558815
chosw1002@naver.com <chosw1002@naver.com>
2024-02-01 14:04:29 +0900

    test: 로직 변경에 따른 테스트 수정

--- src/test/java/com/gdschongik/gdsc/domain/member/application/MemberServiceTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/member/application/MemberServiceTest.java
@@ -1,21 +1,18 @@
-package com.gdschongik.gdsc.domain.member.dao;
+package com.gdschongik.gdsc.domain.member.application;
 
 import static org.assertj.core.api.Assertions.*;
 
-import com.gdschongik.gdsc.config.TestQuerydslConfig;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
 
-@DataJpaTest
-@Import(TestQuerydslConfig.class)
-class MemberRepositoryTest {
+@SpringBootTest
+class MemberServiceTest {
 
     @Autowired
-    private MemberRepository memberRepository;
+    private MemberService memberService;
 
     @Test
     void 유효하지_않은_type이면_예외_발생() {
@@ -23,7 +20,7 @@ class MemberRepositoryTest {
         Pageable pageable = Pageable.ofSize(20);
 
         // when, then
-        assertThatThrownBy(() -> memberRepository.findAll("keyword", "invalid type", pageable))
+        assertThatThrownBy(() -> memberService.findAll("keyword", "invalid type", pageable))
                 .isInstanceOf(CustomException.class);
     }
 }
