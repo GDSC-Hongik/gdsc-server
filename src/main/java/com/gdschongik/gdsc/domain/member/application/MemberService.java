d331b79513c42a10a822a68a38ff6d52d1af0e7b
chosw1002@naver.com <chosw1002@naver.com>
2024-02-04 22:29:23 +0900

    refactor: refresh 토큰 삭제 로직 추가

--- src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
@@ -1,9 +1,13 @@
 package com.gdschongik.gdsc.domain.member.application;
 
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
+import com.gdschongik.gdsc.domain.auth.dao.RefreshTokenRepository;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.dto.request.MemberQueryRequest;
 import com.gdschongik.gdsc.domain.member.dto.response.MemberFindAllResponse;
+import com.gdschongik.gdsc.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public Page<MemberFindAllResponse> findAll(MemberQueryRequest queryRequest, Pageable pageable) {
         Page<Member> members = memberRepository.findAll(queryRequest, pageable);
@@ -24,6 +29,8 @@ public class MemberService {
 
     @Transactional
     public void withdrawMember(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
         memberRepository.deleteById(memberId);
+        refreshTokenRepository.deleteById(member.getId());
     }
 }
