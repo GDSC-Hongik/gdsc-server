4cc0683a11d0203d4378992020c1e6378843d398
chosw1002@naver.com <chosw1002@naver.com>
2024-02-08 16:46:51 +0900

    style: 개행 제거

--- src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
@@ -29,7 +29,6 @@ public class MemberService {
     public void updateMember(Long memberId, MemberUpdateRequest request) {
         Member member =
                 memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-
         member.updateMemberInfo(request);
     }
 }
