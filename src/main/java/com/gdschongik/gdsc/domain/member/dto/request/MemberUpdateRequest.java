c15036b27556784335586f49f1998ff9d509efbb
chosw1002@naver.com <chosw1002@naver.com>
2024-02-08 15:51:42 +0900

    feat: 어드민 멤버 수정 request dto 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/request/MemberUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.gdschongik.gdsc.domain.member.dto.request;
+
+import static com.gdschongik.gdsc.global.common.constant.RegexConstant.*;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record MemberUpdateRequest(
+        @NotNull @Pattern(regexp = STUDENT_ID) String studentId,
+        @NotNull String name,
+        @NotNull @Pattern(regexp = PHONE) String phone,
+        @NotNull String department,
+        @NotNull @Email String email,
+        @NotNull String discordUsername,
+        @NotNull @Pattern(regexp = DISCORD_NICKNAME) String discordNickname) {}
