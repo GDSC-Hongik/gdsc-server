9f6e25188b0fe871bd96f7e0650b1daf9c02682e
chosw1002@naver.com <chosw1002@naver.com>
2024-02-08 16:01:53 +0900

    fix: 닉네임 최소 1자 이상이도록 수정

--- src/main/java/com/gdschongik/gdsc/global/common/constant/RegexConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/RegexConstant.java
@@ -3,7 +3,7 @@ package com.gdschongik.gdsc.global.common.constant;
 public class RegexConstant {
     public static final String STUDENT_ID = "^[A-C]{1}[0-9]{6}$";
     public static final String PHONE = "^010-[0-9]{4}-[0-9]{4}$";
-    public static final String DISCORD_NICKNAME = "[ㄱ-ㅣ가-힣]{0,6}$";
+    public static final String DISCORD_NICKNAME = "[ㄱ-ㅣ가-힣]{1,6}$";
 
     private RegexConstant() {}
 }
