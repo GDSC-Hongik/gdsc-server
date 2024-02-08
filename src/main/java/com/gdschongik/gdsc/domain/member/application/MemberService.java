e2551c631c8df89c8cc7e0df6ccf1cc4bad2cab8
chosw1002@naver.com <chosw1002@naver.com>
2024-02-08 15:52:18 +0900

    feat: 어드민 멤버 수정 서비스 추가

--- src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
@@ -3,7 +3,10 @@ package com.gdschongik.gdsc.domain.member.application;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.dto.request.MemberQueryRequest;
+import com.gdschongik.gdsc.domain.member.dto.request.MemberUpdateRequest;
 import com.gdschongik.gdsc.domain.member.dto.response.MemberFindAllResponse;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,5 +23,13 @@ public class MemberService {
     public Page<MemberFindAllResponse> findAll(MemberQueryRequest queryRequest, Pageable pageable) {
         Page<Member> members = memberRepository.findAll(queryRequest, pageable);
         return members.map(MemberFindAllResponse::of);
+    }
+
+    @Transactional
+    public void updateMember(Long memberId, MemberUpdateRequest request) {
+        Member member =
+                memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        member.updateMemberInfo(request);
     }
 }
