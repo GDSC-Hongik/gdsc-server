a223438fb6c7dbabef3616783ad4d2438209006c
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-02-09 20:15:38 +0900

    feat: 멤버 탈퇴 api 구현 (#38)
    * feat: 멤버 탈퇴 api 구현
    
    * feat: 멤버 탈퇴 service 구현
    
    * feat: 멤버 관련 에러 코드 추가
    
    * refactor: refresh 토큰 삭제 로직 추가
    
    * feat: 쿠키 제거 로직 추가
    
    * remove: 쿠키 제거 로직 제거
    
    * feat: 멤버 softDelete 메서드 추가
    
    * refactor: 어드민 멤버 삭제 softDelete로 변경
    
    * fix: 이미 상태가 delete인 경우 예외 처리
    
    * refactor: soft deleted 회원 로그인 시도 시 예외 처리
    
    * refactor: 상태 확인 로직 MemberStatus로 이동
    
    * refactor: 조건 수정
    
    * fix: 재가입 가능하도록 수정
    
    * remove: 사용하지 않는 메서드 제거
    
    * fix: 에러 코드 수정
    
    * fix: 메서드명 수정
    
    * remove: 사용하지 않는 의존성 제거
    
    * fix: 상태코드 수정

--- src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/MemberService.java
@@ -1,9 +1,12 @@
 package com.gdschongik.gdsc.domain.member.application;
 
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.dto.request.MemberQueryRequest;
 import com.gdschongik.gdsc.domain.member.dto.response.MemberFindAllResponse;
+import com.gdschongik.gdsc.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,5 +23,11 @@ public class MemberService {
     public Page<MemberFindAllResponse> findAll(MemberQueryRequest queryRequest, Pageable pageable) {
         Page<Member> members = memberRepository.findAll(queryRequest, pageable);
         return members.map(MemberFindAllResponse::of);
+    }
+
+    @Transactional
+    public void withdrawMember(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+        member.withdraw();
     }
 }
