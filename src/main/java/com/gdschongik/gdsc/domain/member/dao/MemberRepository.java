bd60c5be1f5d94291288ec81586856ce6bbbaac4
chosw1002@naver.com <chosw1002@naver.com>
2024-02-09 15:57:08 +0900

    remove: 사용하지 않는 메서드 제거

--- src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberRepository.java
@@ -1,10 +1,6 @@
 package com.gdschongik.gdsc.domain.member.dao;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
-
-    Optional<Member> findByOauthId(String oauthId);
-}
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {}
