5126fa769f6f4aead3ba6399b8f60e885f5e9600
chosw1002@naver.com <chosw1002@naver.com>
2024-02-09 15:56:22 +0900

    fix: 재가입 가능하도록 수정

--- src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberCustomRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberCustomRepository.java
@@ -2,9 +2,12 @@ package com.gdschongik.gdsc.domain.member.dao;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.dto.request.MemberQueryRequest;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MemberCustomRepository {
     Page<Member> findAll(MemberQueryRequest queryRequest, Pageable pageable);
+
+    Optional<Member> findNormalByOauthId(String oauthId);
 }
--- src/main/java/com/gdschongik/gdsc/global/security/CustomUserService.java
+++ src/main/java/com/gdschongik/gdsc/global/security/CustomUserService.java
@@ -2,8 +2,6 @@ package com.gdschongik.gdsc.global.security;
 
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
-import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -25,12 +23,7 @@ public class CustomUserService extends DefaultOAuth2UserService {
     }
 
     private Member fetchOrCreate(OAuth2User oAuth2User) {
-        Member member =
-                memberRepository.findByOauthId(oAuth2User.getName()).orElseGet(() -> registerMember(oAuth2User));
-        if (member.isDeleted()) {
-            throw new CustomException(ErrorCode.MEMBER_SOFT_DELETED);
-        }
-        return member;
+        return memberRepository.findNormalByOauthId(oAuth2User.getName()).orElseGet(() -> registerMember(oAuth2User));
     }
 
     private Member registerMember(OAuth2User oAuth2User) {
