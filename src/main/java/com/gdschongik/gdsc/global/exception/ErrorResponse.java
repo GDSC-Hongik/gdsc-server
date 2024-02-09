13753079e4b8912fb50b8d8f11f1a1d68415bfd7
chosw1002@naver.com <chosw1002@naver.com>
2024-02-09 21:46:26 +0900

    feat: dto 검증 예외 처리 메서드 생성

--- src/main/java/com/gdschongik/gdsc/global/exception/ErrorResponse.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/ErrorResponse.java
@@ -4,4 +4,8 @@ public record ErrorResponse(String errorCodeName, String errorMessage) {
     public static ErrorResponse of(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.name(), errorCode.getMessage());
     }
+
+    public static ErrorResponse of(String errorCodeName, String errorMessage) {
+        return new ErrorResponse(errorCodeName, errorMessage);
+    }
 }
--- src/main/java/com/gdschongik/gdsc/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.gdschongik.gdsc.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -21,5 +25,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("INTERNAL_SERVER_ERROR : {}", e.getMessage(), e);
         return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
                 .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.error("METHOD_ARGUMENT_NOT_VALID : {}", e.getMessage(), e);
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.status(status.value())
+                .body(ErrorResponse.of(ErrorCode.METHOD_ARGUMENT_NOT_VALID.name(), errorMessage));
     }
 }
