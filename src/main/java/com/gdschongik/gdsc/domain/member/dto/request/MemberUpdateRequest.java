6377e5382909167f94051875c4faa12bab5ecf50
chosw1002@naver.com <chosw1002@naver.com>
2024-02-09 21:44:13 +0900

    refactor: NotBlank로 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/request/MemberUpdateRequest.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/request/MemberUpdateRequest.java
@@ -3,14 +3,14 @@ package com.gdschongik.gdsc.domain.member.dto.request;
 import static com.gdschongik.gdsc.global.common.constant.RegexConstant.*;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record MemberUpdateRequest(
-        @NotNull @Pattern(regexp = STUDENT_ID) String studentId,
-        @NotNull String name,
-        @NotNull @Pattern(regexp = PHONE) String phone,
-        @NotNull String department,
-        @NotNull @Email String email,
-        @NotNull String discordUsername,
-        @NotNull @Pattern(regexp = NICKNAME) String nickname) {}
+        @NotBlank @Pattern(regexp = STUDENT_ID, message = "학번은 " + STUDENT_ID + " 형식이어야 합니다.") String studentId,
+        @NotBlank String name,
+        @NotBlank @Pattern(regexp = PHONE, message = "전화번호는 " + PHONE + " 형식이어야 합니다.") String phone,
+        @NotBlank String department,
+        @NotBlank @Email String email,
+        @NotBlank String discordUsername,
+        @NotBlank @Pattern(regexp = NICKNAME, message = "닉네임은 " + NICKNAME + " 형식이어야 합니다.") String nickname) {}
