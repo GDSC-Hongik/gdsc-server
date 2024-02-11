73610945eb22640b26fdc1e36b5a6756c4fae0fc
binary_ho <dfghcvb11@naver.com>
2024-02-12 04:37:25 +0900

    feat : Java Mail Sender Config 세팅 (#28)

---
+++ src/main/java/com/gdschongik/gdsc/domain/integration/JavaMailSenderConfig.java
@@ -0,0 +1,41 @@
+package com.gdschongik.gdsc.domain.integration;
+
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailSenderConfig {
+
+    @Value("${gmail.id}")
+    private String id;
+
+    @Value("${gmail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(456);
+        javaMailSender.setUsername(id);
+        javaMailSender.setPassword(password);
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        javaMailSender.setDefaultEncoding("UTF-8");
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.socketFactory.port", 456);
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.starttls.required", true);
+        properties.put("mail.smtp.socketFactory.fallback", false);
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        return properties;
+    }
+}
