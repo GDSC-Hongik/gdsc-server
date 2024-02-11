9ee3af9498e5482de332ec7dae2e9c21c132c798
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-02-11 18:43:33 +0900

    feat: 현재 로그인한 멤버 조회하는 유틸리티 구현 (#41)
    * feat: 멤버 유틸리티 구현
    
    * refactor: 예외 케이스 분리 및 불필요한 메서드 제거

--- src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
@@ -9,16 +9,19 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다."),
 
-    // Jwt
+    // Auth
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
+    AUTH_NOT_EXIST(HttpStatus.INTERNAL_SERVER_ERROR, "시큐리티 인증 정보가 존재하지 않습니다."),
+    AUTH_NOT_PARSABLE(HttpStatus.INTERNAL_SERVER_ERROR, "시큐리티 인증 정보 파싱에 실패했습니다."),
 
     // Parameter
     INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 쿼리 파라미터입니다."),
 
     // Member
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    MEMBER_DELETED(HttpStatus.CONFLICT, "탈퇴한 회원입니다.");
+    MEMBER_DELETED(HttpStatus.CONFLICT, "탈퇴한 회원입니다."),
+    ;
 
     private final HttpStatus status;
     private final String message;
