3d88172667ccee4a44e807a50e3f180af292a3d4
binary_ho <dfghcvb11@naver.com>
2024-02-11 18:03:12 +0900

    feat : 인증 메일 내용을 작성하는 VerificationMailContentWriter 구현 (#28)

---
+++ src/main/java/com/gdschongik/gdsc/domain/integration/VerificationMailContentWriter.java
@@ -0,0 +1,18 @@
+package com.gdschongik.gdsc.domain.integration;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VerificationMailContentWriter {
+
+    private static final String NOTIFICATION_MESSAGE = "<div style='margin:20px;'>"
+        + "<h1> 안녕하세요 GDSC Hongik 이메일 인증 메일입니다. </h1> <br>"
+        + "<h3> 아래의 인증 코드를 %d분 안에 입력해주세요. </h3> <br>"
+        + "<h3> 감사힙니다. </h3> <br>"
+        + "CODE : <strong>";
+
+    public String writeContentWithVerificationCode(String verificationCode, int codeAliveTime) {
+        return String.format(NOTIFICATION_MESSAGE, codeAliveTime)
+            + verificationCode;
+    }
+}
