87169fd99942c393adbec2afe8335cdaeb27263d
chosw1002@naver.com <chosw1002@naver.com>
2024-02-12 01:38:30 +0900

    remove: 테스트 제거

--- src/test/java/com/gdschongik/gdsc/domain/member/application/MemberServiceTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/member/application/MemberServiceTest.java
@@ -4,11 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
-import com.gdschongik.gdsc.domain.member.dto.request.MemberGrantRequest;
 import com.gdschongik.gdsc.domain.member.dto.request.MemberUpdateRequest;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.global.exception.ErrorCode;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,18 +32,5 @@ class MemberServiceTest {
         assertThatThrownBy(() -> memberService.updateMember(member.getId(), requestBody))
                 .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
-    }
-
-    @Test
-    void requirement가_충족되지_않으면_승인_실패() {
-        // given
-        Member unverifiedMember = Member.createGuestMember("oAuthId");
-        memberRepository.save(unverifiedMember);
-        MemberGrantRequest request = new MemberGrantRequest(List.of(unverifiedMember.getId()));
-
-        // when & then
-        assertThatThrownBy(() -> memberService.grantMember(request))
-                .isInstanceOf(CustomException.class)
-                .hasMessage(ErrorCode.MEMBER_NOT_VERIFIED.getMessage());
     }
 }
