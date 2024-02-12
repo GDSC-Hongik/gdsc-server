1c53b563c96406005a53e981c4423d9b7ecbe0dd
uwoobeat <desktop3711@gmail.com>
2024-02-13 01:39:30 +0900

    chore: 디스코드가 활성화된 상태에서만 빈을 초기화하도록 설정

--- src/main/java/com/gdschongik/gdsc/global/config/DiscordConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/DiscordConfig.java
@@ -1,5 +1,6 @@
 package com.gdschongik.gdsc.global.config;
 
+import com.gdschongik.gdsc.global.discord.ListenerBeanPostProcessor;
 import com.gdschongik.gdsc.global.property.DiscordProperty;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
@@ -7,6 +8,8 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +20,7 @@ public class DiscordConfig {
     private final DiscordProperty discordProperty;
 
     @Bean
+    @ConditionalOnProperty(value = "discord.enabled", havingValue = "true", matchIfMissing = true)
     public JDA jda() {
         return JDABuilder.createDefault(discordProperty.getToken())
                 .setActivity(Activity.playing("테스트"))
