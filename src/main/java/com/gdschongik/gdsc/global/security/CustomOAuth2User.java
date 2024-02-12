a13d3f42463dcdd9b50d5b272e3df60cbcbf4017
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-02-12 19:33:40 +0900

    feat: 소셜 로그인 시 랜딩할 페이지 결정에 사용할 헤더 추가 (#64)
    * feat: 랜딩 상태 추가
    
    * feat: oauth2 인증주체에 랜딩상태 필드 추가
    
    * refactor: 미사용 메서드 제거
    
    * feat: 소셜 로그인 응답 헤더에 랜딩상태 추가

--- src/main/java/com/gdschongik/gdsc/global/security/CustomOAuth2User.java
+++ src/main/java/com/gdschongik/gdsc/global/security/CustomOAuth2User.java
@@ -13,14 +13,12 @@ public class CustomOAuth2User extends DefaultOAuth2User {
 
     private final Long memberId;
     private final MemberRole memberRole;
+    private final LandingStatus landingStatus;
 
     public CustomOAuth2User(OAuth2User oAuth2User, Member member) {
         super(oAuth2User.getAuthorities(), oAuth2User.getAttributes(), GITHUB_NAME_ATTR_KEY);
         this.memberId = member.getId();
         this.memberRole = member.getRole();
-    }
-
-    public boolean isGuest() {
-        return memberRole == MemberRole.GUEST;
+        this.landingStatus = LandingStatus.of(member);
     }
 }
