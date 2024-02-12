7fc808a98aef70856a9359234def7cfc7b871cfe
uwoobeat <desktop3711@gmail.com>
2024-02-13 00:53:06 +0900

    feat: 디스코드 리스너를 컴포넌트로 등록하도록 설정

---
+++ src/main/java/com/gdschongik/gdsc/global/discord/Listener.java
@@ -0,0 +1,12 @@
+package com.gdschongik.gdsc.global.discord;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.stereotype.Component;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Listener {}
---
+++ src/main/java/com/gdschongik/gdsc/global/discord/ListenerBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package com.gdschongik.gdsc.global.discord;
+
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.JDA;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ListenerBeanPostProcessor implements BeanPostProcessor {
+
+    private final JDA jda;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        if (bean.getClass().isAnnotationPresent(Listener.class)) {
+            jda.addEventListener(bean);
+        }
+        return bean;
+    }
+}
