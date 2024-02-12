94b67dc0a459efda6c85c4986b708801886d38eb
binary_ho <dfghcvb11@naver.com>
2024-02-13 07:23:29 +0900

    chore : 재학생 인증 메일 문구 수정

--- src/main/java/com/gdschongik/gdsc/domain/integration/VerificationMailContentWriter.java
+++ src/main/java/com/gdschongik/gdsc/domain/integration/VerificationMailContentWriter.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Component;
 public class VerificationMailContentWriter {
 
     private static final String NOTIFICATION_MESSAGE = "<div style='margin:20px;'>"
-        + "<h1> 안녕하세요 GDSC Hongik 이메일 인증 메일입니다. </h1> <br>"
+        + "<h1> 안녕하세요 GDSC Hongik 재학생 인증 메일입니다. </h1> <br>"
         + "<h3> 아래의 인증 코드를 %d분 안에 입력해주세요. </h3> <br>"
-        + "<h3> 감사힙니다. </h3> <br>"
+        + "<h3> 감사합니다. </h3> <br>"
         + "CODE : <strong>";
 
     public String writeContentWithVerificationCode(String verificationCode, Duration codeAliveTime) {
