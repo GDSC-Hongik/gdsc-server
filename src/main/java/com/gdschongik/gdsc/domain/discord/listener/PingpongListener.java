09a2837d18cc06b5298112b528cec3aca46cd6eb
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-02-13 02:02:38 +0900

    chore: 디스코드 연동 세팅 (#69)
    * chore: 디스코드 구성 추가
    
    * chore: discord yml 설정 추가
    
    * chore: JDA 설정 변경
    
    * feat: 디스코드 리스너를 컴포넌트로 등록하도록 설정
    
    * feat: 테스트 핑퐁 리스너 추가
    
    * fix: 테스트 시 jda 로드하지 않도록 수정
    
    * fix: JDA 의존성을 설정 클래스에서 직접 주입하도록 수정
    
    * test: 테스트 프로파일 활성화
    
    * chore: 디스코드가 활성화된 상태에서만 빈을 초기화하도록 설정
    
    * fix: JDA가 초기화된 경우에만 빈 후처리기에 주입하도록 수정
    
    * chore: 디스코드 yml을 항상 로드하도록 수정
    
    * test: 테스트 시에는 jda 비활성화

---
+++ src/main/java/com/gdschongik/gdsc/domain/discord/listener/PingpongListener.java
@@ -0,0 +1,30 @@
+package com.gdschongik.gdsc.domain.discord.listener;
+
+import com.gdschongik.gdsc.global.discord.Listener;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+@Slf4j
+@Listener
+public class PingpongListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        User author = event.getAuthor();
+        TextChannel channel = event.getChannel().asTextChannel();
+        Message message = event.getMessage();
+        String content = message.getContentRaw(); // get only textual content of message
+
+        log.info("Message from {} in {}: {}", author.getName(), channel.getName(), message.getContentDisplay());
+
+        if (author.isBot()) return;
+
+        if (content.equals("!ping")) {
+            channel.sendMessage("Pong!").queue();
+        }
+    }
+}
--- src/main/java/com/gdschongik/gdsc/global/config/PropertyConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/PropertyConfig.java
@@ -1,11 +1,12 @@
 package com.gdschongik.gdsc.global.config;
 
+import com.gdschongik.gdsc.global.property.DiscordProperty;
 import com.gdschongik.gdsc.global.property.JwtProperty;
 import com.gdschongik.gdsc.global.property.RedisProperty;
 import com.gdschongik.gdsc.global.property.SwaggerProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@EnableConfigurationProperties({JwtProperty.class, RedisProperty.class, SwaggerProperty.class})
+@EnableConfigurationProperties({JwtProperty.class, RedisProperty.class, SwaggerProperty.class, DiscordProperty.class})
 @Configuration
 public class PropertyConfig {}
