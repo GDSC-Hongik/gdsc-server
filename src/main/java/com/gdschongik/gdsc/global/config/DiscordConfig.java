67de44f145ddc86bdd598e7e88a54e7f290a7dc8
uwoobeat <desktop3711@gmail.com>
2024-02-13 00:52:35 +0900

    chore: JDA 설정 변경

--- src/main/java/com/gdschongik/gdsc/global/config/DiscordConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/DiscordConfig.java
@@ -4,6 +4,9 @@ import com.gdschongik.gdsc.global.property.DiscordProperty;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +17,11 @@ public class DiscordConfig {
     private final DiscordProperty discordProperty;
 
     @Bean
-    public JDA discord() {
-        return JDABuilder.createDefault(discordProperty.getToken()).build();
+    public JDA jda() {
+        return JDABuilder.createDefault(discordProperty.getToken())
+                .setActivity(Activity.playing("테스트"))
+                .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .build();
     }
 }
