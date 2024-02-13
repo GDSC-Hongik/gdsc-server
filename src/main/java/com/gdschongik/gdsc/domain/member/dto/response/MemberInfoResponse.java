edfd75df731988af22ca306af17a43b543213c43
chosw1002@naver.com <chosw1002@naver.com>
2024-02-13 12:28:34 +0900

    style: spotless apply

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberInfoResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberInfoResponse.java
@@ -3,31 +3,31 @@ package com.gdschongik.gdsc.domain.member.dto.response;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.domain.MemberRole;
 import com.gdschongik.gdsc.domain.member.domain.RequirementStatus;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record MemberInfoResponse(Long memberId,
-								 String studentId,
-								 String name,
-								 String phone,
-								 String department,
-								 String email,
-								 String discordUsername,
-								 String nickname,
-								 @Schema(description = "회비 입금 상태") RequirementStatus paymentStatus,
-								 @Schema(description = "가입 상태") MemberRole role) {
+public record MemberInfoResponse(
+        Long memberId,
+        String studentId,
+        String name,
+        String phone,
+        String department,
+        String email,
+        String discordUsername,
+        String nickname,
+        @Schema(description = "회비 입금 상태") RequirementStatus paymentStatus,
+        @Schema(description = "가입 상태") MemberRole role) {
 
-	public static MemberInfoResponse of(Member member) {
-		return new MemberInfoResponse(
-				member.getId(),
-				member.getStudentId(),
-				member.getName(),
-				member.getPhone(),
-				member.getDepartment(),
-				member.getEmail(),
-				member.getDiscordUsername(),
-				member.getNickname(),
-				member.getRequirement().getPaymentStatus(),
-				member.getRole());
-	}
+    public static MemberInfoResponse of(Member member) {
+        return new MemberInfoResponse(
+                member.getId(),
+                member.getStudentId(),
+                member.getName(),
+                member.getPhone(),
+                member.getDepartment(),
+                member.getEmail(),
+                member.getDiscordUsername(),
+                member.getNickname(),
+                member.getRequirement().getPaymentStatus(),
+                member.getRole());
+    }
 }
