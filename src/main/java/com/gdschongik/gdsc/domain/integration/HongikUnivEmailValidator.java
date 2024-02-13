4acedc7f974050e551b04b9e7a886b2a61e56d32
binary_ho <dfghcvb11@naver.com>
2024-02-13 15:25:13 +0900

    refactor : 홍익대학교 이메일 Regex를 RegexConstant로 이동

--- src/main/java/com/gdschongik/gdsc/domain/integration/HongikUnivEmailValidator.java
+++ src/main/java/com/gdschongik/gdsc/domain/integration/HongikUnivEmailValidator.java
@@ -1,5 +1,7 @@
 package com.gdschongik.gdsc.domain.integration;
 
+import static com.gdschongik.gdsc.global.common.constant.RegexConstant.HONGIK_EMAIL_REGEX;
+
 import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -9,17 +11,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class HongikUnivEmailValidator {
 
-
     private static final String HONGIK_UNIV_MAIL_DOMAIN = "@g.hongik.ac.kr";
-
-    private static final String EMAIL_REGEX = "^[^\\W&=_'-+,<>]+(\\.[^\\W&=_'-+,<>]+)*@g\\.hongik\\.ac\\.kr$";
 
     public void validate(String email) {
         if (!email.contains(HONGIK_UNIV_MAIL_DOMAIN)) {
             throw new CustomException(ErrorCode.UNIV_EMAIL_DOMAIN_MISMATCH);
         }
 
-        if (!email.matches(EMAIL_REGEX)) {
+        if (!email.matches(HONGIK_EMAIL_REGEX)) {
             throw new CustomException(ErrorCode.UNIV_EMAIL_FORMAT_MISMATCH);
         }
     }
