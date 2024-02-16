6796d1bb13c1a2b055c9562e81b2950f5c709b88
binary_ho <dfghcvb11@naver.com>
2024-02-16 16:53:40 +0900

    refactor: UnivEmailVerificationRepository 불필요한 메서드 제거

--- src/main/java/com/gdschongik/gdsc/domain/email/dao/UnivEmailVerificationRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/email/dao/UnivEmailVerificationRepository.java
@@ -1,10 +1,6 @@
 package com.gdschongik.gdsc.domain.email.dao;
 
 import com.gdschongik.gdsc.domain.email.domain.UnivEmailVerification;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UnivEmailVerificationRepository extends CrudRepository<UnivEmailVerification, String> {
-
-    Optional<UnivEmailVerification> findByUnivEmail(String univEmail);
-}
+public interface UnivEmailVerificationRepository extends CrudRepository<UnivEmailVerification, String> {}
