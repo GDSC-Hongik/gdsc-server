877a1671ad1daa98fd1dcf30ef2a3a19f1f28d43
Jaehyun Ahn <desktop3711@gmail.com>
2024-02-16 22:36:12 +0900

    feat: 디스코드 인증코드 발급 로직 구현

--- src/main/java/com/gdschongik/gdsc/domain/discord/listener/RegisterCommandListener.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/listener/RegisterCommandListener.java
@@ -1,17 +1,14 @@
 package com.gdschongik.gdsc.domain.discord.listener;
 
+import static com.gdschongik.gdsc.global.common.constant.DiscordConstant.*;
+
 import com.gdschongik.gdsc.domain.discord.application.OnboardingDiscordService;
+import com.gdschongik.gdsc.domain.discord.dto.response.DiscordVerificationCodeResponse;
 import com.gdschongik.gdsc.global.discord.Listener;
-import jakarta.annotation.Nonnull;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.text.TextInput;
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
-import net.dv8tion.jda.api.interactions.modals.Modal;
+import org.jetbrains.annotations.NotNull;
 
 @Listener
 @RequiredArgsConstructor
@@ -20,27 +17,23 @@ public class RegisterCommandListener extends ListenerAdapter {
     private final OnboardingDiscordService onboardingDiscordService;
 
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        if (event.getName().equals("register")) {
-            TextInput input = TextInput.create("code", "인증코드를 입력해주세요.", TextInputStyle.SHORT)
-                    .setPlaceholder("발급받은 4자리 인증코드")
-                    .setMinLength(4)
-                    .setMaxLength(4)
-                    .build();
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if (event.getName().equals(COMMAND_NAME_ISSUING_CODE)) {
+            event.deferReply()
+                    .setEphemeral(true)
+                    .setContent(DEFER_MESSAGE_ISSUING_CODE)
+                    .queue();
 
-            Modal modal = Modal.create("registerModal", "디스코드 연동 인증코드 입력")
-                    .addComponents(ActionRow.of(input))
-                    .build();
+            String discordUsername = event.getUser().getName();
+            DiscordVerificationCodeResponse verificationCode =
+                    onboardingDiscordService.createVerificationCode(discordUsername);
 
-            event.replyModal(modal).queue();
-        }
-    }
+            String message = String.format(
+                    REPLY_MESSAGE_ISSUING_CODE,
+                    verificationCode.code(),
+                    verificationCode.ttl().toMinutes());
 
-    @Override
-    public void onModalInteraction(@Nonnull ModalInteractionEvent event) {
-        if (event.getModalId().equals("registerModal")) {
-            String code = Objects.requireNonNull(event.getValue("code")).getAsString();
-            onboardingDiscordService.verifyDiscordCode(event.getUser().getName(), code);
+            event.getHook().sendMessage(message).setEphemeral(true).queue();
         }
     }
 }
