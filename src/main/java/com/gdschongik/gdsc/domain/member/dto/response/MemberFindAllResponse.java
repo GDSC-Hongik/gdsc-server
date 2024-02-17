0583500ec896c308e55a56ecbe8c241f4c59639c
chosw1002@naver.com <chosw1002@naver.com>
2024-02-17 18:36:01 +0900

    refactor: 전화번호를 dto 단에서 포맷팅

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberFindAllResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberFindAllResponse.java
@@ -17,7 +17,11 @@ public record MemberFindAllResponse(
                 member.getId(),
                 member.getStudentId(),
                 member.getName(),
-                member.getPhone().getNumber(),
+                String.format(
+                        "%s-%s-%s",
+                        member.getPhone().substring(0, 3),
+                        member.getPhone().substring(3, 7),
+                        member.getPhone().substring(7)),
                 member.getDepartment(),
                 member.getEmail(),
                 member.getDiscordUsername(),
