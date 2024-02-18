caa0ee3bd620ca415400452ea9ca464f6700ff36
chosw1002@naver.com <chosw1002@naver.com>
2024-02-18 23:40:55 +0900

    refactor: 대기중인 멤버 조회 메서드를 querydsl로 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberRepository.java
@@ -3,7 +3,4 @@ package com.gdschongik.gdsc.domain.member.dao;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
-
-    Page<Member> findAllByRole(MemberRole role, Pageable pageable);
-}
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {}
