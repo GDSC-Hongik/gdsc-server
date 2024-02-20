d4d901c679e01b657de3f3c4b9c004786bc8b590
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-02-20 17:34:01 +0900

    feat: 회비 미납 회원 목록 조회 api 구현 (#89)
    * feat: 회비 미납 회원 목록 조회 api 구현
    
    * refactor: endpoint 수정
    
    * refactor: RequirementStatus 확인 메서드 통합
    
    * refactor: eqRequirementStatus 메서드 확장성 개선

--- src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberCustomRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberCustomRepository.java
@@ -2,6 +2,7 @@ package com.gdschongik.gdsc.domain.member.dao;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.domain.MemberRole;
+import com.gdschongik.gdsc.domain.member.domain.RequirementStatus;
 import com.gdschongik.gdsc.domain.member.dto.request.MemberQueryRequest;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
@@ -17,4 +18,6 @@ public interface MemberCustomRepository {
     Page<Member> findAllGrantable(Pageable pageable);
 
     Page<Member> findAllByRole(MemberRole role, Pageable pageable);
+
+    Page<Member> findAllByPaymentStatus(RequirementStatus paymentStatus, Pageable pageable);
 }
