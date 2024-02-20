faed00c3560d10e0c9dffb4e2ae7416aab43d179
Jaehyun Ahn <desktop3711@gmail.com>
2024-02-20 23:26:12 +0900

    feat: 레포지터리 메서드 추가

--- src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dao/MemberRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
     boolean existsByDiscordUsername(String discordUsername);
+
+    boolean existsByNickname(String nickname);
 }
