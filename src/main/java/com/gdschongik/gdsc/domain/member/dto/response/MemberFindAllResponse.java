0ca2eb6685791fbdbb6f2cf9c61ff088989bb427
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-02-22 00:29:07 +0900

    fix: dto의 phone에서 발생 가능한 NPE 제거 (#104)

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberFindAllResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberFindAllResponse.java
@@ -13,15 +13,19 @@ public record MemberFindAllResponse(
         String nickname) {
 
     public static MemberFindAllResponse of(Member member) {
+        String phone = null;
+        if (member.getPhone() != null) {
+            phone = String.format(
+                    "%s-%s-%s",
+                    member.getPhone().substring(0, 3),
+                    member.getPhone().substring(3, 7),
+                    member.getPhone().substring(7));
+        }
         return new MemberFindAllResponse(
                 member.getId(),
                 member.getStudentId(),
                 member.getName(),
-                String.format(
-                        "%s-%s-%s",
-                        member.getPhone().substring(0, 3),
-                        member.getPhone().substring(3, 7),
-                        member.getPhone().substring(7)),
+                phone,
                 member.getDepartment(),
                 member.getEmail(),
                 member.getDiscordUsername(),
