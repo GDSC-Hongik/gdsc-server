3c61fede2f25b5ddc001ac85e267f366fac32c8f
uwoobeat <desktop3711@gmail.com>
2024-02-23 01:09:09 +0900

    feat: 디스코드 이벤트 핸들러 인터페이스 구현

---
+++ src/main/java/com/gdschongik/gdsc/domain/discord/handler/DiscordEventHandler.java
@@ -0,0 +1,9 @@
+package com.gdschongik.gdsc.domain.discord.handler;
+
+import net.dv8tion.jda.api.events.GenericEvent;
+
+public interface DiscordEventHandler {
+
+    // TODO: GenericEvent에 대한 어댑터 추가
+    void delegate(GenericEvent genericEvent);
+}
