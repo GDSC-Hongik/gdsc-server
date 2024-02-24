1c4a6888e92d563dddba776887b3bc552a83baff
uwoobeat <desktop3711@gmail.com>
2024-02-25 03:04:08 +0900

    feat: 학과 목록 조회 API 구현

---
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/CommonMemberService.java
@@ -0,0 +1,21 @@
+package com.gdschongik.gdsc.domain.member.application;
+
+import com.gdschongik.gdsc.domain.member.domain.Department;
+import com.gdschongik.gdsc.domain.member.dto.response.MemberDepartmentResponse;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CommonMemberService {
+
+    public List<MemberDepartmentResponse> getDepartments() {
+        return Arrays.stream(Department.values())
+                .map(MemberDepartmentResponse::from)
+                .toList();
+    }
+}
