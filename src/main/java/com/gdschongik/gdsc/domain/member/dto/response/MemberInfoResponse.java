a7a57b6bb147b3592a1c4f342a3cf84a8535effd
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-02-25 01:35:18 +0900

    refactor: 회원 정보 조회 API에 가입 상태 추가 (#121)
    * refactor: 회원 정보 조회 api에 가입 상태 추가
    
    * refactor: enum이 값을 직접 설정하도록 수정

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberInfoResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberInfoResponse.java
@@ -17,7 +17,8 @@ public record MemberInfoResponse(
         @Schema(description = "회비 입금 상태") RequirementStatus paymentStatus,
         @Schema(description = "디스코드 연동 상태") RequirementStatus discordStatus,
         @Schema(description = "가입 상태") MemberRole role,
-        @Schema(description = "입금자명") String depositorName) {
+        @Schema(description = "입금자명") String depositorName,
+        @Schema(description = "가입 상태") RegistrationStatus registrationStatus) {
 
     public static MemberInfoResponse of(Member member) {
         return new MemberInfoResponse(
@@ -36,6 +37,23 @@ public record MemberInfoResponse(
                 member.getRequirement().getPaymentStatus(),
                 member.getRequirement().getDiscordStatus(),
                 member.getRole(),
-                String.format("%s%s", member.getName(), member.getPhone().substring(7)));
+                String.format("%s%s", member.getName(), member.getPhone().substring(7)),
+                RegistrationStatus.getRegistrationStatus(member));
+    }
+
+    enum RegistrationStatus {
+        APPLIED,
+        PENDING,
+        GRANTED;
+
+        static RegistrationStatus getRegistrationStatus(Member member) {
+            if (member.isGranted()) {
+                return GRANTED;
+            }
+            if (member.isGrantAvailable()) {
+                return PENDING;
+            }
+            return APPLIED;
+        }
     }
 }
