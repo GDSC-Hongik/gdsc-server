eb0e9188be0e52d76efcb979b9737a1511051b0b
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-02-26 00:43:47 +0900

    fix: DTO의 학과 정보 로직 NPE 제거 (#135)
    * fix: dto의 학과 정보 로직 NPE 제거
    
    * refactor: 옵셔널로 바로 처리하도록 개선
    
    * refactor: 전화번호를 옵셔널로 처리하도록 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberFindAllResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberFindAllResponse.java
@@ -1,6 +1,8 @@
 package com.gdschongik.gdsc.domain.member.dto.response;
 
+import com.gdschongik.gdsc.domain.member.domain.Department;
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import java.util.Optional;
 
 public record MemberFindAllResponse(
         Long memberId,
@@ -13,20 +15,17 @@ public record MemberFindAllResponse(
         String nickname) {
 
     public static MemberFindAllResponse of(Member member) {
-        String phone = null;
-        if (member.getPhone() != null) {
-            phone = String.format(
-                    "%s-%s-%s",
-                    member.getPhone().substring(0, 3),
-                    member.getPhone().substring(3, 7),
-                    member.getPhone().substring(7));
-        }
         return new MemberFindAllResponse(
                 member.getId(),
                 member.getStudentId(),
                 member.getName(),
-                phone,
-                member.getDepartment().getDepartmentName(),
+                Optional.ofNullable(member.getPhone())
+                        .map(phone -> String.format(
+                                "%s-%s-%s", phone.substring(0, 3), phone.substring(3, 7), phone.substring(7)))
+                        .orElse(null),
+                Optional.ofNullable(member.getDepartment())
+                        .map(Department::getDepartmentName)
+                        .orElse(null),
                 member.getEmail(),
                 member.getDiscordUsername(),
                 member.getNickname());
--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberPendingFindAllResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberPendingFindAllResponse.java
@@ -1,7 +1,9 @@
 package com.gdschongik.gdsc.domain.member.dto.response;
 
+import com.gdschongik.gdsc.domain.member.domain.Department;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.domain.Requirement;
+import java.util.Optional;
 
 public record MemberPendingFindAllResponse(
         Long memberId,
@@ -15,20 +17,17 @@ public record MemberPendingFindAllResponse(
         Requirement requirement) {
 
     public static MemberPendingFindAllResponse of(Member member) {
-        String phone = null;
-        if (member.getPhone() != null) {
-            phone = String.format(
-                    "%s-%s-%s",
-                    member.getPhone().substring(0, 3),
-                    member.getPhone().substring(3, 7),
-                    member.getPhone().substring(7));
-        }
         return new MemberPendingFindAllResponse(
                 member.getId(),
                 member.getStudentId(),
                 member.getName(),
-                phone,
-                member.getDepartment().getDepartmentName(),
+                Optional.ofNullable(member.getPhone())
+                        .map(phone -> String.format(
+                                "%s-%s-%s", phone.substring(0, 3), phone.substring(3, 7), phone.substring(7)))
+                        .orElse(null),
+                Optional.ofNullable(member.getDepartment())
+                        .map(Department::getDepartmentName)
+                        .orElse(null),
                 member.getEmail(),
                 member.getDiscordUsername(),
                 member.getNickname(),
