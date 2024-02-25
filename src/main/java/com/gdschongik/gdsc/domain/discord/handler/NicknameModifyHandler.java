7bc4166832a65cc44a02d5c74525ad1d41bba826
uwoobeat <desktop3711@gmail.com>
2024-02-26 03:14:25 +0900

    feat: 닉네임 변경 이벤트 핸들러 구현

---
+++ src/main/java/com/gdschongik/gdsc/domain/discord/handler/NicknameModifyHandler.java
@@ -0,0 +1,31 @@
+package com.gdschongik.gdsc.domain.discord.handler;
+
+import com.gdschongik.gdsc.domain.discord.application.CommonDiscordService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NicknameModifyHandler implements DiscordEventHandler {
+
+    private final CommonDiscordService commonDiscordService;
+
+    @Override
+    public void delegate(GenericEvent genericEvent) {
+        GuildMemberUpdateNicknameEvent event = (GuildMemberUpdateNicknameEvent) genericEvent;
+
+        Member member = event.getMember();
+        Guild guild = event.getGuild();
+        String discordUsername = member.getUser().getName();
+
+        String originalNickname = commonDiscordService.getNicknameByDiscordUsername(discordUsername);
+
+        guild.modifyNickname(member, originalNickname).queue();
+    }
+}
