dfb6e51cd9017aada693b3a8d5123cb45dcdb7c1
Jaehyun Ahn <desktop3711@gmail.com>
2024-02-28 23:11:48 +0900

    feat: 시큐리티 상수 추가

--- src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
@@ -7,6 +7,8 @@ public class SecurityConstant {
     public static final String GITHUB_NAME_ATTR_KEY = "id";
     public static final String ACCESS_TOKEN_HEADER_PREFIX = "Bearer ";
     public static final String ACCESS_TOKEN_HEADER_NAME = "Authorization";
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URI = "redirect-uri";
 
     private SecurityConstant() {}
 }
