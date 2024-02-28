1ec768b93c55d0dc6af7af8c26f98079fd79c5be
chosw1002@naver.com <chosw1002@naver.com>
2024-02-28 23:41:09 +0900

    chore: 클라이언트 어드민 url에 대한 cors 설정

--- src/main/java/com/gdschongik/gdsc/global/util/email/VerificationLinkUtil.java
+++ src/main/java/com/gdschongik/gdsc/global/util/email/VerificationLinkUtil.java
@@ -4,9 +4,9 @@ import static com.gdschongik.gdsc.global.common.constant.EmailConstant.VERIFY_EM
 import static com.gdschongik.gdsc.global.common.constant.EmailConstant.VERIFY_EMAIL_REQUEST_PARAMETER_KEY;
 import static com.gdschongik.gdsc.global.common.constant.EnvironmentConstant.Constants.DEV_ENV;
 import static com.gdschongik.gdsc.global.common.constant.EnvironmentConstant.Constants.PROD_ENV;
-import static com.gdschongik.gdsc.global.common.constant.UrlConstant.DEV_CLIENT_URL;
+import static com.gdschongik.gdsc.global.common.constant.UrlConstant.DEV_CLIENT_ONBOARDING_URL;
 import static com.gdschongik.gdsc.global.common.constant.UrlConstant.LOCAL_VITE_CLIENT_SECURE_URL;
-import static com.gdschongik.gdsc.global.common.constant.UrlConstant.PROD_CLIENT_URL;
+import static com.gdschongik.gdsc.global.common.constant.UrlConstant.PROD_CLIENT_ONBOARDING_URL;
 
 import com.gdschongik.gdsc.global.util.EnvironmentUtil;
 import lombok.RequiredArgsConstructor;
@@ -25,8 +25,8 @@ public class VerificationLinkUtil {
 
     private String getClientUrl() {
         return switch (environmentUtil.getCurrentProfile()) {
-            case PROD_ENV -> PROD_CLIENT_URL;
-            case DEV_ENV -> DEV_CLIENT_URL;
+            case PROD_ENV -> PROD_CLIENT_ONBOARDING_URL;
+            case DEV_ENV -> DEV_CLIENT_ONBOARDING_URL;
             default -> LOCAL_VITE_CLIENT_SECURE_URL;
         };
     }
