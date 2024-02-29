4e7b835f88dd1230f4c184d97d5174fb44617cb0
Jaehyun Ahn <desktop3711@gmail.com>
2024-02-29 16:38:14 +0900

    feat: 시큐리티 상수 업데이트

--- src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
@@ -3,12 +3,13 @@ package com.gdschongik.gdsc.global.common.constant;
 public class SecurityConstant {
 
     public static final String LANDING_STATUS_PARAM = "landing-status";
+    public static final String ACCESS_TOKEN_PARAM = "access";
+    public static final String REFRESH_TOKEN_PARAM = "refresh";
     public static final String TOKEN_ROLE_NAME = "role";
     public static final String GITHUB_NAME_ATTR_KEY = "id";
     public static final String ACCESS_TOKEN_HEADER_PREFIX = "Bearer ";
     public static final String ACCESS_TOKEN_HEADER_NAME = "Authorization";
-    public static final String OAUTH_REDIRECT_URI_COOKIE_NAME = "oauth-redirect-uri";
-    public static final String REDIRECT_URI_PARAM = "redirect-uri";
+    public static final String OAUTH_BASE_URI_COOKIE_NAME = "oauth-base-uri";
 
     private SecurityConstant() {}
 }
