f40451da2daea55c403c314f155af959c8ba7db5
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-03-02 00:03:31 +0900

    feat: 2차 모집기간 이전 새로 가입한 멤버는 대기 페이지로 랜딩하도록 변경 (#217)
    feat: 대기 페이지 랜딩 로직 구현

--- src/main/java/com/gdschongik/gdsc/global/security/LandingStatus.java
+++ src/main/java/com/gdschongik/gdsc/global/security/LandingStatus.java
@@ -1,14 +1,24 @@
 package com.gdschongik.gdsc.global.security;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public enum LandingStatus {
+    ONBOARDING_NOT_OPENED, // 대기 페이지로 랜딩
     TO_STUDENT_AUTHENTICATION, // 재학생 인증 페이지로 랜딩
     TO_REGISTRATION, // 가입신청 페이지로 랜딩
     TO_DASHBOARD, // 대시보드로 랜딩
     ;
 
     public static LandingStatus of(Member member) {
+        // 1차 모집기간 종료 ~ 2차 모집기간 시작 사이 가입했고, 현재는 2차 모집기간이 아닐 때 대기 페이지로 랜딩
+        if (member.getCreatedAt().isAfter(Constants.FIRST_RECRUITMENT_END_DATE.atStartOfDay())
+                && member.getCreatedAt().isBefore(Constants.SECOND_RECRUITMENT_START_DATE.atStartOfDay())
+                && LocalDateTime.now().isBefore(Constants.SECOND_RECRUITMENT_START_DATE.atStartOfDay())) {
+            return ONBOARDING_NOT_OPENED;
+        }
+
         // 아직 재학생 인증을 하지 않았다면 재학생 인증 페이지로 랜딩
         if (!member.getRequirement().isUnivVerified()) {
             return TO_STUDENT_AUTHENTICATION;
@@ -22,5 +32,10 @@ public enum LandingStatus {
 
         // 재학생 인증과 가입신청을 모두 완료했다면 대시보드로 랜딩
         return TO_DASHBOARD;
+    }
+
+    private static class Constants {
+        private static final LocalDate FIRST_RECRUITMENT_END_DATE = LocalDate.of(2024, 3, 2);
+        private static final LocalDate SECOND_RECRUITMENT_START_DATE = LocalDate.of(2024, 3, 4);
     }
 }
