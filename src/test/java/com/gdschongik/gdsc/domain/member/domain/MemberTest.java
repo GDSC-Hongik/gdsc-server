405d49dc3cc93fe294d90197779b461891ef92bd
Hanbee Lee <bee1162@naver.com>
2024-04-12 18:32:23 +0900

    feat: Member도메인 테스트-grant()

--- src/test/java/com/gdschongik/gdsc/domain/member/domain/MemberTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/member/domain/MemberTest.java
@@ -1,11 +1,16 @@
 package com.gdschongik.gdsc.domain.member.domain;
 
+import static com.gdschongik.gdsc.domain.member.domain.Department.D022;
 import static org.assertj.core.api.Assertions.*;
 
+import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.exception.ErrorCode;
+import com.gdschongik.gdsc.integration.IntegrationTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MemberTest {
-
     @Test
     void 회원가입시_MemberRole은_GUEST이다() {
         // given
@@ -28,5 +33,75 @@ class MemberTest {
 
         // then
         assertThat(status).isEqualTo(MemberStatus.NORMAL);
+    }
+
+    @Test
+    void grant할때_회비를_납부하지_않았으면_에러_반환() {
+        //given
+        Member guestMember = Member.createGuestMember("testOauthId");
+
+        //when
+        guestMember.verifyBevy();
+        guestMember.verifyDiscord("testDiscord","testNickname");
+        guestMember.completeUnivEmailVerification("test@g.hongik.ac.kr");
+        guestMember.signup("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr");
+
+        //when & then
+        assertThatThrownBy(() -> {
+            guestMember.grant();
+        }).isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.PAYMENT_NOT_VERIFIED.getMessage());
+    }
+
+    @Test
+    void grant할때_디스코드_인증하지_않았으면_에러_반환() {
+        //given
+        Member guestMember = Member.createGuestMember("testOauthId");
+
+        //when
+        guestMember.verifyBevy();
+        guestMember.updatePaymentStatus(RequirementStatus.VERIFIED);
+        guestMember.completeUnivEmailVerification("test@g.hongik.ac.kr");
+        guestMember.signup("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr");
+
+        //when & then
+        assertThatThrownBy(() -> {
+            guestMember.grant();
+        }).isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.DISCORD_NOT_VERIFIED.getMessage());
+    }
+
+    @Test
+    void grant할때_Bevy_인증하지_않았으면_에러_반환() {
+        //given
+        Member guestMember = Member.createGuestMember("testOauthId");
+
+        //when
+        guestMember.updatePaymentStatus(RequirementStatus.VERIFIED);
+        guestMember.verifyDiscord("testDiscord","testNickname");
+        guestMember.completeUnivEmailVerification("test@g.hongik.ac.kr");
+
+        //when & then
+        assertThatThrownBy(() -> {
+            guestMember.grant();
+        }).isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.BEVY_NOT_VERIFIED.getMessage());
+    }
+
+
+    @Test
+    void grant할때_회비_납부_디스코드인증_Bevy인증_재학생인증하면_성공() {
+        //given
+        Member guestMember = Member.createGuestMember("testOauthId");
+
+
+        //when
+        guestMember.verifyBevy();
+        guestMember.updatePaymentStatus(RequirementStatus.VERIFIED);
+        guestMember.verifyDiscord("testDiscord","testNickname");
+        guestMember.completeUnivEmailVerification("test@g.hongik.ac.kr");
+
+        //then
+        assertThatNoException();
     }
 }
