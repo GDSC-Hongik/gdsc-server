c9028fdf8c19189ed3b9bcc0d32b3e7ad9d9a6be
Hanbee Lee <bee1162@naver.com>
2024-04-15 13:36:13 +0900

    feat: Member도메인 테스트-spotlessApply해서 다시 commit #269

--- src/test/java/com/gdschongik/gdsc/domain/member/domain/MemberTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/member/domain/MemberTest.java
@@ -39,7 +39,7 @@ class MemberTest {
 
         // when
         member.completeUnivEmailVerification("test@g.hongik.ac.kr");
-        member.signup("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr");
+        member.signup("C123456", "김홍익", "01012345678", D022, "test@g.hongik.ac.kr");
 
         // then
         assertThatNoException();
@@ -52,10 +52,10 @@ class MemberTest {
 
         // when & then
         assertThatThrownBy(() -> {
-            member.signup("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr");
-        }).isInstanceOf(CustomException.class)
+                    member.signup("C123456", "김홍익", "01012345678", D022, "test@g.hongik.ac.kr");
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.UNIV_NOT_VERIFIED.getMessage());
-
     }
 
     @Test
@@ -65,31 +65,34 @@ class MemberTest {
 
         // when
         member.verifyBevy();
-        member.verifyDiscord("testDiscord","testNickname");
+        member.verifyDiscord("testDiscord", "testNickname");
         member.completeUnivEmailVerification("test@g.hongik.ac.kr");
-        member.signup("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr");
+        member.signup("C123456", "김홍익", "01012345678", D022, "test@g.hongik.ac.kr");
 
         // when & then
         assertThatThrownBy(() -> {
-            member.grant();
-        }).isInstanceOf(CustomException.class)
+                    member.grant();
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.PAYMENT_NOT_VERIFIED.getMessage());
     }
+
     @Test
     void grant할때_디스코드_인증하지_않았으면_에러_반환() {
-        //given
+        // given
         Member member = Member.createGuestMember("testOauthId");
 
         // when
         member.verifyBevy();
         member.updatePaymentStatus(RequirementStatus.VERIFIED);
         member.completeUnivEmailVerification("test@g.hongik.ac.kr");
-        member.signup("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr");
+        member.signup("C123456", "김홍익", "01012345678", D022, "test@g.hongik.ac.kr");
 
         // when & then
         assertThatThrownBy(() -> {
-            member.grant();
-        }).isInstanceOf(CustomException.class)
+                    member.grant();
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.DISCORD_NOT_VERIFIED.getMessage());
     }
 
@@ -100,13 +103,14 @@ class MemberTest {
 
         // when
         member.updatePaymentStatus(RequirementStatus.VERIFIED);
-        member.verifyDiscord("testDiscord","testNickname");
+        member.verifyDiscord("testDiscord", "testNickname");
         member.completeUnivEmailVerification("test@g.hongik.ac.kr");
 
         // when & then
         assertThatThrownBy(() -> {
-            member.grant();
-        }).isInstanceOf(CustomException.class)
+                    member.grant();
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.BEVY_NOT_VERIFIED.getMessage());
     }
 
@@ -115,11 +119,10 @@ class MemberTest {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
-
         // when
         member.verifyBevy();
         member.updatePaymentStatus(RequirementStatus.VERIFIED);
-        member.verifyDiscord("testDiscord","testNickname");
+        member.verifyDiscord("testDiscord", "testNickname");
         member.completeUnivEmailVerification("test@g.hongik.ac.kr");
         member.grant();
 
@@ -132,23 +135,23 @@ class MemberTest {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
-
         // when
         member.verifyBevy();
         member.updatePaymentStatus(RequirementStatus.VERIFIED);
-        member.verifyDiscord("testDiscord","testNickname");
+        member.verifyDiscord("testDiscord", "testNickname");
         member.completeUnivEmailVerification("test@g.hongik.ac.kr");
         member.grant();
 
         // when & then
         assertThatThrownBy(() -> {
-            member.grant();
-        }).isInstanceOf(CustomException.class)
+                    member.grant();
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_ALREADY_GRANTED.getMessage());
     }
 
     @Test
-    void 회원탈퇴시_이미_삭제된_유저면_실패(){
+    void 회원탈퇴시_이미_삭제된_유저면_실패() {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
@@ -157,13 +160,14 @@ class MemberTest {
 
         // when & then
         assertThatThrownBy(() -> {
-            member.grant();
-        }).isInstanceOf(CustomException.class)
+                    member.grant();
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
     }
 
     @Test
-    void 회원탈퇴시_이전에_탈퇴하지_않은_유저면_성공(){
+    void 회원탈퇴시_이전에_탈퇴하지_않은_유저면_성공() {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
@@ -180,14 +184,14 @@ class MemberTest {
         Member member = Member.createGuestMember("testOauthId");
 
         // when
-        member.updateMemberInfo("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr","dis","cord");
+        member.updateMemberInfo("C123456", "김홍익", "01012345678", D022, "test@g.hongik.ac.kr", "dis", "cord");
 
         // then
         assertThatNoException();
     }
 
     @Test
-    void 회원수정시_탈퇴한_유저면_실패(){
+    void 회원수정시_탈퇴한_유저면_실패() {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
@@ -196,29 +200,15 @@ class MemberTest {
 
         // when & then
         assertThatThrownBy(() -> {
-            member.updateMemberInfo("C123456","김홍익","01012345678",D022,"test@g.hongik.ac.kr","dis","cord");
-        }).isInstanceOf(CustomException.class)
-                .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
-
-    }
-
-    @Test
-    void 디스코드인증시_탈퇴한_유저면_실패(){
-        // given
-        Member member = Member.createGuestMember("testOauthId");
-
-        // when
-        member.withdraw();
-
-        // when & then
-        assertThatThrownBy(() -> {
-            member.verifyDiscord("testDiscord","testNickname");
-        }).isInstanceOf(CustomException.class)
+                    member.updateMemberInfo(
+                            "C123456", "김홍익", "01012345678", D022, "test@g.hongik.ac.kr", "dis", "cord");
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
     }
 
     @Test
-    void 회비납부시_탈퇴한_유저면_실패(){
+    void 디스코드인증시_탈퇴한_유저면_실패() {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
@@ -227,14 +217,14 @@ class MemberTest {
 
         // when & then
         assertThatThrownBy(() -> {
-            member.updatePaymentStatus(RequirementStatus.VERIFIED);
-        }).isInstanceOf(CustomException.class)
+                    member.verifyDiscord("testDiscord", "testNickname");
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
-
     }
 
     @Test
-    void Bevy인증시_탈퇴한_유저면_실패(){
+    void 회비납부시_탈퇴한_유저면_실패() {
         // given
         Member member = Member.createGuestMember("testOauthId");
 
@@ -243,10 +233,25 @@ class MemberTest {
 
         // when & then
         assertThatThrownBy(() -> {
-            member.verifyBevy();
-        }).isInstanceOf(CustomException.class)
+                    member.updatePaymentStatus(RequirementStatus.VERIFIED);
+                })
+                .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
-
     }
 
+    @Test
+    void Bevy인증시_탈퇴한_유저면_실패() {
+        // given
+        Member member = Member.createGuestMember("testOauthId");
+
+        // when
+        member.withdraw();
+
+        // when & then
+        assertThatThrownBy(() -> {
+                    member.verifyBevy();
+                })
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.MEMBER_DELETED.getMessage());
+    }
 }
