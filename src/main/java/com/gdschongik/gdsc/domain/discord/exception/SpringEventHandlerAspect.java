a34e17f0ca59ad53e31d1247eb38124f54655f7a
Jaehyun Ahn <desktop3711@gmail.com>
2024-04-24 21:57:46 +0900

    feat: 스프링 이벤트 예외 처리기에 로그 기록하도록 추가

--- src/main/java/com/gdschongik/gdsc/domain/discord/exception/SpringEventHandlerAspect.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/exception/SpringEventHandlerAspect.java
@@ -2,12 +2,14 @@ package com.gdschongik.gdsc.domain.discord.exception;
 
 import com.gdschongik.gdsc.global.util.DiscordUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -15,12 +17,13 @@ public class SpringEventHandlerAspect {
 
     private final DiscordUtil discordUtil;
 
-	@Around(
+    @Around(
             "execution(* com.gdschongik.gdsc.domain.discord.application.handler.SpringEventHandler.delegate(*)) && args(ignoredContext)")
     public Object doAround(ProceedingJoinPoint joinPoint, Object ignoredContext) throws Throwable {
         try {
             return joinPoint.proceed();
         } catch (Exception e) {
+            log.error("[SpringEventHandlerAspect] Exception occurred in SpringEventHandler", e);
             sendErrorMessageToDiscord(e);
             return null;
         }
