fc89d58451f899f6aa00c40cae5c79deb944bda7
Jaehyun Ahn <desktop3711@gmail.com>
2024-04-24 21:57:18 +0900

    fix: 유저네임으로 멤버 찾는 유틸 호출하도록 수정

--- src/main/java/com/gdschongik/gdsc/domain/discord/application/handler/MemberGrantEventHandler.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/application/handler/MemberGrantEventHandler.java
@@ -3,6 +3,8 @@ package com.gdschongik.gdsc.domain.discord.application.handler;
 import static com.gdschongik.gdsc.global.common.constant.DiscordConstant.*;
 
 import com.gdschongik.gdsc.domain.member.domain.MemberGrantEvent;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.exception.ErrorCode;
 import com.gdschongik.gdsc.global.util.DiscordUtil;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Guild;
@@ -20,7 +22,8 @@ public class MemberGrantEventHandler implements SpringEventHandler {
     public void delegate(Object context) {
         MemberGrantEvent event = (MemberGrantEvent) context;
         Guild guild = discordUtil.getCurrentGuild();
-        Member member = guild.getMemberById(event.discordUsername());
+        // TODO: 이름이 아닌 ID로 찾기 위해 전체 멤버의 디스코드 사용자 ID를 저장해야 함
+        Member member = discordUtil.getMemberByUsername(event.discordUsername());
         Role role = discordUtil.findRoleByName(MEMBER_ROLE_NAME);
 
         guild.addRoleToMember(member, role).queue();
