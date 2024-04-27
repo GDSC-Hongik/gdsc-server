4f90397f0a80fc7412b016850e6c45ce9f2e6118
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-04-27 23:35:24 +0900

    test: MemberRole에 대한 멤버 레포지토리 테스트 (#314)
    * test: 역할에 대한 멤버 레포지토리 테스트 추가
    
    * test: 조회 앞에 flush & clear 추가
    
    * rename: 변수명 수정

--- src/test/java/com/gdschongik/gdsc/domain/member/dao/MemberRepositoryTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/member/dao/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.gdschongik.gdsc.domain.member.dao;
 
+import static com.gdschongik.gdsc.domain.member.domain.Department.*;
+import static com.gdschongik.gdsc.domain.member.domain.MemberRole.*;
 import static com.gdschongik.gdsc.domain.member.domain.RequirementStatus.*;
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,6 +27,11 @@ class MemberRepositoryTest extends RepositoryTest {
     private Member getMember() {
         Member member = Member.createGuestMember(TEST_OAUTH_ID);
         return memberRepository.save(member);
+    }
+
+    private void flushAndClearBeforeExecute() {
+        testEntityManager.flush();
+        testEntityManager.clear();
     }
 
     @Nested
@@ -132,6 +139,92 @@ class MemberRepositoryTest extends RepositoryTest {
 
             // then
             assertThat(members).doesNotContain(member);
+        }
+    }
+
+    @Nested
+    class 역할로_조회할때 {
+        private static final String UNIV_EMAIL = "test@g.hongik.ac.kr";
+        private static final String DISCORD_USERNAME = "testDiscord";
+        private static final String NICKNAME = "testNickname";
+        private static final String NAME = "김홍익";
+        private static final String STUDENT_ID = "C123456";
+        private static final String PHONE_NUMBER = "01012345678";
+
+        @Test
+        void 승인전이라면_GUEST로_조회된다() {
+            // given
+            Member member = getMember();
+            member.completeUnivEmailVerification(UNIV_EMAIL);
+            member.signup(STUDENT_ID, NAME, PHONE_NUMBER, D022, UNIV_EMAIL);
+
+            flushAndClearBeforeExecute();
+
+            // when
+            Page<Member> members = memberRepository.findAllByRole(EMPTY_QUERY_OPTION, PageRequest.of(0, 10), GUEST);
+
+            // then
+            Member guest = memberRepository.findById(1L).get();
+            assertThat(members).contains(guest);
+        }
+
+        @Test
+        void 승인전이라면_USER로_조회되지_않는다() {
+            // given
+            Member member = getMember();
+            member.completeUnivEmailVerification(UNIV_EMAIL);
+            member.signup(STUDENT_ID, NAME, PHONE_NUMBER, D022, UNIV_EMAIL);
+
+            flushAndClearBeforeExecute();
+
+            // when
+            Page<Member> members = memberRepository.findAllByRole(EMPTY_QUERY_OPTION, PageRequest.of(0, 10), USER);
+
+            // then
+            Member guest = memberRepository.findById(1L).get();
+            assertThat(members).doesNotContain(guest);
+        }
+
+        @Test
+        void 승인후라면_USER로_조회된다() {
+            // given
+            Member member = getMember();
+            member.completeUnivEmailVerification(UNIV_EMAIL);
+            member.signup(STUDENT_ID, NAME, PHONE_NUMBER, D022, UNIV_EMAIL);
+            member.updatePaymentStatus(VERIFIED);
+            member.verifyDiscord(DISCORD_USERNAME, NICKNAME);
+            member.verifyBevy();
+            member.grant();
+
+            flushAndClearBeforeExecute();
+
+            // when
+            Page<Member> members = memberRepository.findAllByRole(EMPTY_QUERY_OPTION, PageRequest.of(0, 10), USER);
+
+            // then
+            Member user = memberRepository.findById(1L).get();
+            assertThat(members).contains(user);
+        }
+
+        @Test
+        void 승인후라면_GUEST로_조회되지_않는다() {
+            // given
+            Member member = getMember();
+            member.completeUnivEmailVerification(UNIV_EMAIL);
+            member.signup(STUDENT_ID, NAME, PHONE_NUMBER, D022, UNIV_EMAIL);
+            member.updatePaymentStatus(VERIFIED);
+            member.verifyDiscord(DISCORD_USERNAME, NICKNAME);
+            member.verifyBevy();
+            member.grant();
+
+            flushAndClearBeforeExecute();
+
+            // when
+            Page<Member> members = memberRepository.findAllByRole(EMPTY_QUERY_OPTION, PageRequest.of(0, 10), GUEST);
+
+            // then
+            Member user = memberRepository.findById(1L).get();
+            assertThat(members).doesNotContain(user);
         }
     }
 }
