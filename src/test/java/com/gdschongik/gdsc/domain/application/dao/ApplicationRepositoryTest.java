9204d47a658ac11787e408dc14d35580fbaa5800
Hanbee Lee <bee1162@naver.com>
2024-05-02 10:54:24 +0900

    test: 회비 납부 상태에 따른 조회 api test code 작성

---
+++ src/test/java/com/gdschongik/gdsc/domain/application/dao/ApplicationRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.gdschongik.gdsc.domain.application.dao;
+
+import static com.gdschongik.gdsc.global.common.constant.MemberConstant.OAUTH_ID;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.gdschongik.gdsc.domain.application.domain.Application;
+import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
+import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.member.domain.RequirementStatus;
+import com.gdschongik.gdsc.repository.RepositoryTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+class ApplicationRepositoryTest extends RepositoryTest {
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member getMember() {
+        Member member = Member.createGuestMember(OAUTH_ID);
+        return memberRepository.save(member);
+    }
+
+    private Application getApplication(Member member) {
+        Application application = Application.createApplication(member);
+        return applicationRepository.save(application);
+    }
+
+    private void flushAndClearBeforeExecute() {
+        testEntityManager.flush();
+        testEntityManager.clear();
+    }
+
+    @Nested
+    class 가입신청_조회할때 {
+        @Test
+        void 회비납부_안했으면_PENDING으로_조회_성공() {
+            // given
+            Member member = getMember();
+            Application application = getApplication(member);
+
+            // when
+            Page<Application> applications =
+                    applicationRepository.findAllByPaymentStatus(RequirementStatus.PENDING, PageRequest.of(0, 10));
+
+            // then
+            Assertions.assertThat(applications).contains(application);
+        }
+
+        @Test
+        void 회비납부_안헀으면_VERIFIED_조회되지_않는다() {
+            // given
+            Member member = getMember();
+            Application application = getApplication(member);
+
+            // when
+            Page<Application> applications =
+                    applicationRepository.findAllByPaymentStatus(RequirementStatus.VERIFIED, PageRequest.of(0, 10));
+
+            // then
+            Assertions.assertThat(applications).doesNotContain(application);
+        }
+    }
+}
