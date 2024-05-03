19500f99b37c0c2b7c237ef13710647af8c5a77e
chosw1002@naver.com <chosw1002@naver.com>
2024-05-03 21:35:11 +0900

    test: 가입 신청서 생성 서비스 테스트 추가

---
+++ src/test/java/com/gdschongik/gdsc/domain/application/application/ApplicationServiceTest.java
@@ -0,0 +1,43 @@
+package com.gdschongik.gdsc.domain.application.application;
+
+import static com.gdschongik.gdsc.global.common.constant.MemberConstant.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
+import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.member.domain.MemberRole;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.exception.ErrorCode;
+import com.gdschongik.gdsc.integration.IntegrationTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ApplicationServiceTest extends IntegrationTest {
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private void setFixture() {
+        Member member = Member.createGuestMember(OAUTH_ID);
+        memberRepository.save(member);
+    }
+
+    @Nested
+    class 가입신청_생성시 {
+        @Test
+        void 역할이_GUEST라면_가입신청서_생성에_실패한다() {
+            // given
+            setFixture();
+            logoutAndReloginAs(1L, MemberRole.GUEST);
+
+            // when & then
+            assertThatThrownBy(() -> applicationService.apply())
+                    .isInstanceOf(CustomException.class)
+                    .hasMessage(ErrorCode.MEMBER_NOT_GRANTED.getMessage());
+        }
+    }
+}
