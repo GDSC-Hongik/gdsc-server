89721ee8543ee7f88db25af0d0fee32ffeaca43a
chosw1002@naver.com <chosw1002@naver.com>
2024-05-03 21:13:42 +0900

    feat: 가입 신청서 생성 api 구현

---
+++ src/main/java/com/gdschongik/gdsc/domain/application/api/ApplicationController.java
@@ -0,0 +1,26 @@
+package com.gdschongik.gdsc.domain.application.api;
+
+import com.gdschongik.gdsc.domain.application.application.ApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Application Member", description = "가입 신청 API입니다.")
+@RestController
+@RequestMapping("/application")
+@RequiredArgsConstructor
+public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    @Operation(summary = "새학기 가입 신청서 생성", description = "새학기 가입 신청서를 생성합니다.")
+    @PostMapping("/apply")
+    public ResponseEntity<Void> apply() {
+        applicationService.apply();
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/gdschongik/gdsc/domain/application/application/ApplicationService.java
+++ src/main/java/com/gdschongik/gdsc/domain/application/application/ApplicationService.java
@@ -1,6 +1,9 @@
 package com.gdschongik.gdsc.domain.application.application;
 
 import com.gdschongik.gdsc.domain.application.dao.ApplicationRepository;
+import com.gdschongik.gdsc.domain.application.domain.Application;
+import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,5 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ApplicationService {
 
+    private final MemberUtil memberUtil;
     private final ApplicationRepository applicationRepository;
+
+    public void apply() {
+        Member currentMember = memberUtil.getCurrentMember();
+        Application application = Application.createApplication(currentMember);
+        applicationRepository.save(application);
+    }
 }
