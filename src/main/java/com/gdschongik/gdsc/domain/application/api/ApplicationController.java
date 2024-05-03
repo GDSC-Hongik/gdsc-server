78ef472e47d2b200faf98b950778da5c8e3077d9
chosw1002@naver.com <chosw1002@naver.com>
2024-05-03 21:58:37 +0900

    docs: swagger tag name 수정

--- src/main/java/com/gdschongik/gdsc/domain/application/api/ApplicationController.java
+++ src/main/java/com/gdschongik/gdsc/domain/application/api/ApplicationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Application Member", description = "가입 신청 API입니다.")
+@Tag(name = "Application", description = "가입 신청 API입니다.")
 @RestController
 @RequestMapping("/application")
 @RequiredArgsConstructor
