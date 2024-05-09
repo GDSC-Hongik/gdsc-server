cd4339b9522186370daeb0036afe921ee67dbc98
chosw1002@naver.com <chosw1002@naver.com>
2024-05-09 15:34:40 +0900

    feat: 배치 명령어 관련 상수 추가

--- src/main/java/com/gdschongik/gdsc/global/common/constant/DiscordConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/DiscordConstant.java
@@ -20,4 +20,10 @@ public class DiscordConstant {
     public static final String COMMAND_DESCRIPTION_JOIN = "가입 신청이 승인된 멤버에게 역할을 부여합니다.";
     public static final String DEFER_MESSAGE_JOIN = "가입 신청을 처리하는 중입니다...";
     public static final String REPLY_MESSAGE_JOIN = "가입 신청이 승인되었습니다. GDSC Hongik에 합류하신 것을 환영합니다!";
+
+    // 디스코드 ID 저장 커맨드
+    public static final String COMMAND_NAME_BATCH_DISCORD_ID = "디스코드id-배치실행";
+    public static final String COMMAND_DESCRIPTION_BATCH_DISCORD_ID = "디스코드 ID 배치를 진행합니다.";
+    public static final String DEFER_MESSAGE_BATCH_DISCORD_ID = "디스코드 ID 배치를 진행하는 중입니다...";
+    public static final String REPLY_MESSAGE_BATCH_DISCORD_ID = "디스코드 ID 배치가 완료되었습니다.";
 }
