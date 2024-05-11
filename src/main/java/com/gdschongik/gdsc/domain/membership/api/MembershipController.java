73868a9fbd539e939b76168f7b114abba11e08e2
chosw1002@naver.com <chosw1002@naver.com>
2024-05-11 21:05:17 +0900

    docs: swagger 문서 수정

--- src/main/java/com/gdschongik/gdsc/domain/membership/api/MembershipController.java
+++ src/main/java/com/gdschongik/gdsc/domain/membership/api/MembershipController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Membership", description = "멤버십 가입 신청 API입니다.")
+@Tag(name = "Membership", description = "멤버십 API입니다.")
 @RestController
 @RequestMapping("/membership")
 @RequiredArgsConstructor
@@ -17,10 +17,10 @@ public class MembershipController {
 
     private final MembershipService membershipService;
 
-    @Operation(summary = "새학기 가입 신청서 접수", description = "새학기 가입 신청서를 접수합니다.")
-    @PostMapping("/receive")
-    public ResponseEntity<Void> receive() {
-        membershipService.receive();
+    @Operation(summary = "멤버십 가입 신청", description = "멤버십 가입을 신청합니다.")
+    @PostMapping
+    public ResponseEntity<Void> applyMembership() {
+        membershipService.apply();
         return ResponseEntity.ok().build();
     }
 }
