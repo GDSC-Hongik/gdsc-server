511b43fc44bef74fed5a3f7087a8f5067d5c2439
chosw1002@naver.com <chosw1002@naver.com>
2024-05-11 20:58:17 +0900

    test: 테스트를 도메인테스트로 이동

--- src/test/java/com/gdschongik/gdsc/domain/membership/application/MembershipServiceTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/membership/application/MembershipServiceTest.java
@@ -5,12 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
-import com.gdschongik.gdsc.domain.member.domain.MemberRole;
-import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
 import com.gdschongik.gdsc.integration.IntegrationTest;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MembershipServiceTest extends IntegrationTest {
@@ -24,20 +19,5 @@ public class MembershipServiceTest extends IntegrationTest {
     private void setFixture() {
         Member member = Member.createGuestMember(OAUTH_ID);
         memberRepository.save(member);
-    }
-
-    @Nested
-    class 가입신청_접수시 {
-        @Test
-        void 승인되지_않은_멤버라면_가입신청서_접수에_실패한다() {
-            // given
-            setFixture();
-            logoutAndReloginAs(1L, MemberRole.GUEST);
-
-            // when & then
-            assertThatThrownBy(() -> membershipService.receive())
-                    .isInstanceOf(CustomException.class)
-                    .hasMessage(ErrorCode.MEMBER_NOT_GRANTED.getMessage());
-        }
     }
 }
