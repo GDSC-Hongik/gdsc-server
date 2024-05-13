dc5a9d35379b014328f15e1719bf61e242a84914
Hanbee Lee <bee1162@naver.com>
2024-05-13 22:03:06 +0900

    fix: recruitment test에 존재하는 중복 테스트 삭제

--- src/test/java/com/gdschongik/gdsc/domain/recruitment/domain/RecruitmentTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/recruitment/domain/RecruitmentTest.java
@@ -5,7 +5,6 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import static org.assertj.core.api.Assertions.*;
 
 import com.gdschongik.gdsc.domain.recruitment.domain.vo.Period;
-import com.gdschongik.gdsc.global.exception.CustomException;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -25,26 +24,6 @@ class RecruitmentTest {
             assertThat(recruitment.getPeriod().getStartDate()).isEqualTo(START_DATE);
             assertThat(recruitment.getPeriod().getEndDate()).isEqualTo(END_DATE);
             assertThat(recruitment.getPeriod().equals(period)).isTrue();
-        }
-
-        @Test
-        void 잘못된_Period_순서_입력되면_생성_실패한다() {
-            // when & then
-            assertThatThrownBy(() -> {
-                        Recruitment.createRecruitment(RECRUITMENT_NAME, END_DATE, START_DATE);
-                    })
-                    .isInstanceOf(CustomException.class)
-                    .hasMessage(DATE_PRECEDENCE_INVALID.getMessage());
-        }
-
-        @Test
-        void Period_같은날짜가_입력되면_생성_실패한다() {
-            // when & then
-            assertThatThrownBy(() -> {
-                        Recruitment.createRecruitment(RECRUITMENT_NAME, START_DATE, START_DATE);
-                    })
-                    .isInstanceOf(CustomException.class)
-                    .hasMessage(DATE_PRECEDENCE_INVALID.getMessage());
         }
     }
 }
