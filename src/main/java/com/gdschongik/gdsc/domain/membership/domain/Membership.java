c546a549b2e3720ef93afcb7d4085d00b5419154
chosw1002@naver.com <chosw1002@naver.com>
2024-05-14 14:29:55 +0900

    feat: BaseSemesterEntity에 값을 넣는 로직 추가

--- src/main/java/com/gdschongik/gdsc/domain/membership/domain/Membership.java
+++ src/main/java/com/gdschongik/gdsc/domain/membership/domain/Membership.java
@@ -3,6 +3,7 @@ package com.gdschongik.gdsc.domain.membership.domain;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.common.model.BaseSemesterEntity;
+import com.gdschongik.gdsc.domain.common.model.SemesterType;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.domain.MemberRole;
 import com.gdschongik.gdsc.domain.member.domain.RequirementStatus;
@@ -40,16 +41,20 @@ public class Membership extends BaseSemesterEntity {
     private RequirementStatus paymentStatus;
 
     @Builder(access = AccessLevel.PRIVATE)
-    private Membership(Member member, RequirementStatus paymentStatus) {
+    private Membership(
+            Member member, RequirementStatus paymentStatus, Integer academicYear, SemesterType semesterType) {
+        super(academicYear, semesterType);
         this.member = member;
         this.paymentStatus = paymentStatus;
     }
 
-    public static Membership createMembership(Member member) {
+    public static Membership createMembership(Member member, Integer academicYear, SemesterType semesterType) {
         validateMembershipApplicable(member);
         return Membership.builder()
                 .member(member)
                 .paymentStatus(RequirementStatus.PENDING)
+                .academicYear(academicYear)
+                .semesterType(semesterType)
                 .build();
     }
 
