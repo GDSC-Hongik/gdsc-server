3e4deab9dc87eb55c72646023235ee7057486608
chosw1002@naver.com <chosw1002@naver.com>
2024-05-20 12:24:30 +0900

    docs: 대시보드 조회 기능에서 수정해야할 todo 추가

--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepositoryImpl.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepositoryImpl.java
@@ -20,6 +20,7 @@ public class RecruitmentCustomRepositoryImpl implements RecruitmentCustomReposit
                 queryFactory.selectFrom(recruitment).where(isOpen()).fetchOne());
     }
 
+    // todo: 대시보드 기능에서 현재 시간을 클라이언트에게 받도록 수정
     private BooleanBuilder isOpen() {
         LocalDateTime now = LocalDateTime.now();
         return new BooleanBuilder().and(recruitment.period.startDate.loe(now)).and(recruitment.period.endDate.goe(now));
