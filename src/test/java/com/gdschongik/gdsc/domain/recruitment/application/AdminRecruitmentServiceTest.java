157743b42fa50947b7faedcbd39972170bd038bd
chosw1002@naver.com <chosw1002@naver.com>
2024-05-21 21:02:11 +0900

    test: 서비스를 테스트 하도록 수정

--- src/test/java/com/gdschongik/gdsc/domain/recruitment/application/AdminRecruitmentServiceTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/recruitment/application/AdminRecruitmentServiceTest.java
@@ -4,8 +4,7 @@ import static com.gdschongik.gdsc.global.common.constant.RecruitmentConstant.*;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import static org.assertj.core.api.Assertions.*;
 
-import com.gdschongik.gdsc.domain.recruitment.dao.RecruitmentRepository;
-import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
+import com.gdschongik.gdsc.domain.recruitment.dto.request.RecruitmentCreateRequest;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.integration.IntegrationTest;
 import org.junit.jupiter.api.Nested;
@@ -15,22 +14,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 class AdminRecruitmentServiceTest extends IntegrationTest {
 
     @Autowired
-    private RecruitmentRepository recruitmentRepository;
+    private AdminRecruitmentService adminRecruitmentService;
 
     @Nested
     class 모집기간_생성시 {
         @Test
         void 기간이_중복되는_Recruitment가_있다면_실패한다() {
             // given
-            Recruitment recruitment = Recruitment.createRecruitment(RECRUITMENT_NAME, START_DATE, END_DATE);
-            recruitmentRepository.save(recruitment);
+            RecruitmentCreateRequest request = new RecruitmentCreateRequest(RECRUITMENT_NAME, START_DATE, END_DATE);
+            adminRecruitmentService.createRecruitment(request);
 
             // when & then
-            assertThatThrownBy(() -> {
-                        Recruitment duplicatedRecruitment =
-                                Recruitment.createRecruitment(RECRUITMENT_NAME, START_DATE, END_DATE);
-                        recruitmentRepository.save(duplicatedRecruitment);
-                    })
+            assertThatThrownBy(() -> adminRecruitmentService.createRecruitment(request))
                     .isInstanceOf(CustomException.class)
                     .hasMessage(PERIOD_DUPLICATE.getMessage());
         }
