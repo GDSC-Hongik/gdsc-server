152894c8f7a2033bb1fdad38a142d2cb5e6339e4
chosw1002@naver.com <chosw1002@naver.com>
2024-05-21 23:49:54 +0900

    remove: repository 코드 제거

--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepository.java
@@ -1,9 +1,3 @@
 package com.gdschongik.gdsc.domain.recruitment.dao;
 
-import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
-import java.util.Optional;
-
-public interface RecruitmentCustomRepository {
-
-    Optional<Recruitment> findOpenRecruitment();
-}
+public interface RecruitmentCustomRepository {}
--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepositoryImpl.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepositoryImpl.java
@@ -1,28 +1,10 @@
 package com.gdschongik.gdsc.domain.recruitment.dao;
 
-import static com.gdschongik.gdsc.domain.recruitment.domain.QRecruitment.*;
-
-import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
-import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDateTime;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class RecruitmentCustomRepositoryImpl implements RecruitmentCustomRepository {
 
     private final JPAQueryFactory queryFactory;
-
-    @Override
-    public Optional<Recruitment> findOpenRecruitment() {
-        return Optional.ofNullable(
-                queryFactory.selectFrom(recruitment).where(isOpen()).fetchOne());
-    }
-
-    // todo: 대시보드 기능에서 현재 시간을 클라이언트에게 받도록 수정
-    private BooleanBuilder isOpen() {
-        LocalDateTime now = LocalDateTime.now();
-        return new BooleanBuilder().and(recruitment.period.startDate.loe(now)).and(recruitment.period.endDate.goe(now));
-    }
 }
