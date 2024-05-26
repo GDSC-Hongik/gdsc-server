b617659a21d3a51bcbdf00c9d8e3a4221f4eb321
chosw1002@naver.com <chosw1002@naver.com>
2024-05-26 21:01:56 +0900

    docs: 용어 변경

--- src/main/java/com/gdschongik/gdsc/domain/recruitment/api/AdminRecruitmentController.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/api/AdminRecruitmentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Admin Recruitment", description = "어드민 리쿠르트먼트 관리 API입니다.")
+@Tag(name = "Admin Recruitment", description = "어드민 리쿠르팅 관리 API입니다.")
 @RestController
 @RequestMapping("/admin/recruitment")
 @RequiredArgsConstructor
@@ -20,7 +20,7 @@ public class AdminRecruitmentController {
 
     private final AdminRecruitmentService adminRecruitmentService;
 
-    @Operation(summary = "리쿠르트먼트 생성", description = "새로운 리쿠르트먼트(모집 기간)를 생성합니다.")
+    @Operation(summary = "리쿠르팅 생성", description = "새로운 리쿠르팅(모집 기간)를 생성합니다.")
     @PostMapping
     public ResponseEntity<Void> createRecruitment(@Valid @RequestBody RecruitmentCreateRequest request) {
         adminRecruitmentService.createRecruitment(request);
