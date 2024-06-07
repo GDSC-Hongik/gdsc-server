4119284d32a0e4478ed70299f9478c672b6d9701
chosw1002@naver.com <chosw1002@naver.com>
2024-06-07 20:31:34 +0900

    feat: 리쿠르팅 목록 조회 repository 구현

--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepository.java
@@ -1,3 +1,10 @@
 package com.gdschongik.gdsc.domain.recruitment.dao;
 
-public interface RecruitmentCustomRepository {}
+import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
+import com.gdschongik.gdsc.domain.recruitment.dto.request.RecruitmentQueryOption;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RecruitmentCustomRepository {
+    Page<Recruitment> findAllOrderByStartDate(RecruitmentQueryOption queryOption, Pageable pageable);
+}
--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepositoryImpl.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepositoryImpl.java
@@ -1,10 +1,35 @@
 package com.gdschongik.gdsc.domain.recruitment.dao;
 
+import static com.gdschongik.gdsc.domain.recruitment.domain.QRecruitment.*;
+
+import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
+import com.gdschongik.gdsc.domain.recruitment.dto.request.RecruitmentQueryOption;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 @RequiredArgsConstructor
-public class RecruitmentCustomRepositoryImpl implements RecruitmentCustomRepository {
+public class RecruitmentCustomRepositoryImpl extends RecruitmentQueryMethod implements RecruitmentCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<Recruitment> findAllOrderByStartDate(RecruitmentQueryOption queryOption, Pageable pageable) {
+        List<Recruitment> recruitments = queryFactory
+                .selectFrom(recruitment)
+                .where(matchesQueryOption(queryOption))
+                .orderBy(recruitment.period.startDate.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Long> countQuery =
+                queryFactory.select(recruitment.count()).from(recruitment).where(matchesQueryOption(queryOption));
+
+        return PageableExecutionUtils.getPage(recruitments, pageable, countQuery::fetchOne);
+    }
 }
