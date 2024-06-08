940f9c87249320614541481fab322ec0d5a9d6d5
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-08 21:42:59 +0900

    test: 발급쿠폰 테스트 추가

---
+++ src/test/java/com/gdschongik/gdsc/domain/coupon/domain/IssuedCouponTest.java
@@ -0,0 +1,26 @@
+package com.gdschongik.gdsc.domain.coupon.domain;
+
+import static com.gdschongik.gdsc.global.common.constant.MemberConstant.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.gdschongik.gdsc.domain.common.vo.Money;
+import com.gdschongik.gdsc.domain.member.domain.Member;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+
+class IssuedCouponTest {
+
+    @Test
+    void 발급쿠폰_사용하면_사용여부는_true이다() {
+        // given
+        Coupon coupon = Coupon.createCoupon("쿠폰이름", Money.from(BigDecimal.ONE));
+        Member member = Member.createGuestMember(OAUTH_ID);
+        IssuedCoupon issuedCoupon = IssuedCoupon.issue(coupon, member);
+
+        // when
+        issuedCoupon.useCoupon();
+
+        // then
+        assertThat(issuedCoupon.isUsed()).isTrue();
+    }
+}
