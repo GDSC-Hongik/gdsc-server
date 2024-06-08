7e478789dfdb86ed713bceb0e9cc423f1b2390e6
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-08 23:16:49 +0900

    test: 사용 및 회수 상황에 대한 예외 테스트 추가

--- src/test/java/com/gdschongik/gdsc/domain/coupon/domain/IssuedCouponTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/coupon/domain/IssuedCouponTest.java
@@ -1,26 +1,61 @@
 package com.gdschongik.gdsc.domain.coupon.domain;
 
 import static com.gdschongik.gdsc.global.common.constant.MemberConstant.*;
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+import static java.math.BigDecimal.*;
 import static org.assertj.core.api.Assertions.*;
 
 import com.gdschongik.gdsc.domain.common.vo.Money;
 import com.gdschongik.gdsc.domain.member.domain.Member;
-import java.math.BigDecimal;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class IssuedCouponTest {
 
-    @Test
-    void 발급쿠폰_사용하면_사용여부는_true이다() {
-        // given
-        Coupon coupon = Coupon.createCoupon("쿠폰이름", Money.from(BigDecimal.ONE));
-        Member member = Member.createGuestMember(OAUTH_ID);
-        IssuedCoupon issuedCoupon = IssuedCoupon.issue(coupon, member);
+    @Nested
+    class 발급쿠폰_사용할때 {
 
-        // when
-        issuedCoupon.use();
+        @Test
+        void 성공하면_사용여부는_true이다() {
+            // given
+            Coupon coupon = Coupon.createCoupon("쿠폰이름", Money.from(ONE));
+            Member member = Member.createGuestMember(OAUTH_ID);
+            IssuedCoupon issuedCoupon = IssuedCoupon.issue(coupon, member);
 
-        // then
-        assertThat(issuedCoupon.isUsed()).isTrue();
+            // when
+            issuedCoupon.use();
+
+            // then
+            assertThat(issuedCoupon.isUsed()).isTrue();
+        }
+
+        @Test
+        void 이미_사용한_쿠폰이면_실패한다() {
+            // given
+            Coupon coupon = Coupon.createCoupon("쿠폰이름", Money.from(ONE));
+            Member member = Member.createGuestMember(OAUTH_ID);
+            IssuedCoupon issuedCoupon = IssuedCoupon.issue(coupon, member);
+            issuedCoupon.use();
+
+            // when, then
+            assertThatThrownBy(issuedCoupon::use)
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(COUPON_ALREADY_USED.getMessage());
+        }
+
+        @Test
+        void 이미_회수한_쿠폰이면_실패한다() {
+            // given
+            Coupon coupon = Coupon.createCoupon("쿠폰이름", Money.from(ONE));
+            Member member = Member.createGuestMember(OAUTH_ID);
+            IssuedCoupon issuedCoupon = IssuedCoupon.issue(coupon, member);
+            issuedCoupon.revoke();
+
+            // when, then
+            assertThatThrownBy(issuedCoupon::use)
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(COUPON_ALREADY_REVOKED.getMessage());
+        }
     }
 }
