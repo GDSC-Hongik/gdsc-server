78ecf30f316d590ca1c5e48e79f796d935b4f00e
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-06-09 17:30:44 +0900

    feat: 쿠폰 도메인 구현 (#356)
    * feat: 금액 VO 클래스 구현
    
    * feat: 쿠폰 도메인 구현
    
    * chore: intellij로 빌드 시 generated 디렉토리 gitignore에 추가
    
    * feat: 발급쿠폰 엔티티 구현
    
    * test: 발급쿠폰 테스트 추가
    
    * feat: 회수여부 필드 및 사용가능 검사 정책 추가
    
    * refactor: 각 로직을 섹션별로 분리
    
    * refactor: useCoupon을 use로 변경
    
    * feat: 회수된 쿠폰이기에 발생하는 에러임을 명료하게 표현
    
    * fix: 회수여부 참일때 사용불가하도록 수정
    
    * feat: 쿠폰 회수하기 메서드 추가
    
    * test: 사용 및 회수 상황에 대한 예외 테스트 추가
    
    * feat: 이미 사용하면 쿠폰이면 회수 불가하도록 변경
    
    * test: 발급쿠폰 회수 테스트 추가
    
    * feat: 회수여부 리턴하는 메서드 추가
    
    * refactor: 회수된 쿠폰은 사용할 수 없다는 의미가 더 잘 드러나게 수정
    
    * refactor: 이미 사용한 쿠폰일 때 재사용 및 회수 상황 분리하도록 수정
    
    * test: 쿠폰 상수 클래스 추가
    
    * docs: 테스트 주석 수정
    
    * fix: 예외 메세지 수정 누락된 부분 픽스
    
    * feat: 쿠폰 생성시 할인금액 양수 정책 추가
    
    * test: 쿠폰 생성 테스트 추가
    
    * feat: 쿠폰 정보 업데이트 메서드 추가
    
    * test: 쿠폰 수정 테스트 추가
    
    * test: when절의 인자값이 더 명료하게 표현되도록 변경

--- src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
@@ -25,6 +25,9 @@ public enum ErrorCode {
     // Parameter
     INVALID_QUERY_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 쿼리 파라미터입니다."),
 
+    // Money
+    MONEY_AMOUNT_NOT_NULL(HttpStatus.INTERNAL_SERVER_ERROR, "금액은 null이 될 수 없습니다."),
+
     // Member
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 커뮤니티 멤버입니다."),
     MEMBER_DELETED(HttpStatus.CONFLICT, "탈퇴한 회원입니다."),
@@ -71,7 +74,14 @@ public enum ErrorCode {
     RECRUITMENT_PERIOD_MISMATCH_ACADEMIC_YEAR(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일의 연도가 학년도와 일치하지 않습니다."),
     RECRUITMENT_PERIOD_MISMATCH_SEMESTER_TYPE(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일의 입력된 학기가 일치하지 않습니다."),
     RECRUITMENT_PERIOD_SEMESTER_TYPE_UNMAPPED(HttpStatus.CONFLICT, "모집 시작일과 종료일이 매핑되는 학기가 없습니다."),
-    RECRUITMENT_PERIOD_NOT_WITHIN_TWO_WEEKS(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일이 학기 시작일로부터 2주 이내에 있지 않습니다.");
+    RECRUITMENT_PERIOD_NOT_WITHIN_TWO_WEEKS(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일이 학기 시작일로부터 2주 이내에 있지 않습니다."),
+
+    // Coupon
+    COUPON_DISCOUNT_AMOUNT_NOT_POSITIVE(HttpStatus.CONFLICT, "쿠폰의 할인 금액은 0보다 커야 합니다."),
+    COUPON_NOT_USABLE_ALREADY_USED(HttpStatus.CONFLICT, "이미 사용한 쿠폰은 사용할 수 없습니다."),
+    COUPON_NOT_USABLE_REVOKED(HttpStatus.CONFLICT, "회수된 쿠폰은 사용할 수 없습니다."),
+    COUPON_NOT_REVOKABLE_ALREADY_USED(HttpStatus.CONFLICT, "이미 사용한 쿠폰은 회수할 수 없습니다."),
+    ;
 
     private final HttpStatus status;
     private final String message;
