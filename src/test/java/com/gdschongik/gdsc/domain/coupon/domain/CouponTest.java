bbd3872c44d9840c7709a9ce1d7f0c338674f313
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-09 17:19:00 +0900

    test: 쿠폰 생성 테스트 추가

---
+++ src/test/java/com/gdschongik/gdsc/domain/coupon/domain/CouponTest.java
@@ -0,0 +1,41 @@
+package com.gdschongik.gdsc.domain.coupon.domain;
+
+import static com.gdschongik.gdsc.global.common.constant.CouponConstant.*;
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+import static java.math.BigDecimal.*;
+import static org.assertj.core.api.Assertions.*;
+
+import com.gdschongik.gdsc.domain.common.vo.Money;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class CouponTest {
+
+    @Nested
+    class 쿠폰_생성할때 {
+
+        @Test
+        void 성공한다() {
+            // given
+            Money discountAmount = Money.from(ONE);
+
+            // when
+            Coupon coupon = Coupon.createCoupon(COUPON_NAME, discountAmount);
+
+            // then
+            assertThat(coupon).isNotNull();
+        }
+
+        @Test
+        void 할인금액이_양수가_아니라면_실패한다() {
+            // given
+            Money discountAmount = Money.from(ZERO);
+
+            // when & then
+            assertThatThrownBy(() -> Coupon.createCoupon(COUPON_NAME, discountAmount))
+                    .isInstanceOf(CustomException.class)
+                    .hasMessageContaining(COUPON_DISCOUNT_AMOUNT_NOT_POSITIVE.getMessage());
+        }
+    }
+}
