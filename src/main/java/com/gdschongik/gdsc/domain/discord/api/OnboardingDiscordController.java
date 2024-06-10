45654fde7dbf9931ecc5b0f53d6e3a74c6544bad
uwoobeat <desktop3711@gmail.com>
2024-06-11 03:14:49 +0900

    feat: 디스코드 유저네임 및 닉네임에 요청값에 대한 검증 추가

--- src/main/java/com/gdschongik/gdsc/domain/discord/api/OnboardingDiscordController.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/api/OnboardingDiscordController.java
@@ -1,13 +1,17 @@
 package com.gdschongik.gdsc.domain.discord.api;
 
+import static com.gdschongik.gdsc.global.common.constant.RegexConstant.*;
+
 import com.gdschongik.gdsc.domain.discord.application.OnboardingDiscordService;
 import com.gdschongik.gdsc.domain.discord.dto.request.DiscordLinkRequest;
 import com.gdschongik.gdsc.domain.discord.dto.response.DiscordCheckDuplicateResponse;
 import com.gdschongik.gdsc.domain.discord.dto.response.DiscordCheckJoinResponse;
-import com.gdschongik.gdsc.domain.discord.dto.response.DiscordNicknameResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,21 +37,27 @@ public class OnboardingDiscordController {
 
     @Operation(summary = "디스코드 사용자명 중복 확인하기", description = "디스코드 사용자명이 중복되는지 확인합니다.")
     @GetMapping("/check-discord-username")
-    public ResponseEntity<DiscordCheckDuplicateResponse> checkDiscordUsername(String discordUsername) {
+    public ResponseEntity<DiscordCheckDuplicateResponse> checkDiscordUsername(
+            @NotBlank @Schema(description = "디스코드 유저네임") String discordUsername) {
         DiscordCheckDuplicateResponse response = onboardingDiscordService.checkUsernameDuplicate(discordUsername);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "디스코드 닉네임 중복 확인하기", description = "디스코드 닉네임이 중복되는지 확인합니다.")
     @GetMapping("/check-discord-nickname")
-    public ResponseEntity<DiscordNicknameResponse> checkDiscordNickname(String nickname) {
+    public ResponseEntity<DiscordCheckDuplicateResponse> checkDiscordNickname(
+            @NotBlank
+                    @Pattern(regexp = NICKNAME, message = "닉네임은 " + NICKNAME + " 형식이어야 합니다.")
+                    @Schema(description = "커뮤니티 닉네임", pattern = NICKNAME)
+                    String nickname) {
         DiscordCheckDuplicateResponse response = onboardingDiscordService.checkNicknameDuplicate(nickname);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "디스코드 합류 확인하기", description = "해당 사용자명을 가진 유저가 디스코드 서버에 합류했는지 확인합니다.")
     @GetMapping("/check-discord-join")
-    public ResponseEntity<DiscordCheckJoinResponse> checkDiscordJoin(String discordUsername) {
+    public ResponseEntity<DiscordCheckJoinResponse> checkDiscordJoin(
+            @NotBlank @Schema(description = "디스코드 유저네임") String discordUsername) {
         DiscordCheckJoinResponse response = onboardingDiscordService.checkServerJoined(discordUsername);
         return ResponseEntity.ok(response);
     }
