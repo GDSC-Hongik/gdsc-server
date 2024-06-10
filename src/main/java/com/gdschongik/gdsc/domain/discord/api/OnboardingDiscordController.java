517bf16412f2115c824405be025f15ef2acdafa7
uwoobeat <desktop3711@gmail.com>
2024-06-11 02:52:50 +0900

    feat: 디스코드 관련 컨트롤러 메서드 및 dto 구현

--- src/main/java/com/gdschongik/gdsc/domain/discord/api/OnboardingDiscordController.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/api/OnboardingDiscordController.java
@@ -2,11 +2,15 @@ package com.gdschongik.gdsc.domain.discord.api;
 
 import com.gdschongik.gdsc.domain.discord.application.OnboardingDiscordService;
 import com.gdschongik.gdsc.domain.discord.dto.request.DiscordLinkRequest;
+import com.gdschongik.gdsc.domain.discord.dto.response.DiscordCheckDuplicateResponse;
+import com.gdschongik.gdsc.domain.discord.dto.response.DiscordCheckJoinResponse;
+import com.gdschongik.gdsc.domain.discord.dto.response.DiscordNicknameResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +29,26 @@ public class OnboardingDiscordController {
     public ResponseEntity<Void> linkDiscord(@Valid @RequestBody DiscordLinkRequest request) {
         onboardingDiscordService.verifyDiscordCode(request);
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "디스코드 사용자명 중복 확인하기", description = "디스코드 사용자명이 중복되는지 확인합니다.")
+    @GetMapping("/check-discord-username")
+    public ResponseEntity<DiscordCheckDuplicateResponse> checkDiscordUsername(String discordUsername) {
+        DiscordCheckDuplicateResponse response = onboardingDiscordService.checkUsernameDuplicate(discordUsername);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "디스코드 닉네임 중복 확인하기", description = "디스코드 닉네임이 중복되는지 확인합니다.")
+    @GetMapping("/check-discord-nickname")
+    public ResponseEntity<DiscordNicknameResponse> checkDiscordNickname(String nickname) {
+        DiscordCheckDuplicateResponse response = onboardingDiscordService.checkNicknameDuplicate(nickname);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "디스코드 합류 확인하기", description = "해당 사용자명을 가진 유저가 디스코드 서버에 합류했는지 확인합니다.")
+    @GetMapping("/check-discord-join")
+    public ResponseEntity<DiscordCheckJoinResponse> checkDiscordJoin(String discordUsername) {
+        DiscordCheckJoinResponse response = onboardingDiscordService.checkServerJoined(discordUsername);
+        return ResponseEntity.ok(response);
     }
 }
