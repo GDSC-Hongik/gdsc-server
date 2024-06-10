d032c408953ddfcae8646a6dc1fa777ce8d7c89d
uwoobeat <desktop3711@gmail.com>
2024-06-11 03:22:37 +0900

    feat: 디스코드 연동 체크 로직 구현

--- src/main/java/com/gdschongik/gdsc/domain/discord/application/OnboardingDiscordService.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/application/OnboardingDiscordService.java
@@ -6,6 +6,8 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import com.gdschongik.gdsc.domain.discord.dao.DiscordVerificationCodeRepository;
 import com.gdschongik.gdsc.domain.discord.domain.DiscordVerificationCode;
 import com.gdschongik.gdsc.domain.discord.dto.request.DiscordLinkRequest;
+import com.gdschongik.gdsc.domain.discord.dto.response.DiscordCheckDuplicateResponse;
+import com.gdschongik.gdsc.domain.discord.dto.response.DiscordCheckJoinResponse;
 import com.gdschongik.gdsc.domain.discord.dto.response.DiscordNicknameResponse;
 import com.gdschongik.gdsc.domain.discord.dto.response.DiscordVerificationCodeResponse;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
@@ -103,5 +105,23 @@ public class OnboardingDiscordService {
         }
 
         return DiscordNicknameResponse.of(member.getNickname());
+    }
+
+    @Transactional(readOnly = true)
+    public DiscordCheckDuplicateResponse checkUsernameDuplicate(String discordUsername) {
+        boolean isExist = memberRepository.existsByDiscordUsername(discordUsername);
+        return DiscordCheckDuplicateResponse.from(isExist);
+    }
+
+    @Transactional(readOnly = true)
+    public DiscordCheckDuplicateResponse checkNicknameDuplicate(String nickname) {
+        boolean isExist = memberRepository.existsByNickname(nickname);
+        return DiscordCheckDuplicateResponse.from(isExist);
+    }
+
+    public DiscordCheckJoinResponse checkServerJoined(String discordUsername) {
+        boolean isJoined =
+                discordUtil.getOptionalMemberByUsername(discordUsername).isPresent();
+        return DiscordCheckJoinResponse.from(isJoined);
     }
 }
