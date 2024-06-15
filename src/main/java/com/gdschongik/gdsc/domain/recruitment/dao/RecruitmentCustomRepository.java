a50ea175c2ad7329feb5661f762a288aa616b2c6
chosw1002@naver.com <chosw1002@naver.com>
2024-06-15 10:11:15 +0900

    remove: 사용하지 않는 클래스 제거

--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dao/RecruitmentCustomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gdschongik.gdsc.domain.recruitment.dao;
-
-import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
-import java.util.List;
-
-public interface RecruitmentCustomRepository {
-    List<Recruitment> findAllOrderByStartDate();
-}
