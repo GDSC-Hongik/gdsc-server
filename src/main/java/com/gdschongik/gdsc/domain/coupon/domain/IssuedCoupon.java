1f1a170b430992f21c0ba8becf7f81d846a5ee7c
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-15 21:46:41 +0900

    feat: 발급쿠폰에 누락된 엔티티 세팅 추가

--- src/main/java/com/gdschongik/gdsc/domain/coupon/domain/IssuedCoupon.java
+++ src/main/java/com/gdschongik/gdsc/domain/coupon/domain/IssuedCoupon.java
@@ -7,20 +7,27 @@ import com.gdschongik.gdsc.domain.common.model.BaseTimeEntity;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
-import org.springframework.data.annotation.Id;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class IssuedCoupon extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "issued_coupon_id")
     private Long id;
 
