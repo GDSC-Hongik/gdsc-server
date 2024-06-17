bfded713f9b208ac573364ebde8f40475f5e6f61
Hanbee Lee <bee1162@naver.com>
2024-06-17 20:22:47 +0900

    fix: push 오류

--- src/test/java/com/gdschongik/gdsc/domain/member/application/MemberIntegrationTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/member/application/MemberIntegrationTest.java
@@ -2,7 +2,6 @@ package com.gdschongik.gdsc.domain.member.application;
 
 import static com.gdschongik.gdsc.domain.member.domain.Department.D022;
 import static com.gdschongik.gdsc.domain.member.domain.MemberRole.ASSOCIATE;
-import static com.gdschongik.gdsc.domain.member.domain.MemberRole.REGULAR;
 import static com.gdschongik.gdsc.global.common.constant.MemberConstant.*;
 import static com.gdschongik.gdsc.global.common.constant.MemberConstant.NICKNAME;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,7 +11,7 @@ import com.gdschongik.gdsc.domain.member.application.handler.MemberRegularEventH
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.domain.MemberAssociateEvent;
-import com.gdschongik.gdsc.domain.member.domain.MemberRegularEvent;
+import com.gdschongik.gdsc.domain.membership.dao.MembershipRepository;
 import com.gdschongik.gdsc.integration.IntegrationTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -28,6 +27,9 @@ public class MemberIntegrationTest extends IntegrationTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private MembershipRepository membershipRepository;
 
     @Test
     void 준회원_승급조건_만족됐으면_MemberRole은_ASSOCIATE이다() {
@@ -45,23 +47,5 @@ public class MemberIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(member.getRole()).isEqualTo(ASSOCIATE);
-    }
-
-    @Test
-    void 정회원_승급조건_만족됐으면_MemberRole은_REGULAR이다() {
-        // given
-        Member member = Member.createGuestMember(OAUTH_ID);
-        memberRepository.save(member);
-        member.completeUnivEmailVerification(UNIV_EMAIL);
-        member.updateBasicMemberInfo(STUDENT_ID, NAME, PHONE_NUMBER, D022, EMAIL);
-        member.verifyDiscord(DISCORD_USERNAME, NICKNAME);
-        member.verifyBevy();
-        member.advanceToAssociate();
-
-        // when
-        memberRegularEventHandler.advanceToRegular(new MemberRegularEvent(member.getId(), DISCORD_USERNAME));
-
-        // then
-        assertThat(member.getRole()).isEqualTo(REGULAR);
     }
 }
