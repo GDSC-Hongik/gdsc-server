fdb89c6657b9620fa5f4ac179a210c40d05611d2
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-19 02:16:01 +0900

    feat: 쿠폰 조회 API 구현

--- src/main/java/com/gdschongik/gdsc/domain/coupon/api/AdminCouponController.java
+++ src/main/java/com/gdschongik/gdsc/domain/coupon/api/AdminCouponController.java
@@ -2,11 +2,17 @@ package com.gdschongik.gdsc.domain.coupon.api;
 
 import com.gdschongik.gdsc.domain.coupon.application.CouponService;
 import com.gdschongik.gdsc.domain.coupon.dto.request.CouponCreateRequest;
+import com.gdschongik.gdsc.domain.coupon.dto.request.CouponIssueRequest;
+import com.gdschongik.gdsc.domain.coupon.dto.response.CouponResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +31,12 @@ public class AdminCouponController {
     public ResponseEntity<Void> createCoupon(@Valid @RequestBody CouponCreateRequest request) {
         couponService.createCoupon(request);
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "쿠폰 조회", description = "쿠폰을 조회합니다.")
+    @GetMapping
+    public ResponseEntity<List<CouponResponse>> getCoupons() {
+        List<CouponResponse> response = couponService.findAllCoupons();
+        return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/gdschongik/gdsc/domain/coupon/application/CouponService.java
+++ src/main/java/com/gdschongik/gdsc/domain/coupon/application/CouponService.java
@@ -1,9 +1,19 @@
 package com.gdschongik.gdsc.domain.coupon.application;
 
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
 import com.gdschongik.gdsc.domain.common.vo.Money;
 import com.gdschongik.gdsc.domain.coupon.dao.CouponRepository;
+import com.gdschongik.gdsc.domain.coupon.dao.IssuedCouponRepository;
 import com.gdschongik.gdsc.domain.coupon.domain.Coupon;
+import com.gdschongik.gdsc.domain.coupon.domain.IssuedCoupon;
 import com.gdschongik.gdsc.domain.coupon.dto.request.CouponCreateRequest;
+import com.gdschongik.gdsc.domain.coupon.dto.request.CouponIssueRequest;
+import com.gdschongik.gdsc.domain.coupon.dto.response.CouponResponse;
+import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.util.MemberUtil;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,12 +25,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CouponService {
 
+    private final MemberUtil memberUtil;
     private final CouponRepository couponRepository;
+    private final IssuedCouponRepository issuedCouponRepository;
 
     @Transactional
     public void createCoupon(CouponCreateRequest request) {
         Coupon coupon = Coupon.createCoupon(request.name(), Money.from(request.discountAmount()));
         couponRepository.save(coupon);
         log.info("[CouponService] 쿠폰 생성: name={}, discountAmount={}", request.name(), request.discountAmount());
+    }
+
+    public List<CouponResponse> findAllCoupons() {
+        return couponRepository.findAll().stream().map(CouponResponse::from).toList();
     }
 }
