9e8abc9c430a08f3e9ef9dffefb61e9c9a6f3d80
chosw1002@naver.com <chosw1002@naver.com>
2024-06-18 21:45:47 +0900

    remove: 사용하지 않는 레포지토리 제거

--- src/main/java/com/gdschongik/gdsc/domain/email/dao/UnivEmailVerificationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gdschongik.gdsc.domain.email.dao;
-
-import com.gdschongik.gdsc.domain.email.domain.UnivEmailVerification;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UnivEmailVerificationRepository extends CrudRepository<UnivEmailVerification, String> {}
