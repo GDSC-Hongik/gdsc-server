458bc007f69933432ebd59f48e8f93e81d872cf3
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-06-23 23:15:39 +0900

    fix: PhoneFormatter의 null 처리 (#403)

--- src/main/java/com/gdschongik/gdsc/global/util/formatter/PhoneFormatter.java
+++ src/main/java/com/gdschongik/gdsc/global/util/formatter/PhoneFormatter.java
@@ -1,17 +1,20 @@
 package com.gdschongik.gdsc.global.util.formatter;
 
+import jakarta.annotation.Nullable;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PhoneFormatter {
-    public static String format(String phone) {
-        return new StringBuilder(12)
-                .append(phone, 0, 3)
-                .append('-')
-                .append(phone, 3, 7)
-                .append('-')
-                .append(phone, 7, 11)
-                .toString();
+    public static String format(@Nullable String phone) {
+        return phone == null
+                ? null
+                : new StringBuilder(12)
+                        .append(phone, 0, 3)
+                        .append('-')
+                        .append(phone, 3, 7)
+                        .append('-')
+                        .append(phone, 7, 11)
+                        .toString();
     }
 }
