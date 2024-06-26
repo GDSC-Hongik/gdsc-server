8ce2d0aac791dcf396c26b99d3ec6f04f735d96d
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-26 23:54:40 +0900

    feat: 임시 주문 생성하는 정적 팩토리 생성 메서드 구현

--- src/main/java/com/gdschongik/gdsc/domain/order/domain/Order.java
+++ src/main/java/com/gdschongik/gdsc/domain/order/domain/Order.java
@@ -2,6 +2,8 @@ package com.gdschongik.gdsc.domain.order.domain;
 
 import com.gdschongik.gdsc.domain.common.model.BaseTimeEntity;
 import com.gdschongik.gdsc.domain.common.vo.Money;
+import com.gdschongik.gdsc.domain.coupon.domain.IssuedCoupon;
+import com.gdschongik.gdsc.domain.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -11,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
@@ -50,4 +53,43 @@ public class Order extends BaseTimeEntity {
     @Comment("최종결제금액")
     @Embedded
     private Money finalPaymentAmount;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Order(
+            OrderStatus status,
+            String nanoId,
+            Long memberId,
+            Long issuedCouponId,
+            Money totalAmount,
+            Money discountAmount,
+            Money finalPaymentAmount) {
+        this.status = status;
+        this.nanoId = nanoId;
+        this.memberId = memberId;
+        this.issuedCouponId = issuedCouponId;
+        this.totalAmount = totalAmount;
+        this.discountAmount = discountAmount;
+        this.finalPaymentAmount = finalPaymentAmount;
+    }
+
+    /**
+     * 결제 요청 전 임시 주문을 생성합니다.
+     */
+    public static Order createPending(
+            String nanoId,
+            Member member,
+            IssuedCoupon issuedCoupon,
+            Money totalAmount,
+            Money discountAmount,
+            Money finalPaymentAmount) {
+        return Order.builder()
+                .status(OrderStatus.PENDING)
+                .nanoId(nanoId)
+                .memberId(member.getId())
+                .issuedCouponId(issuedCoupon.getId())
+                .totalAmount(totalAmount)
+                .discountAmount(discountAmount)
+                .finalPaymentAmount(finalPaymentAmount)
+                .build();
+    }
 }
