2f97e48f3d388c0aeedd5436d4a69cc50d88a629
Jaehyun Ahn <desktop3711@gmail.com>
2024-06-29 22:46:48 +0900

    feat: 주문 생성 서비스 메서드 수정

--- src/main/java/com/gdschongik/gdsc/domain/order/application/OrderService.java
+++ src/main/java/com/gdschongik/gdsc/domain/order/application/OrderService.java
@@ -5,14 +5,16 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import com.gdschongik.gdsc.domain.common.vo.Money;
 import com.gdschongik.gdsc.domain.coupon.dao.IssuedCouponRepository;
 import com.gdschongik.gdsc.domain.coupon.domain.IssuedCoupon;
-import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.membership.dao.MembershipRepository;
+import com.gdschongik.gdsc.domain.membership.domain.Membership;
 import com.gdschongik.gdsc.domain.order.dao.OrderRepository;
+import com.gdschongik.gdsc.domain.order.domain.MoneyInfo;
 import com.gdschongik.gdsc.domain.order.domain.Order;
+import com.gdschongik.gdsc.domain.order.domain.OrderValidator;
 import com.gdschongik.gdsc.domain.order.dto.request.OrderCreateRequest;
-import com.gdschongik.gdsc.domain.recruitment.dao.RecruitmentRepository;
-import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,36 +25,40 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private final MemberUtil memberUtil;
     private final OrderRepository orderRepository;
-    private final RecruitmentRepository recruitmentRepository;
+    private final MembershipRepository membershipRepository;
     private final IssuedCouponRepository issuedCouponRepository;
-    private final MemberRepository memberRepository;
+    private final OrderValidator orderValidator;
 
     @Transactional
     public void createPendingOrder(OrderCreateRequest request) {
 
-        Member member =
-                memberRepository.findById(request.memberId()).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+        Membership membership = membershipRepository
+                .findById(request.membershipId())
+                .orElseThrow(() -> new CustomException(MEMBERSHIP_NOT_FOUND));
 
-        Recruitment recruitment = recruitmentRepository
-                .findById(request.recruitmentId())
-                .orElseThrow(() -> new CustomException(RECRUITMENT_NOT_FOUND));
+        IssuedCoupon issuedCoupon = request.issuedCouponId() == null ? null : getIssuedCoupon(request.issuedCouponId());
 
-        IssuedCoupon issuedCoupon = issuedCouponRepository
-                .findById(request.issuedCouponId())
-                .orElseThrow(() -> new CustomException(ISSUED_COUPON_NOT_FOUND));
-
-        Order order = Order.createPending(
-                request.orderNanoId(),
-                member,
-                recruitment,
-                issuedCoupon,
+        MoneyInfo moneyInfo = MoneyInfo.of(
                 Money.from(request.totalAmount()),
                 Money.from(request.discountAmount()),
                 Money.from(request.finalPaymentAmount()));
 
+        Member currentMember = memberUtil.getCurrentMember();
+
+        orderValidator.validatePendingOrderCreate(membership, issuedCoupon, moneyInfo, currentMember);
+
+        Order order = Order.createPending(request.orderNanoId(), membership, issuedCoupon, moneyInfo);
+
         orderRepository.save(order);
 
         log.info("[OrderService] 임시 주문 생성: orderId={}", order.getId());
+    }
+
+    private IssuedCoupon getIssuedCoupon(Long issuedCouponId) {
+        return issuedCouponRepository
+                .findById(issuedCouponId)
+                .orElseThrow(() -> new CustomException(ISSUED_COUPON_NOT_FOUND));
     }
 }
