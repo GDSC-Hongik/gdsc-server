5bf026dfdda1658d64ed4e72a9be838331ff457e
Hanbee Lee <bee1162@naver.com>
2024-06-30 15:50:10 +0900

    feat: impl클래스 네임 수정

--- src/main/java/com/gdschongik/gdsc/domain/coupon/dao/CouponCustomRepositoryImpl.java
+++ src/main/java/com/gdschongik/gdsc/domain/coupon/dao/CouponCustomRepositoryImpl.java
@@ -13,7 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 
 @RequiredArgsConstructor
-public class CouponCustomRepositoryIml extends CouponQueryMethod implements CouponCustomRepository {
+public class CouponCustomRepositoryImpl extends CouponQueryMethod implements CouponCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
