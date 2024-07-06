e889d7a9857287c4f361cadc93785458a6274033
uwoobeat <desktop3711@gmail.com>
2024-07-07 02:09:31 +0900

    feat: 토스페이먼츠 헤더 설정 추가

--- src/main/java/com/gdschongik/gdsc/infra/payment/client/PaymentClient.java
+++ src/main/java/com/gdschongik/gdsc/infra/payment/client/PaymentClient.java
@@ -1,13 +1,16 @@
 package com.gdschongik.gdsc.infra.payment.client;
 
+import com.gdschongik.gdsc.infra.payment.config.BasicAuthConfig;
 import com.gdschongik.gdsc.infra.payment.dto.request.PaymentConfirmRequest;
+import com.gdschongik.gdsc.infra.payment.dto.response.PaymentResponse;
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "https://api.tosspayments.com")
+@FeignClient(name = "paymentClient", url = "https://api.tosspayments.com", configuration = BasicAuthConfig.class)
 public interface PaymentClient {
 
     @PostMapping("/v1/payments/confirm")
-    void confirm(@RequestBody PaymentConfirmRequest request);
+    PaymentResponse confirm(@Valid @RequestBody PaymentConfirmRequest request);
 }
