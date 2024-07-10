9857250f93e458f601c1f9f55f18f21add07a69f
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-11 01:04:31 +0900

    feat: 시간 관련 포매팅 설정

--- src/main/java/com/gdschongik/gdsc/global/config/FeignConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/FeignConfig.java
@@ -2,8 +2,10 @@ package com.gdschongik.gdsc.global.config;
 
 import feign.Logger;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 
 @Configuration
 @EnableFeignClients("com.gdschongik.gdsc.infra")
@@ -12,5 +14,14 @@ public class FeignConfig {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public FeignFormatterRegistrar dateTimeFormatterRegistrar() {
+        return registry -> {
+            var registrar = new DateTimeFormatterRegistrar();
+            registrar.setUseIsoFormat(true);
+            registrar.registerFormatters(registry);
+        };
     }
 }
