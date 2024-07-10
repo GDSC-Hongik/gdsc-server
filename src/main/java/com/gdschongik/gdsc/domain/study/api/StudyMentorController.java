98fbf3a1008800697f2c081f06c4984c34f5a3ad
Hanbee Lee <bee1162@naver.com>
2024-07-10 20:56:51 +0900

    feat: 멘토의 스터디 과제 개설 API 생성

--- src/main/java/com/gdschongik/gdsc/domain/study/api/StudyMentorController.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/api/StudyMentorController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Study Mentor Management", description = "스터디 멘토 관리 API입니다.")
 @RestController
-@RequestMapping("/admin/mentor/management")
+@RequestMapping("/admin/mentor")
 @RequiredArgsConstructor
 public class StudyMentorController {
 
     @Operation(summary = "스터디 과제 개설", description = "멘토나 어드민이 과제를 개설할 수 있습니다.")
-    @PostMapping
+    @PostMapping("/assignment")
     public ResponseEntity<Void> createStudyAssignment(
             @PathVariable Long assignmentId, @Valid @RequestBody AssignmentCreateRequest request) {
         return null;
