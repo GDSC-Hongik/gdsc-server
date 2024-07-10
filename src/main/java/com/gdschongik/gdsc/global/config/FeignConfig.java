e425f1e269d13bb487295f791c8602b129898dc1
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-10 21:28:57 +0900

    feat: feign 로그 설정

--- src/main/java/com/gdschongik/gdsc/global/config/FeignConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/FeignConfig.java
@@ -1,8 +1,16 @@
 package com.gdschongik.gdsc.global.config;
 
+import feign.Logger;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients("com.gdschongik.gdsc.infra")
-public class FeignConfig {}
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
