e4adcd5f73b03db4dfea9d2e73ca17204f8bb3a6
chosw1002@naver.com <chosw1002@naver.com>
2024-07-13 00:13:52 +0900

    fix: 학기 시작일과 종료일을 직접 입력할 수 있도록 수정

--- src/main/java/com/gdschongik/gdsc/domain/recruitment/dto/request/RecruitmentCreateRequest.java
+++ src/main/java/com/gdschongik/gdsc/domain/recruitment/dto/request/RecruitmentCreateRequest.java
@@ -4,10 +4,14 @@ import static com.gdschongik.gdsc.global.common.constant.RegexConstant.*;
 
 import com.gdschongik.gdsc.domain.common.model.SemesterType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public record RecruitmentCreateRequest(
+        @Future @Schema(description = "학기 시작일", pattern = DATETIME) LocalDateTime semesterStartDate,
+        @Future @Schema(description = "학기 종료일", pattern = DATETIME) LocalDateTime semesterEndDate,
         @NotNull(message = "학년도는 null이 될 수 없습니다.") @Schema(description = "학년도", pattern = ACADEMIC_YEAR)
                 Integer academicYear,
         @NotNull(message = "학기는 null이 될 수 없습니다.") @Schema(description = "학기") SemesterType semesterType,
