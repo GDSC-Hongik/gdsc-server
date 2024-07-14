609211d0b11ded9f86ae13377a85a9b4851fe531
chosw1002@naver.com <chosw1002@naver.com>
2024-07-14 13:51:47 +0900

    test: 모집회차 생성 메서드 분리

--- src/test/java/com/gdschongik/gdsc/domain/membership/domain/MembershipValidatorTest.java
+++ src/test/java/com/gdschongik/gdsc/domain/membership/domain/MembershipValidatorTest.java
@@ -1,34 +1,59 @@
 package com.gdschongik.gdsc.domain.membership.domain;
 
 import static com.gdschongik.gdsc.domain.member.domain.Department.*;
+import static com.gdschongik.gdsc.domain.member.domain.Member.*;
 import static com.gdschongik.gdsc.global.common.constant.MemberConstant.*;
 import static com.gdschongik.gdsc.global.common.constant.RecruitmentConstant.*;
 import static com.gdschongik.gdsc.global.common.constant.SemesterConstant.*;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import static org.assertj.core.api.Assertions.*;
 
+import com.gdschongik.gdsc.domain.common.model.SemesterType;
+import com.gdschongik.gdsc.domain.common.vo.Money;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
 import com.gdschongik.gdsc.domain.recruitment.domain.RecruitmentRound;
 import com.gdschongik.gdsc.domain.recruitment.domain.vo.Period;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 class MembershipValidatorTest {
 
     MembershipValidator membershipValidator = new MembershipValidator();
+
+    private Member createAssociateMember(Long id) {
+        Member member = createGuestMember(OAUTH_ID);
+        member.updateBasicMemberInfo(STUDENT_ID, NAME, PHONE_NUMBER, D022, EMAIL);
+        member.completeUnivEmailVerification(UNIV_EMAIL);
+        member.verifyDiscord(DISCORD_USERNAME, NICKNAME);
+        member.verifyBevy();
+        member.advanceToAssociate();
+        ReflectionTestUtils.setField(member, "id", id);
+        return member;
+    }
+
+    private RecruitmentRound createRecruitmentRound(
+            Integer academicYear,
+            SemesterType semesterType,
+            Money fee,
+            LocalDateTime startDate,
+            LocalDateTime endDate) {
+        Recruitment recruitment = Recruitment.createRecruitment(
+                academicYear, semesterType, fee, Period.createPeriod(SEMESTER_START_DATE, SEMESTER_END_DATE));
+        return RecruitmentRound.create(RECRUITMENT_NAME, startDate, endDate, recruitment, ROUND_TYPE);
+    }
 
     @Nested
     class 멤버십_가입신청시 {
         @Test
         void 해당_RecruitmentRound의_모집기간이_아니라면_실패한다() {
             // given
-            Recruitment recruitment = Recruitment.createRecruitment(
-                    ACADEMIC_YEAR, SEMESTER_TYPE, FEE, Period.createPeriod(SEMESTER_START_DATE, SEMESTER_END_DATE));
             RecruitmentRound recruitmentRound =
-                    RecruitmentRound.create(RECRUITMENT_NAME, START_DATE, END_DATE, recruitment, ROUND_TYPE);
+                    createRecruitmentRound(ACADEMIC_YEAR, SEMESTER_TYPE, FEE, START_DATE, END_DATE);
 
             // when & then
             assertThatThrownBy(() -> membershipValidator.validateMembershipSubmit(recruitmentRound, Optional.empty()))
@@ -39,18 +64,10 @@ class MembershipValidatorTest {
         @Test
         void 해당_학기에_이미_Membership을_발급받았다면_실패한다() {
             // given
-            Member member = Member.createGuestMember(OAUTH_ID);
-            member.updateBasicMemberInfo(STUDENT_ID, NAME, PHONE_NUMBER, D022, EMAIL);
-            member.completeUnivEmailVerification(UNIV_EMAIL);
-            member.verifyDiscord(DISCORD_USERNAME, NICKNAME);
-            member.verifyBevy();
-            member.advanceToAssociate();
-
-            Recruitment recruitment = Recruitment.createRecruitment(
-                    ACADEMIC_YEAR, SEMESTER_TYPE, FEE, Period.createPeriod(SEMESTER_START_DATE, SEMESTER_END_DATE));
+            Member member = createAssociateMember(1L);
 
             RecruitmentRound recruitmentRound =
-                    RecruitmentRound.create(RECRUITMENT_NAME, START_DATE, END_DATE, recruitment, ROUND_TYPE);
+                    createRecruitmentRound(ACADEMIC_YEAR, SEMESTER_TYPE, FEE, START_DATE, END_DATE);
 
             Membership membership = Membership.createMembership(member, recruitmentRound);
 
@@ -67,18 +84,10 @@ class MembershipValidatorTest {
         @Test
         void 해당_학기에_이미_Membership을_생성한_적이_있다면_실패한다() {
             // given
-            Member member = Member.createGuestMember(OAUTH_ID);
-            member.updateBasicMemberInfo(STUDENT_ID, NAME, PHONE_NUMBER, D022, EMAIL);
-            member.completeUnivEmailVerification(UNIV_EMAIL);
-            member.verifyDiscord(DISCORD_USERNAME, NICKNAME);
-            member.verifyBevy();
-            member.advanceToAssociate();
-
-            Recruitment recruitment = Recruitment.createRecruitment(
-                    ACADEMIC_YEAR, SEMESTER_TYPE, FEE, Period.createPeriod(SEMESTER_START_DATE, SEMESTER_END_DATE));
+            Member member = createAssociateMember(1L);
 
             RecruitmentRound recruitmentRound =
-                    RecruitmentRound.create(RECRUITMENT_NAME, START_DATE, END_DATE, recruitment, ROUND_TYPE);
+                    createRecruitmentRound(ACADEMIC_YEAR, SEMESTER_TYPE, FEE, START_DATE, END_DATE);
 
             Membership membership = Membership.createMembership(member, recruitmentRound);
 
