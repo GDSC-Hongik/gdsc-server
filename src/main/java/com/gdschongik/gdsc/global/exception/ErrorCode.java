d2fba644d3c00eb29912cb6bfd055ed91aabe933
chosw1002@naver.com <chosw1002@naver.com>
2024-07-17 22:04:40 +0900

    remove: 사용하지 않는 ErrorCode 제거

--- src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
@@ -80,12 +80,9 @@ public enum ErrorCode {
     DATE_PRECEDENCE_INVALID(HttpStatus.BAD_REQUEST, "종료일이 시작일과 같거나 앞설 수 없습니다."),
     RECRUITMENT_OVERLAP(HttpStatus.BAD_REQUEST, "해당 학기에 이미 리크루팅이 존재합니다."),
     RECRUITMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "리크루팅이 존재하지 않습니다."),
-    RECRUITMENT_PERIOD_MISMATCH_ACADEMIC_YEAR(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일의 연도가 학년도와 일치하지 않습니다."),
-    RECRUITMENT_PERIOD_MISMATCH_SEMESTER_TYPE(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일의 입력된 학기가 일치하지 않습니다."),
     RECRUITMENT_PERIOD_NOT_WITHIN_TWO_WEEKS(HttpStatus.BAD_REQUEST, "모집 시작일과 종료일이 학기 시작일로부터 2주 이내에 있지 않습니다."),
 
     // RecruitmentRound
-    RECRUITMENT_ROUND_NOT_OPEN(HttpStatus.CONFLICT, "모집회차 모집기간이 아닙니다."),
     RECRUITMENT_ROUND_NOT_FOUND(HttpStatus.NOT_FOUND, "모집회차가 존재하지 않습니다."),
     RECRUITMENT_ROUND_TYPE_OVERLAP(HttpStatus.BAD_REQUEST, "모집 차수가 중복됩니다."),
     RECRUITMENT_ROUND_STARTDATE_ALREADY_PASSED(HttpStatus.BAD_REQUEST, "이미 모집 시작일이 지난 모집회차입니다."),
