38b8955762ecd3f63a095d06a79189db94d55925
uwoobeat <desktop3711@gmail.com>
2024-07-19 00:08:22 +0900

    chore: feign 역직렬화 관련 설정

--- src/main/java/com/gdschongik/gdsc/infra/feign/global/config/FeignConfig.java
+++ src/main/java/com/gdschongik/gdsc/infra/feign/global/config/FeignConfig.java
@@ -1,6 +1,10 @@
 package com.gdschongik.gdsc.infra.feign.global.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Logger;
+import feign.codec.Decoder;
+import feign.jackson.JacksonDecoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +14,17 @@ import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 @Configuration
 @EnableFeignClients("com.gdschongik.gdsc.infra")
 public class FeignConfig {
+
+    @Bean
+    public Decoder feignDecoder() {
+        return new JacksonDecoder(customObjectMapper());
+    }
+
+    public ObjectMapper customObjectMapper() {
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+    }
 
     @Bean
     Logger.Level feignLoggerLevel() {
