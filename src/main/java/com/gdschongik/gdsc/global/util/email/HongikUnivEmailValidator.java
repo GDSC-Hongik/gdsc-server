2d3f2823434661d4b611f4e48a29aab4bdc3cfd5
chosw1002@naver.com <chosw1002@naver.com>
2024-07-19 22:35:52 +0900

    refactor: ErrorCode를 static import 하도록 수정

--- src/main/java/com/gdschongik/gdsc/global/util/email/HongikUnivEmailValidator.java
+++ src/main/java/com/gdschongik/gdsc/global/util/email/HongikUnivEmailValidator.java
@@ -2,19 +2,19 @@ package com.gdschongik.gdsc.global.util.email;
 
 import static com.gdschongik.gdsc.global.common.constant.EmailConstant.HONGIK_UNIV_MAIL_DOMAIN;
 import static com.gdschongik.gdsc.global.common.constant.RegexConstant.HONGIK_EMAIL;
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
+import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import java.util.Optional;
 
 @DomainService
 public class HongikUnivEmailValidator {
 
     public void validate(String email, Optional<Member> optionalMember) {
         if (!email.contains(HONGIK_UNIV_MAIL_DOMAIN)) {
-            throw new CustomException(ErrorCode.UNIV_EMAIL_DOMAIN_MISMATCH);
+            throw new CustomException(UNIV_EMAIL_DOMAIN_MISMATCH);
         }
 
         if (!email.matches(HONGIK_EMAIL)) {
