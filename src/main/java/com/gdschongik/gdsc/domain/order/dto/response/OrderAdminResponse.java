cb4d267acff493956d51cc38b044116955c7fcaf
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-21 20:45:26 +0900

    feat: QueryProjection 추가

--- src/main/java/com/gdschongik/gdsc/domain/order/dto/response/OrderAdminResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/order/dto/response/OrderAdminResponse.java
@@ -1,5 +1,7 @@
 package com.gdschongik.gdsc.domain.order.dto.response;
 
+import com.gdschongik.gdsc.domain.common.model.SemesterType;
+import com.gdschongik.gdsc.domain.common.vo.Money;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.order.domain.MoneyInfo;
 import com.gdschongik.gdsc.domain.order.domain.Order;
@@ -7,7 +9,8 @@ import com.gdschongik.gdsc.domain.order.domain.OrderStatus;
 import com.gdschongik.gdsc.domain.recruitment.domain.Recruitment;
 import com.gdschongik.gdsc.global.util.formatter.MoneyFormatter;
 import com.gdschongik.gdsc.global.util.formatter.SemesterFormatter;
-import java.time.LocalDateTime;
+import com.querydsl.core.annotations.QueryProjection;
+import java.time.ZonedDateTime;
 
 public record OrderAdminResponse(
         Long orderId,
@@ -21,6 +24,35 @@ public record OrderAdminResponse(
         String discountAmount,
         String finalPaymentAmount,
         ZonedDateTime approvedAt) {
+
+    @QueryProjection
+    public OrderAdminResponse(
+            Long orderId,
+            Integer academicYear,
+            SemesterType semesterType,
+            String memberName,
+            OrderStatus status,
+            String studentId,
+            String nanoId,
+            String paymentKey,
+            Money totalAmount,
+            Money discountAmount,
+            Money finalPaymentAmount,
+            ZonedDateTime approvedAt) {
+        this(
+                orderId,
+                SemesterFormatter.format(academicYear, semesterType),
+                memberName,
+                status,
+                studentId,
+                nanoId,
+                paymentKey,
+                MoneyFormatter.format(totalAmount),
+                MoneyFormatter.format(discountAmount),
+                MoneyFormatter.format(finalPaymentAmount),
+                approvedAt);
+    }
+
     public static OrderAdminResponse from(Order order, Recruitment recruitment, Member member) {
         MoneyInfo moneyInfo = order.getMoneyInfo();
         return new OrderAdminResponse(
@@ -34,6 +66,6 @@ public record OrderAdminResponse(
                 MoneyFormatter.format(moneyInfo.getTotalAmount()),
                 MoneyFormatter.format(moneyInfo.getDiscountAmount()),
                 MoneyFormatter.format(moneyInfo.getFinalPaymentAmount()),
-                order.getUpdatedAt());
+                order.getApprovedAt());
     }
 }
