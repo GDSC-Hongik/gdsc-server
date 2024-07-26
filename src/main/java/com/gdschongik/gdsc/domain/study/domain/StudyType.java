48030c5348280b463608aebb1f5c5048b8b4fcf0
chosw1002@naver.com <chosw1002@naver.com>
2024-07-26 22:18:47 +0900

    fix: enum value 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyType.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyType.java
@@ -6,10 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum StudyType {
-    // todo: value를 과제 스터디, 온라인 세션, 오프라인 세션으로 변경 해야함.
-    ASSIGNMENT("과제"),
-    ONLINE("온라인"),
-    OFFLINE("오프라인");
+    ASSIGNMENT("과제 스터디"),
+    ONLINE("온라인 세션"),
+    OFFLINE("오프라인 세션");
 
     private final String value;
 }
