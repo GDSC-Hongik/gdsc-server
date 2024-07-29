08ba589fad3a70e5fa1e68851651097554ce50b3
seulgi99 <tmfrk0426@gmail.com>
2024-07-29 19:07:30 +0900

    refactor: 변경사항 반영

--- src/main/java/com/gdschongik/gdsc/domain/email/domain/HongikUnivEmailValidator.java
+++ src/main/java/com/gdschongik/gdsc/domain/email/domain/HongikUnivEmailValidator.java
@@ -6,6 +6,7 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import java.util.Optional;
 
 @DomainService
 public class HongikUnivEmailValidator {
@@ -21,6 +22,20 @@ public class HongikUnivEmailValidator {
 
         if (isUnivEmailDuplicate) {
             throw new CustomException(UNIV_EMAIL_ALREADY_SATISFIED);
+        }
+    }
+
+    /**
+     * redis 안의 존재하는 메일인증 정보로 검증
+     * 1. 토큰이 비었는데 인증하려할 시 에러 (인증메일을 보내지 않았거나, 만료된 경우)
+     * 2. 토큰이 redis에 저장된 토큰과 다르면 만료되었다는 에러 (메일 여러번 보낸 경우)
+     */
+    public void validateUnivEmailVerification(
+            Optional<UnivEmailVerification> optionalUnivEmailVerification, String currentToken) {
+        if (optionalUnivEmailVerification.isEmpty()) {
+            throw new CustomException(EMAIL_NOT_SENT);
+        } else if (!optionalUnivEmailVerification.get().getVerificationToken().equals(currentToken)) {
+            throw new CustomException(EXPIRED_EMAIL_VERIFICATION_TOKEN);
         }
     }
 }
