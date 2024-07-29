4696df047a7836a8053849b815f32586dd6236bf
chosw1002@naver.com <chosw1002@naver.com>
2024-07-30 00:16:13 +0900

    refactor: 스터디의 데이터 전달 로직 활용하도록 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/application/StudyService.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/application/StudyService.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.study.application;
 
 import com.gdschongik.gdsc.domain.study.dao.StudyRepository;
+import com.gdschongik.gdsc.domain.study.domain.Study;
 import com.gdschongik.gdsc.domain.study.dto.response.StudyResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,7 @@ public class StudyService {
 
     public List<StudyResponse> getAllApplicableStudies() {
         return studyRepository.findAll().stream()
-                .filter(study -> study.getApplicationPeriod().isOpen())
+                .filter(Study::isApplicable)
                 .map(StudyResponse::from)
                 .toList();
     }
