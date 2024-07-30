cbb6869ad9ad9311d1344d009ba735fa8e7830e2
uwoobeat <desktop3711@gmail.com>
2024-07-31 01:01:23 +0900

    feat: 클래스 레벨 트랜잭션 설정 분리

--- src/main/java/com/gdschongik/gdsc/domain/member/application/CommonMemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/CommonMemberService.java
@@ -1,7 +1,14 @@
 package com.gdschongik.gdsc.domain.member.application;
 
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
+import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Department;
+import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.domain.member.dto.response.MemberDepartmentResponse;
+import com.gdschongik.gdsc.domain.membership.dao.MembershipRepository;
+import com.gdschongik.gdsc.domain.membership.domain.Membership;
+import com.gdschongik.gdsc.global.exception.CustomException;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class CommonMemberService {
 
+    private final MembershipRepository membershipRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
     public List<MemberDepartmentResponse> getDepartments() {
         return Arrays.stream(Department.values())
                 .map(MemberDepartmentResponse::from)
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     public List<MemberDepartmentResponse> searchDepartments(String departmentName) {
         if (departmentName == null) {
             return getDepartments();
