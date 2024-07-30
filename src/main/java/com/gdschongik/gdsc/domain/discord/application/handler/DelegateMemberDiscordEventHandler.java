af8791b7fd4b2b89fa4cf974b0009b4e76a3043f
uwoobeat <desktop3711@gmail.com>
2024-07-31 01:52:28 +0900

    docs: 로그 추가

--- src/main/java/com/gdschongik/gdsc/domain/discord/application/handler/DelegateMemberDiscordEventHandler.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/application/handler/DelegateMemberDiscordEventHandler.java
@@ -5,11 +5,13 @@ import static com.gdschongik.gdsc.global.common.constant.DiscordConstant.*;
 import com.gdschongik.gdsc.domain.member.domain.MemberRegularEvent;
 import com.gdschongik.gdsc.global.util.DiscordUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DelegateMemberDiscordEventHandler implements SpringEventHandler {
@@ -24,5 +26,10 @@ public class DelegateMemberDiscordEventHandler implements SpringEventHandler {
         Role role = discordUtil.findRoleByName(MEMBER_ROLE_NAME);
 
         guild.addRoleToMember(member, role).queue();
+
+        log.info(
+                "[DelegateMemberDiscordEventHandler] 디스코드 서버 정회원 역할 부여 완료: memberId={}, discordId={}",
+                event.memberId(),
+                event.discordId());
     }
 }
--- src/main/java/com/gdschongik/gdsc/domain/discord/application/listener/DelegateMemberDiscordEventListener.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/application/listener/DelegateMemberDiscordEventListener.java
@@ -3,9 +3,12 @@ package com.gdschongik.gdsc.domain.discord.application.listener;
 import com.gdschongik.gdsc.domain.discord.application.handler.DelegateMemberDiscordEventHandler;
 import com.gdschongik.gdsc.domain.member.domain.MemberRegularEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DelegateMemberDiscordEventListener {
@@ -14,6 +17,7 @@ public class DelegateMemberDiscordEventListener {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void delegateMemberDiscordEvent(MemberRegularEvent event) {
+        log.info("[DelegateMemberDiscordEventListener] 정회원 승급 이벤트 수신: memberId={}", event.memberId());
         delegateMemberDiscordEventHandler.delegate(event);
     }
 }
