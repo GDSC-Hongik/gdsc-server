3a40b6009b3f1d55000c541080144cb7161e6dd8
Hanbee Lee <bee1162@naver.com>
2024-07-30 21:51:04 +0900

    feat: 오타 수정

--- src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/ErrorCode.java
@@ -129,7 +129,7 @@ public enum ErrorCode {
 
     // Assignment
     ASSIGNMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "과제가 존재하지 않습니다."),
-    ASSIGNMENT_CAN_NOT_BE_UPDATED(HttpStatus.CONFLICT, "휴강인 과제는 수정될 수 없습니다."),
+    ASSIGNMENT_CAN_NOT_BE_UPDATED(HttpStatus.CONFLICT, "휴강인 과제는 수정할 수 없습니다."),
     ;
 
     private final HttpStatus status;
