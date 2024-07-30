86bf00a943d63a4bccca583cf50393d4f6cd5cf5
uwoobeat <desktop3711@gmail.com>
2024-07-31 00:59:36 +0900

    feat: 멤버십 인증 이벤트로 변경

---
+++ src/main/java/com/gdschongik/gdsc/domain/membership/application/MembershipVerifiedEventHandler.java
@@ -0,0 +1,22 @@
+package com.gdschongik.gdsc.domain.membership.application;
+
+import com.gdschongik.gdsc.domain.member.application.CommonMemberService;
+import com.gdschongik.gdsc.domain.membership.domain.MembershipVerifedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MembershipVerifiedEventHandler {
+
+    private final CommonMemberService commonMemberService;
+
+    @EventListener
+    public void handleMembershipVerifiedEvent(MembershipVerifedEvent event) {
+        log.info("[MembershipVerifiedEventHandler] 멤버십 인증 이벤트 수신: membershipId={}", event.membershipId());
+        commonMemberService.associateMemberByMembership(event.membershipId());
+    }
+}
