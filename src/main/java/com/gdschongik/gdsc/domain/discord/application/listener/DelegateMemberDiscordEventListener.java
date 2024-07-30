a8c29eb1c76c44d8c2de23224e1476618e90b52a
uwoobeat <desktop3711@gmail.com>
2024-07-31 00:59:58 +0900

    feat: 트랜잭션 이벤트 리스너는 한번만 트리거되므로 일반 이벤트 리스너로 변경

--- src/main/java/com/gdschongik/gdsc/domain/discord/application/listener/DelegateMemberDiscordEventListener.java
+++ src/main/java/com/gdschongik/gdsc/domain/discord/application/listener/DelegateMemberDiscordEventListener.java
@@ -3,9 +3,8 @@ package com.gdschongik.gdsc.domain.discord.application.listener;
 import com.gdschongik.gdsc.domain.discord.application.handler.DelegateMemberDiscordEventHandler;
 import com.gdschongik.gdsc.domain.member.domain.MemberRegularEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -13,7 +12,7 @@ public class DelegateMemberDiscordEventListener {
 
     private final DelegateMemberDiscordEventHandler delegateMemberDiscordEventHandler;
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     public void delegateMemberDiscordEvent(MemberRegularEvent event) {
         delegateMemberDiscordEventHandler.delegate(event);
     }
