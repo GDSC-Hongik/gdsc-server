881efd1e4872d8e43b881fbb86414a3bf49a43b9
uwoobeat <desktop3711@gmail.com>
2024-07-31 21:54:04 +0900

    feat: PrincipalDetails에 관리역할 및 멘토역할 추가

--- src/main/java/com/gdschongik/gdsc/global/security/PrincipalDetails.java
+++ src/main/java/com/gdschongik/gdsc/global/security/PrincipalDetails.java
@@ -1,18 +1,27 @@
 package com.gdschongik.gdsc.global.security;
 
+import com.gdschongik.gdsc.domain.auth.dto.AccessTokenDto;
+import com.gdschongik.gdsc.domain.member.domain.MemberManageRole;
 import com.gdschongik.gdsc.domain.member.domain.MemberRole;
+import com.gdschongik.gdsc.domain.member.domain.MemberStudyRole;
 import java.util.Collection;
 import java.util.Collections;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PrincipalDetails implements UserDetails {
 
     private final Long memberId;
     private final MemberRole memberRole;
+    private final MemberManageRole memberManageRole;
+    private final MemberStudyRole memberStudyRole;
+
+    public static PrincipalDetails from(AccessTokenDto token) {
+        return new PrincipalDetails(token.memberId(), token.memberRole(), null, null);
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
