19bf891b5906b031549876775af2bc62f205dae5
chosw1002@naver.com <chosw1002@naver.com>
2024-07-31 23:44:25 +0900

    feat: todo 추가

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyHistoryValidator.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyHistoryValidator.java
@@ -32,6 +32,7 @@ public class StudyHistoryValidator {
     }
 
     public void validateCancelStudyApply(Study study) {
+        // todo: 스터디 정정기간 고려하도록 수정
         // 스터디 수강신청 기간이 아닌 경우
         if (!study.isApplicable()) {
             throw new CustomException(STUDY_APPLY_NOT_CANCELABLE);
