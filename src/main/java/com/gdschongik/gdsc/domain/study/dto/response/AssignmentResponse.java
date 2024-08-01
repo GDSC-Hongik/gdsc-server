e045867adb5fc65d11d93a71cfe5f82c85bf9f01
chosw1002@naver.com <chosw1002@naver.com>
2024-08-01 11:56:29 +0900

    feat: 과제 조회 api 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/study/dto/response/AssignmentResponse.java
@@ -0,0 +1,23 @@
+package com.gdschongik.gdsc.domain.study.dto.response;
+
+import com.gdschongik.gdsc.domain.study.domain.StudyDetail;
+import com.gdschongik.gdsc.domain.study.domain.StudyStatus;
+import com.gdschongik.gdsc.domain.study.domain.vo.Assignment;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AssignmentResponse(
+        Long studyDetailId,
+        @Schema(description = "과제 제목") String title,
+        @Schema(description = "마감 기한") String deadline,
+        @Schema(description = "과제 명세 링크") String descriptionLink,
+        @Schema(description = "과제 상태") StudyStatus assignmentStatus) {
+    public static AssignmentResponse from(StudyDetail studyDetail) {
+        Assignment assignment = studyDetail.getAssignment();
+        return new AssignmentResponse(
+                studyDetail.getId(),
+                assignment.getTitle(),
+                assignment.getDeadline().toString(),
+                assignment.getDescriptionLink(),
+                assignment.getStatus());
+    }
+}
