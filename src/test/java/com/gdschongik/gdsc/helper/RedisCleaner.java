ac38e5416b8be1bc48fbedc6d8f55a292ce7bd63
seulgi99 <tmfrk0426@gmail.com>
2024-08-03 02:01:37 +0900

    refactor: RedisCleaner afterPropertiesSet로 값들 초기화 하도록 변경

--- src/test/java/com/gdschongik/gdsc/helper/RedisCleaner.java
+++ src/test/java/com/gdschongik/gdsc/helper/RedisCleaner.java
@@ -1,26 +1,21 @@
 package com.gdschongik.gdsc.helper;
 
-import com.gdschongik.gdsc.global.property.RedisProperty;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class RedisCleaner implements InitializingBean {
-    private final RedisProperty redisProperty;
     private RedisClient redisClient;
     private StatefulRedisConnection<String, String> connection;
     private RedisCommands<String, String> commands;
 
     @Override
     public void afterPropertiesSet() {
-        RedisURI redisUri = RedisURI.Builder.redis(redisProperty.getHost(), redisProperty.getPort())
-                .build();
+        RedisURI redisUri = RedisURI.Builder.redis("localhost", 6379).build();
         redisClient = RedisClient.create(redisUri);
         connection = redisClient.connect();
         commands = connection.sync();
