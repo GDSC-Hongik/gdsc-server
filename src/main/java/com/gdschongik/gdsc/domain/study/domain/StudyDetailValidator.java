f96d2d4562abe32e1dfed1bceec2d8d6d5a5b658
Hanbee Lee <bee1162@naver.com>
2024-08-02 23:50:37 +0900

    fix: 코드 통일성 유지

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyDetailValidator.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyDetailValidator.java
@@ -1,11 +1,10 @@
 package com.gdschongik.gdsc.domain.study.domain;
 
-import static com.gdschongik.gdsc.global.exception.ErrorCode.ASSIGNMENT_DEADLINE_INVALID;
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.study.domain.request.AssignmentCreateRequest;
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
 import java.time.LocalDateTime;
 
 @DomainService
@@ -24,7 +23,7 @@ public class StudyDetailValidator {
     // 멘토가 아니라면 과제를 휴강처리 할 수 없다.
     private void validateMemberIsMentor(Long memberId, StudyDetail studyDetail) {
         if (!memberId.equals(studyDetail.getStudy().getMentor().getId())) {
-            throw new CustomException(ErrorCode.STUDY_DETAIL_NOT_MODIFIABLE_INVALID_ROLE);
+            throw new CustomException(STUDY_DETAIL_NOT_MODIFIABLE_INVALID_ROLE);
         }
     }
 
