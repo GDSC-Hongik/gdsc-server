34bb045d4983a21f3b7ef6bea802201dd8ad0cd8
uwoobeat <desktop3711@gmail.com>
2024-08-02 21:57:44 +0900

    feat: 신규 JWT 클레임 관련 상수 추가

--- src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
@@ -6,6 +6,8 @@ public class SecurityConstant {
     public static final String ACCESS_TOKEN_PARAM = "access";
     public static final String REFRESH_TOKEN_PARAM = "refresh";
     public static final String TOKEN_ROLE_NAME = "role";
+    public static final String TOKEN_MANAGE_ROLE_NAME = "manageRole";
+    public static final String TOKEN_STUDY_ROLE_NAME = "studyRole";
     public static final String GITHUB_NAME_ATTR_KEY = "id";
     public static final String ACCESS_TOKEN_HEADER_PREFIX = "Bearer ";
     public static final String ACCESS_TOKEN_HEADER_NAME = "Authorization";
