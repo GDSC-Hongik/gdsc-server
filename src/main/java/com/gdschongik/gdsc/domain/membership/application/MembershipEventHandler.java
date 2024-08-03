2a74662673103220f9f97af727bef7feae268d4d
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-03 17:28:01 +0900

    refactor: MembershipEventHandler로 이름 변경

--- src/main/java/com/gdschongik/gdsc/domain/membership/application/MembershipEventHandler.java
+++ src/main/java/com/gdschongik/gdsc/domain/membership/application/MembershipEventHandler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class MembershipVerifiedEventHandler {
+public class MembershipEventHandler {
 
     private final CommonMemberService commonMemberService;
 
