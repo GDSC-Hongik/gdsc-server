aecef42a7f09d9623ca2f0adcfbaa6c1f08b223b
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-03 20:04:36 +0900

    feat: 멤버십 회비납입상태 취소 이벤트 핸들러 로직 구현

--- src/main/java/com/gdschongik/gdsc/domain/membership/application/MembershipEventHandler.java
+++ src/main/java/com/gdschongik/gdsc/domain/membership/application/MembershipEventHandler.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.membership.application;
 
 import com.gdschongik.gdsc.domain.member.application.CommonMemberService;
+import com.gdschongik.gdsc.domain.membership.domain.MembershipPaymentRevokedEvent;
 import com.gdschongik.gdsc.domain.membership.domain.MembershipVerifiedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,5 +19,11 @@ public class MembershipEventHandler {
     public void handleMembershipVerifiedEvent(MembershipVerifiedEvent event) {
         log.info("[MembershipVerifiedEventHandler] 멤버십 인증 이벤트 수신: membershipId={}", event.membershipId());
         commonMemberService.advanceMemberToRegularByMembership(event.membershipId());
+    }
+
+    @EventListener
+    public void handleMembershipPaymentRevokedEvent(MembershipPaymentRevokedEvent event) {
+        log.info("[MembershipVerifiedEventHandler] 멤버십 회비납입 취소 이벤트 수신: membershipId={}", event.membershipId());
+        commonMemberService.demoteMemberToAssocidateByMembership(event.membershipId());
     }
 }
