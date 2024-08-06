9f999bc11431c9153a1c12bcd23a4ccc5f37ba24
chosw1002@naver.com <chosw1002@naver.com>
2024-08-06 11:39:44 +0900

    rename: enum name 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionStatus.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionStatus.java
@@ -9,7 +9,7 @@ public enum AssignmentSubmissionStatus {
     NOT_SUBMITTED("미제출"),
     WORD_COUNT_INSUFFICIENT("글자수 부족"),
     LOCATION_UNIDENTIFIABLE("위치 확인불가"),
-    SUBMISSION_SUCCESS("제출 성공");
+    SUCCESS("제출 성공");
 
     private final String value;
 }
