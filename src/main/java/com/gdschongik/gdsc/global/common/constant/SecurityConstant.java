9e43d513cda092e632ea14c1134288c8d36e9334
uwoobeat <desktop3711@gmail.com>
2024-08-06 23:29:16 +0900

    feat: referer를 사용하여 처리하도록 base uri 관련 로직 제거

--- src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/SecurityConstant.java
@@ -7,8 +7,6 @@ public class SecurityConstant {
     public static final String TOKEN_STUDY_ROLE_NAME = "studyRole";
     public static final String GITHUB_NAME_ATTR_KEY = "id";
     public static final String ACCESS_TOKEN_HEADER_PREFIX = "Bearer ";
-    public static final String OAUTH_BASE_URI_COOKIE_NAME = "oauth-base-uri";
-    public static final String OAUTH_REDIRECT_PATH_SEGMENT = "/social-login/redirect";
 
     private SecurityConstant() {}
 }
--- src/main/java/com/gdschongik/gdsc/global/security/CustomSuccessHandler.java
+++ src/main/java/com/gdschongik/gdsc/global/security/CustomSuccessHandler.java
@@ -7,16 +7,14 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import com.gdschongik.gdsc.domain.auth.application.JwtService;
 import com.gdschongik.gdsc.domain.auth.dto.AccessTokenDto;
 import com.gdschongik.gdsc.domain.auth.dto.RefreshTokenDto;
-import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.global.util.CookieUtil;
-import jakarta.servlet.http.Cookie;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -27,14 +25,13 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public CustomSuccessHandler(JwtService jwtService, CookieUtil cookieUtil) {
         this.jwtService = jwtService;
         this.cookieUtil = cookieUtil;
+        setUseReferer(true);
     }
 
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request, HttpServletResponse response, Authentication authentication)
-            throws IOException {
-        String baseUri = determineTargetUrl(request, response);
-
+            throws IOException, ServletException {
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
         // 토큰 생성 후 쿠키에 저장
@@ -43,31 +40,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         RefreshTokenDto refreshTokenDto = jwtService.createRefreshToken(memberAuthInfo.memberId());
         cookieUtil.addTokenCookies(response, accessTokenDto.tokenValue(), refreshTokenDto.tokenValue());
 
-        String redirectUri = UriComponentsBuilder.fromHttpUrl(baseUri)
-                .path(OAUTH_REDIRECT_PATH_SEGMENT)
-                .toUriString();
-
-        getRedirectStrategy().sendRedirect(request, response, redirectUri);
-    }
-
-    @Override
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response) {
-        Cookie baseUriCookie = cookieUtil
-                .findCookie(request, OAUTH_BASE_URI_COOKIE_NAME)
-                .orElseThrow(() -> new CustomException(BASE_URI_COOKIE_NOT_FOUND));
-
-        String baseUri = baseUriCookie.getValue();
-        validateBaseUri(baseUri);
-
-        cookieUtil.deleteCookie(response, baseUriCookie);
-
-        return baseUri;
-    }
-
-    private void validateBaseUri(String baseUri) {
-        if (!baseUri.endsWith(ROOT_DOMAIN)) {
-            log.warn("허용되지 않은 BASE URI로의 리다이렉트 요청 발생. 쿠키 조작이 의심됩니다: baseUri={}", baseUri);
-            throw new CustomException(NOT_ALLOWED_BASE_URI);
-        }
+        super.onAuthenticationSuccess(request, response, authentication);
     }
 }
