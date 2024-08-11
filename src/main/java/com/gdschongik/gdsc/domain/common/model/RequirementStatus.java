ff65ee9f12d3450b8bde4e76aa613c9641b3125e
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2024-08-11 21:04:11 +0900

    refactor: 승급조건 상태 enum 값을 UNSATISFIED로 변경 (#598)
    * refactor: 승급조건 enum을 PENDING에서 UNSATISFIED로 변경
    
    * refactor: 재학생 이메일 인증상태의 enum값을 UNSATISFIED로 변경
    
    * refactor: 변경된 값 메서드명에도 적용

--- src/main/java/com/gdschongik/gdsc/domain/common/model/RequirementStatus.java
+++ src/main/java/com/gdschongik/gdsc/domain/common/model/RequirementStatus.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum RequirementStatus {
-    PENDING("PENDING"),
+    UNSATISFIED("UNSATISFIED"),
     SATISFIED("SATISFIED");
 
     private final String value;
