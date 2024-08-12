2da5fcfa62e17fdd1e335375da042fd51d02b579
Hanbee Lee <bee1162@naver.com>
2024-08-12 21:43:31 +0900

    feat: format변환해서 응답 반환해주기

--- src/main/java/com/gdschongik/gdsc/domain/study/dto/response/SessionResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/dto/response/SessionResponse.java
@@ -1,20 +1,22 @@
 package com.gdschongik.gdsc.domain.study.dto.response;
 
-import com.gdschongik.gdsc.domain.recruitment.domain.vo.Period;
-import com.gdschongik.gdsc.domain.study.domain.Difficulty;
 import com.gdschongik.gdsc.domain.study.domain.StudyDetail;
 import com.gdschongik.gdsc.domain.study.domain.vo.Session;
+import java.time.format.DateTimeFormatter;
 
 public record SessionResponse(
-        Long studyDetailId, Period period, Long week, String title, String description, Difficulty difficulty) {
+        Long studyDetailId, String period, String week, String title, String description, String difficulty) {
+
+    // TODO 포매터 분리하기
     public static SessionResponse from(StudyDetail studyDetail) {
         Session session = studyDetail.getSession();
         return new SessionResponse(
                 studyDetail.getId(),
-                studyDetail.getPeriod(),
-                studyDetail.getWeek(),
+                studyDetail.getPeriod().getStartDate().format(DateTimeFormatter.ofPattern("MM.dd")) + "-"
+                        + studyDetail.getPeriod().getEndDate().format(DateTimeFormatter.ofPattern("MM.dd")),
+                studyDetail.getWeek().toString() + "주차",
                 session.getTitle(),
                 session.getDescription(),
-                session.getDifficulty());
+                session.getDifficulty().toString());
     }
 }
