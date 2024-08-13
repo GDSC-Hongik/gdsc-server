b1738b28d3d9d210ee419b63239eb100b44bfc9c
chosw1002@naver.com <chosw1002@naver.com>
2024-08-13 17:11:00 +0900

    refactor: 스터디 종류를 enum으로 반환하도록 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/dto/response/MentorStudyResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/dto/response/MentorStudyResponse.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.study.dto.response;
 
 import com.gdschongik.gdsc.domain.study.domain.Study;
+import com.gdschongik.gdsc.domain.study.domain.StudyType;
 import com.gdschongik.gdsc.global.util.formatter.SemesterFormatter;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -8,7 +9,7 @@ public record MentorStudyResponse(
         Long studyId,
         @Schema(description = "활동 학기") String semester,
         @Schema(description = "이름") String title,
-        @Schema(description = "종류") String studyType,
+        @Schema(description = "종류") StudyType studyType,
         @Schema(description = "상세설명 노션 링크") String notionLink,
         @Schema(description = "멘토 이름") String mentorName) {
 
@@ -17,7 +18,7 @@ public record MentorStudyResponse(
                 study.getId(),
                 SemesterFormatter.format(study.getAcademicYear(), study.getSemesterType()),
                 study.getTitle(),
-                study.getStudyType().getValue(),
+                study.getStudyType(),
                 study.getNotionLink(),
                 study.getMentor().getName());
     }
