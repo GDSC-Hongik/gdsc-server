90846e650bddb6950660f76d9eb2eb4f4846f747
Hanbee Lee <bee1162@naver.com>
2024-08-15 02:13:01 +0900

    feat: 디스코드 관련 명칭 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/dto/response/StudyStudentResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/dto/response/StudyStudentResponse.java
@@ -7,16 +7,16 @@ public record StudyStudentResponse(
         @Schema(description = "멤버 아이디") Long memberId,
         @Schema(description = "학생 이름") String name,
         @Schema(description = "학번") String studentId,
-        @Schema(description = "디스코드 사용자명") String discordName,
-        @Schema(description = "디스코드 닉네임") String discordUsername,
+        @Schema(description = "디스코드 사용자명") String discordUserName,
+        @Schema(description = "디스코드 닉네임") String nickname,
         @Schema(description = "깃허브 링크") String githubLink) {
     public static StudyStudentResponse from(StudyHistory studyHistory) {
         return new StudyStudentResponse(
                 studyHistory.getMentee().getId(),
                 studyHistory.getMentee().getName(),
                 studyHistory.getMentee().getStudentId(),
-                studyHistory.getMentee().getDiscordId(),
                 studyHistory.getMentee().getDiscordUsername(),
+                studyHistory.getMentee().getNickname(),
                 studyHistory.getRepositoryLink());
     }
 }
