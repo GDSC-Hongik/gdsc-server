2b11e711fe25b83f57f45e56363be1949768c0be
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-15 16:36:41 +0900

    feat: 깃허브 과제 경로 상수 추가

--- src/main/java/com/gdschongik/gdsc/global/common/constant/GithubConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/GithubConstant.java
@@ -3,6 +3,7 @@ package com.gdschongik.gdsc.global.common.constant;
 public class GithubConstant {
 
     public static final String GITHUB_DOMAIN = "github.com/";
+    public static final String GITHUB_ASSIGNMENT_PATH = "week%d/WIL.md";
 
     private GithubConstant() {}
 }
