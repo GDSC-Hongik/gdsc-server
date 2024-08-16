6aeede4a32c2b4634af44df13297f97c216d8983
Hanbee Lee <bee1162@naver.com>
2024-08-16 20:18:01 +0900

    feat: 변경된 매개변수 코드에 적용하기

--- src/main/java/com/gdschongik/gdsc/domain/study/application/MentorStudyService.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/application/MentorStudyService.java
@@ -38,7 +38,7 @@ public class MentorStudyService {
         Study study =
                 studyRepository.findById(studyId).orElseThrow(() -> new CustomException(ErrorCode.STUDY_NOT_FOUND));
 
-        studyValidator.validateStudyMentor(currentMember, study.getMentor().getId());
+        studyValidator.validateStudyMentor(currentMember, study);
         List<StudyHistory> studyHistories = studyHistoryRepository.findByStudyId(studyId);
 
         return studyHistories.stream().map(StudyStudentResponse::from).toList();
