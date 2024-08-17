6d2918dbbf0aeb8b1f3a68fe5568bc15be3a998a
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-18 01:57:02 +0900

    feat: 제출상태에서 PENDING 제거

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionStatus.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionStatus.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AssignmentSubmissionStatus {
-    PENDING("제출 전"),
     FAILURE("제출 실패"),
     SUCCESS("제출 성공");
 
