ca13976dc58106026a47ddd8b0ef16e451072316
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-18 20:30:35 +0900

    feat: 예외 시그니처 추가

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionFetchExecutor.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionFetchExecutor.java
@@ -1,7 +1,8 @@
 package com.gdschongik.gdsc.domain.study.domain;
 
+import com.gdschongik.gdsc.global.exception.CustomException;
+
 @FunctionalInterface
 public interface AssignmentSubmissionFetchExecutor {
-
-    AssignmentSubmission execute(String repo, int week);
+    AssignmentSubmission execute(String repo, int week) throws CustomException;
 }
--- src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionFetcher.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentSubmissionFetcher.java
@@ -1,7 +1,9 @@
 package com.gdschongik.gdsc.domain.study.domain;
 
+import com.gdschongik.gdsc.global.exception.CustomException;
+
 public record AssignmentSubmissionFetcher(String repo, int week, AssignmentSubmissionFetchExecutor fetchExecutor) {
-    public AssignmentSubmission fetch() {
+    public AssignmentSubmission fetch() throws CustomException {
         return fetchExecutor.execute(repo, week);
     }
 }
