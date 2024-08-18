5f5a3b67f8284a545be91ad862ca5a7bc22eac4c
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-18 18:33:22 +0900

    feat: 과제 채점 로직 구현

---
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/AssignmentHistoryGrader.java
@@ -0,0 +1,52 @@
+package com.gdschongik.gdsc.domain.study.domain;
+
+import static com.gdschongik.gdsc.domain.study.domain.SubmissionFailureType.*;
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
+
+import com.gdschongik.gdsc.global.annotation.DomainService;
+import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.exception.ErrorCode;
+import java.util.function.Supplier;
+
+@DomainService
+public class AssignmentHistoryGrader {
+
+    public void judge(
+            Supplier<AssignmentSubmission> assignmentSubmissionSupplier, AssignmentHistory assignmentHistory) {
+        try {
+            AssignmentSubmission assignmentSubmission = assignmentSubmissionSupplier.get();
+            judgeAssignmentSubmission(assignmentSubmission, assignmentHistory);
+        } catch (CustomException e) {
+            SubmissionFailureType failureType = translateException(e);
+            assignmentHistory.fail(failureType);
+        }
+    }
+
+    private void judgeAssignmentSubmission(
+            AssignmentSubmission assignmentSubmission, AssignmentHistory assignmentHistory) {
+        if (assignmentSubmission.contentLength() < 300) {
+            assignmentHistory.fail(WORD_COUNT_INSUFFICIENT);
+            return;
+        }
+
+        assignmentHistory.success(
+                assignmentSubmission.url(),
+                assignmentSubmission.commitHash(),
+                assignmentSubmission.contentLength(),
+                assignmentSubmission.committedAt());
+    }
+
+    private SubmissionFailureType translateException(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+
+        if (errorCode == GITHUB_CONTENT_NOT_FOUND) {
+            return LOCATION_UNIDENTIFIABLE;
+        }
+
+        if (errorCode == GITHUB_FILE_READ_FAILED || errorCode == GITHUB_COMMIT_DATE_FETCH_FAILED) {
+            return UNKNOWN;
+        }
+
+        throw e;
+    }
+}
