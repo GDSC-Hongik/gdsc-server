0fb75ea7cdb3e8e8b9b665f58c207c948cb0ba99
Jaehyun Ahn <desktop3711@gmail.com>
2024-08-18 20:37:14 +0900

    docs: 구현 관련 주석 추가

--- src/main/java/com/gdschongik/gdsc/infra/github/client/GithubClient.java
+++ src/main/java/com/gdschongik/gdsc/infra/github/client/GithubClient.java
@@ -4,6 +4,7 @@ import static com.gdschongik.gdsc.global.common.constant.GithubConstant.*;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.study.domain.AssignmentSubmission;
+import com.gdschongik.gdsc.domain.study.domain.AssignmentSubmissionFetchExecutor;
 import com.gdschongik.gdsc.domain.study.domain.AssignmentSubmissionFetcher;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import java.io.IOException;
@@ -31,6 +32,12 @@ public class GithubClient {
         }
     }
 
+    /**
+     * 직접 요청을 수행하는 대신, fetcher를 통해 요청을 수행합니다.
+     * 요청 수행 시 발생하는 예외의 경우 과제 채점에 사용되므로, 실제 요청은 채점 로직 내부에서 수행되어야 합니다.
+     * 따라서 지연 평가가 가능하도록 {@link AssignmentSubmissionFetchExecutor}를 인자로 받습니다.
+     * 또한, 인자로 전달된 repo와 week가 closure로 캡쳐되지 않도록 fetcher 내부에 컨텍스트로 저장합니다.
+     */
     public AssignmentSubmissionFetcher getLatestAssignmentSubmissionFetcher(String repo, int week) {
         return new AssignmentSubmissionFetcher(repo, week, this::getLatestAssignmentSubmission);
     }
