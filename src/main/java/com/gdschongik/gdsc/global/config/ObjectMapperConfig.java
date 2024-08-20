ed23977f367a7c3f123fd279b4191bedaa143c81
seulgi99 <tmfrk0426@gmail.com>
2024-08-20 23:58:10 +0900

    fix: Time관련 필드 Deserializer 맵퍼에 추가

--- src/main/java/com/gdschongik/gdsc/global/config/ObjectMapperConfig.java
+++ src/main/java/com/gdschongik/gdsc/global/config/ObjectMapperConfig.java
@@ -1,7 +1,19 @@
 package com.gdschongik.gdsc.global.config;
 
+import static com.gdschongik.gdsc.global.common.constant.RegexConstant.DATE;
+import static com.gdschongik.gdsc.global.common.constant.RegexConstant.DATETIME;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +22,42 @@ public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(LocalDate.class, new JsonDeserializer<LocalDate>() {
+            @Override
+            public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                    throws IOException {
+                return LocalDate.parse(jsonParser.getValueAsString(), DateTimeFormatter.ofPattern(DATE));
+            }
+        });
+
+        module.addDeserializer(LocalTime.class, new JsonDeserializer<LocalTime>() {
+            @Override
+            public LocalTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                    throws IOException {
+                // JSON 객체를 읽어들여 JsonNode로 변환
+                JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+
+                // JsonNode에서 값을 추출
+                int hour = node.get("hour").asInt();
+                int minute = node.get("minute").asInt();
+                int second = node.get("second").asInt();
+                int nano = node.get("nano").asInt();
+
+                // LocalTime 객체 생성
+                return LocalTime.of(hour, minute, second, nano);
+            }
+        });
+
+        module.addDeserializer(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+            @Override
+            public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                    throws IOException {
+                return LocalDateTime.parse(jsonParser.getValueAsString(), DateTimeFormatter.ofPattern(DATETIME));
+            }
+        });
+
+        mapper.registerModule(module);
         return mapper;
     }
 }
