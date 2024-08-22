83bbc1b356f5607aaf2bb82c7ba24564ce8c983b
chosw1002@naver.com <chosw1002@naver.com>
2024-08-22 22:34:25 +0900

    remove: 포맷팅 로직 제거

--- src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberAccountInfoResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/dto/response/MemberAccountInfoResponse.java
@@ -1,12 +1,9 @@
 package com.gdschongik.gdsc.domain.member.dto.response;
 
-import static com.gdschongik.gdsc.global.common.constant.GithubConstant.*;
-
 import com.gdschongik.gdsc.domain.member.domain.Member;
 
-public record MemberAccountInfoResponse(String name, String githubHandle, String githubLink) {
+public record MemberAccountInfoResponse(String name, String githubHandle) {
     public static MemberAccountInfoResponse of(Member member, String githubHandle) {
-        return new MemberAccountInfoResponse(
-                member.getName(), githubHandle, GITHUB_PROFILE_URL.formatted(githubHandle));
+        return new MemberAccountInfoResponse(member.getName(), githubHandle);
     }
 }
--- src/main/java/com/gdschongik/gdsc/global/common/constant/GithubConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/GithubConstant.java
@@ -3,7 +3,6 @@ package com.gdschongik.gdsc.global.common.constant;
 public class GithubConstant {
 
     public static final String GITHUB_DOMAIN = "github.com/";
-    public static final String GITHUB_PROFILE_URL = "https://github.com/%s";
     public static final String GITHUB_ASSIGNMENT_PATH = "week%d/WIL.md";
     public static final String GITHUB_USER_API_URL = "https://api.github.com/user/%s";
 
