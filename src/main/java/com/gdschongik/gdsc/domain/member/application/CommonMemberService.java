5095b3d5b11c0b70b59e8dd3c6ad2f7ae7e377e5
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-08-23 23:47:27 +0900

    feat: 마이페이지 계정정보 조회 API 추가 (#672)
    * feat: 마이페이지 계정정보 조회 api 추가
    
    * remove: 포맷팅 로직 제거
    
    * rename: 요청 클래스 이름 변경
    
    * fix: 커스텀 예외로 변경
    
    * refactor: DI 대신 직접 초기화

--- src/main/java/com/gdschongik/gdsc/domain/member/application/CommonMemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/CommonMemberService.java
@@ -5,10 +5,13 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Department;
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.member.dto.response.MemberAccountInfoResponse;
 import com.gdschongik.gdsc.domain.member.dto.response.MemberDepartmentResponse;
 import com.gdschongik.gdsc.domain.membership.dao.MembershipRepository;
 import com.gdschongik.gdsc.domain.membership.domain.Membership;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.util.MemberUtil;
+import com.gdschongik.gdsc.infra.github.client.GithubClient;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,8 @@ public class CommonMemberService {
 
     private final MembershipRepository membershipRepository;
     private final MemberRepository memberRepository;
+    private final MemberUtil memberUtil;
+    private final GithubClient githubClient;
 
     @Transactional(readOnly = true)
     public List<MemberDepartmentResponse> getDepartments() {
@@ -80,5 +85,12 @@ public class CommonMemberService {
         memberRepository.save(member);
 
         log.info("[CommonMemberService] 준회원 강등 완료: memberId={}", member.getId());
+    }
+
+    @Transactional(readOnly = true)
+    public MemberAccountInfoResponse getAccountInfo() {
+        Member currentMember = memberUtil.getCurrentMember();
+        String githubHandle = githubClient.getGithubHandle(currentMember.getOauthId());
+        return MemberAccountInfoResponse.of(currentMember, githubHandle);
     }
 }
