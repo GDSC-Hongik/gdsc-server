44b7ffb15dde9b8a02ddccbf21e8838d0deeedf4
Hanbee Lee <bee1162@naver.com>
2024-08-27 23:37:10 +0900

    feat: 스터디 상태 세션에서 오프라인으로 변경

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyType.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyType.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum StudyType {
     ASSIGNMENT("과제 스터디"),
-    ONLINE("온라인 세션"),
-    OFFLINE("오프라인 세션");
+    ONLINE("온라인 커리큘럼"),
+    OFFLINE("오프라인 커리큘럼");
 
     private final String value;
 }
