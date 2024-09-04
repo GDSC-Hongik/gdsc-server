a849f84ef3456ba3b6dd9ae89a35a945098c7b50
Jaehyun Ahn <desktop3711@gmail.com>
2024-09-04 22:08:46 +0900

    feat: 이벤트 핸들러 및 공통 스터디 서비스 로직 추가

--- src/main/java/com/gdschongik/gdsc/domain/study/application/CommonStudyService.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/application/CommonStudyService.java
@@ -1,8 +1,10 @@
 package com.gdschongik.gdsc.domain.study.application;
 
-import static com.gdschongik.gdsc.global.exception.ErrorCode.STUDY_NOT_FOUND;
+import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.study.dao.AssignmentHistoryRepository;
+import com.gdschongik.gdsc.domain.study.dao.AttendanceRepository;
 import com.gdschongik.gdsc.domain.study.dao.StudyAnnouncementRepository;
 import com.gdschongik.gdsc.domain.study.dao.StudyHistoryRepository;
 import com.gdschongik.gdsc.domain.study.dao.StudyRepository;
@@ -29,6 +31,8 @@ public class CommonStudyService {
     private final StudyRepository studyRepository;
     private final StudyHistoryRepository studyHistoryRepository;
     private final StudyAnnouncementRepository studyAnnouncementRepository;
+    private final AttendanceRepository attendanceRepository;
+    private final AssignmentHistoryRepository assignmentHistoryRepository;
     private final MemberUtil memberUtil;
     private final StudyValidator studyValidator;
 
@@ -50,5 +54,27 @@ public class CommonStudyService {
                 studyAnnouncementRepository.findAllByStudyIdOrderByCreatedAtDesc(studyId);
 
         return studyAnnouncements.stream().map(StudyAnnouncementResponse::from).toList();
+    }
+
+    /**
+     * 이벤트 핸들러에서 사용되므로, `@Transactional` 을 사용하지 않습니다.
+     */
+    public void deleteAttendanceByStudyHistory(Long studyHistoryId) {
+        StudyHistory studyHistory = studyHistoryRepository
+                .findById(studyHistoryId)
+                .orElseThrow(() -> new CustomException(STUDY_HISTORY_NOT_FOUND));
+
+        attendanceRepository.deleteByStudyHistory(studyHistory);
+    }
+
+    /**
+     * 이벤트 핸들러에서 사용되므로, `@Transactional` 을 사용하지 않습니다.
+     */
+    public void deleteAssignmentHistoryByStudyHistory(Long studyHistoryId) {
+        StudyHistory studyHistory = studyHistoryRepository
+                .findById(studyHistoryId)
+                .orElseThrow(() -> new CustomException(STUDY_HISTORY_NOT_FOUND));
+
+        assignmentHistoryRepository.deleteByStudyHistory(studyHistory);
     }
 }
