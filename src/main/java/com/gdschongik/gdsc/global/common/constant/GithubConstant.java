f1bdbbdcdb5b31d4e0fe4b5ac39d925796fa923e
Jaehyun Ahn <desktop3711@gmail.com>
2024-09-04 20:58:01 +0900

    feat: wil.md로 변경

--- src/main/java/com/gdschongik/gdsc/global/common/constant/GithubConstant.java
+++ src/main/java/com/gdschongik/gdsc/global/common/constant/GithubConstant.java
@@ -3,7 +3,7 @@ package com.gdschongik.gdsc.global.common.constant;
 public class GithubConstant {
 
     public static final String GITHUB_DOMAIN = "github.com/";
-    public static final String GITHUB_ASSIGNMENT_PATH = "week%d/WIL.md";
+    public static final String GITHUB_ASSIGNMENT_PATH = "week%d/wil.md";
     public static final String GITHUB_USER_API_URL = "https://api.github.com/user/%s";
 
     private GithubConstant() {}
