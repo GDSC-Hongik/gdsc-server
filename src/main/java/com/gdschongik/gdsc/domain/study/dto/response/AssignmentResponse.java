bb700ffb7d1d327909c23962d3322890575bc54d
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2024-09-04 20:42:06 +0900

    feat: 과제 응답 DTO에 커리큘럼 시작일 추가 (#753)
    feat: 과제 응답 dto에 커리큘럼 시작일 추가

--- src/main/java/com/gdschongik/gdsc/domain/study/dto/response/AssignmentResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/dto/response/AssignmentResponse.java
@@ -5,6 +5,7 @@ import com.gdschongik.gdsc.domain.study.domain.StudyStatus;
 import com.gdschongik.gdsc.domain.study.domain.vo.Assignment;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record AssignmentResponse(
         Long studyDetailId,
@@ -13,7 +14,8 @@ public record AssignmentResponse(
         @Schema(description = "마감 기한") LocalDateTime deadline,
         @Schema(description = "주차") Long week,
         @Schema(description = "과제 명세 링크") String descriptionLink,
-        @Schema(description = "과제 상태") StudyStatus assignmentStatus) {
+        @Schema(description = "과제 상태") StudyStatus assignmentStatus,
+        @Schema(description = "커리큘럼 시작일") LocalTime curriculumStartAt) {
     public static AssignmentResponse from(StudyDetail studyDetail) {
         Assignment assignment = studyDetail.getAssignment();
         return new AssignmentResponse(
@@ -23,6 +25,7 @@ public record AssignmentResponse(
                 assignment.getDeadline(),
                 studyDetail.getWeek(),
                 assignment.getDescriptionLink(),
-                assignment.getStatus());
+                assignment.getStatus(),
+                studyDetail.getCurriculum().getStartAt());
     }
 }
