339fb9e6841cc0b06368dfb6e3edfed60d07e6df
chosw1002@naver.com <chosw1002@naver.com>
2024-09-04 23:43:24 +0900

    docs: 스웨거 설명 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/dto/response/AssignmentResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/dto/response/AssignmentResponse.java
@@ -14,7 +14,7 @@ public record AssignmentResponse(
         @Schema(description = "주차") Long week,
         @Schema(description = "과제 명세 링크") String descriptionLink,
         @Schema(description = "과제 상태") StudyStatus assignmentStatus,
-        @Schema(description = "커리큘럼 시작일") LocalDateTime studyDetailStartDate) {
+        @Schema(description = "주차 시작일") LocalDateTime studyDetailStartDate) {
     public static AssignmentResponse from(StudyDetail studyDetail) {
         Assignment assignment = studyDetail.getAssignment();
         return new AssignmentResponse(
