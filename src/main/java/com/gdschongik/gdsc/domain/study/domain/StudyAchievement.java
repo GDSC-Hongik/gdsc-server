d9eeb30b97275d40f5941807038e6f3764e0e743
chosw1002@naver.com <chosw1002@naver.com>
2024-09-25 14:09:38 +0900

    feat: unique constraint 추가

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyAchievement.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyAchievement.java
@@ -11,6 +11,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"member_id", "study_id", "achievement_type"})})
 public class StudyAchievement {
 
     @Id
