adba316b21b58366cdd2f6315a13ab839ffb9fe4
kckc0608 <kckc0608@naver.com>
2024-10-15 17:35:28 +0900

    feat: 통계 조회 로직 작성

--- src/main/java/com/gdschongik/gdsc/domain/study/application/MentorStudyDetailService.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/application/MentorStudyDetailService.java
@@ -3,13 +3,10 @@ package com.gdschongik.gdsc.domain.study.application;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
-import com.gdschongik.gdsc.domain.study.dao.StudyDetailRepository;
-import com.gdschongik.gdsc.domain.study.domain.StudyDetail;
-import com.gdschongik.gdsc.domain.study.domain.StudyDetailValidator;
+import com.gdschongik.gdsc.domain.study.dao.*;
+import com.gdschongik.gdsc.domain.study.domain.*;
 import com.gdschongik.gdsc.domain.study.dto.request.AssignmentCreateUpdateRequest;
-import com.gdschongik.gdsc.domain.study.dto.response.AssignmentResponse;
-import com.gdschongik.gdsc.domain.study.dto.response.StudyCurriculumResponse;
-import com.gdschongik.gdsc.domain.study.dto.response.StudyMentorAttendanceResponse;
+import com.gdschongik.gdsc.domain.study.dto.response.*;
 import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.global.util.MemberUtil;
 import java.time.LocalDate;
@@ -27,6 +24,11 @@ public class MentorStudyDetailService {
     private final MemberUtil memberUtil;
     private final StudyDetailRepository studyDetailRepository;
     private final StudyDetailValidator studyDetailValidator;
+    private final StudyHistoryRepository studyHistoryRepository;
+    private final AttendanceRepository attendanceRepository;
+    private final AssignmentHistoryRepository assignmentHistoryRepository;
+    private final StudyValidator studyValidator;
+    private final StudyRepository studyRepository;
 
     @Transactional(readOnly = true)
     public List<AssignmentResponse> getWeeklyAssignments(Long studyId) {
@@ -107,5 +109,90 @@ public class MentorStudyDetailService {
                 .map(StudyMentorAttendanceResponse::from)
                 .limit(2)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public StudyStatisticsResponse getStudyStatistics(Long studyId) {
+        Member currentMember = memberUtil.getCurrentMember();
+        Study study = studyRepository.findById(studyId).orElseThrow(() -> new CustomException(STUDY_NOT_FOUND));
+        studyValidator.validateStudyMentor(currentMember, study);
+
+        List<StudyHistory> studyHistories = studyHistoryRepository.findAllByStudyId(studyId);
+        long wholeStudentCount = studyHistories.size();
+        long studyCompleteStudentCount =
+                studyHistories.stream().filter(StudyHistory::isComplete).count();
+
+        List<StudyDetail> studyDetails = studyDetailRepository.findAllByStudyIdOrderByWeekAsc(studyId);
+        long openedWeekCount = studyDetails.stream()
+                .filter(studyDetail -> studyDetail.getCurriculum().isOpen())
+                .count();
+
+        List<StudyWeekStatisticsResponse> studyWeekStatisticsResponses =
+                calculateStudyWeekStatistics(studyDetails, wholeStudentCount);
+
+        long averageAttendanceRate = calculateAverageWeekAttendanceRate(studyWeekStatisticsResponses, openedWeekCount);
+        long averageAssignmentSubmitRate =
+                calculateAverageWeekAssignmentSubmitRate(studyWeekStatisticsResponses, openedWeekCount);
+
+        return StudyStatisticsResponse.of(
+                wholeStudentCount,
+                studyCompleteStudentCount,
+                averageAttendanceRate,
+                averageAssignmentSubmitRate,
+                studyWeekStatisticsResponses);
+    }
+
+    private List<StudyWeekStatisticsResponse> calculateStudyWeekStatistics(
+            List<StudyDetail> studyDetails, Long wholeStudentCount) {
+
+        return studyDetails.stream()
+                .map((studyDetail -> {
+                    if (!studyDetail.getCurriculum().isOpen()) {
+                        return StudyWeekStatisticsResponse.createCanceledWeekStatistics(studyDetail.getWeek());
+                    }
+
+                    if (wholeStudentCount == 0) {
+                        return StudyWeekStatisticsResponse.createOpenedWeekStatistics(studyDetail.getWeek(), 0L, 0L);
+                    }
+
+                    long attendanceCount = attendanceRepository.countByStudyDetailId(studyDetail.getId());
+                    long assignmentCount = assignmentHistoryRepository.countByStudyDetailId(studyDetail.getId());
+
+                    return StudyWeekStatisticsResponse.createOpenedWeekStatistics(
+                            studyDetail.getWeek(),
+                            Math.round(attendanceCount / (double) wholeStudentCount * 100),
+                            Math.round(assignmentCount / (double) wholeStudentCount * 100));
+                }))
+                .toList();
+    }
+
+    private long calculateAverageWeekAttendanceRate(
+            List<StudyWeekStatisticsResponse> studyWeekStatisticsResponses, long openedWeekCount) {
+
+        if (openedWeekCount == 0) {
+            return 0;
+        }
+
+        long attendanceRateSum = studyWeekStatisticsResponses.stream()
+                .mapToLong(
+                        weekStatistics -> weekStatistics.isCanceledWeek() ? 0 : weekStatistics.attendanceRate())
+                .sum();
+
+        return Math.round(attendanceRateSum / (double) openedWeekCount * 100);
+    }
+
+    private long calculateAverageWeekAssignmentSubmitRate(
+            List<StudyWeekStatisticsResponse> studyWeekStatisticsResponses, long openedWeekCount) {
+
+        if (openedWeekCount == 0) {
+            return 0;
+        }
+
+        long assignmentSubmitRateSum = studyWeekStatisticsResponses.stream()
+                        .mapToLong(weekStatistics ->
+                                weekStatistics.isCanceledWeek() ? 0 : weekStatistics.assignmentSubmitRate())
+                        .sum();
+
+        return Math.round(assignmentSubmitRateSum / (double) openedWeekCount * 100);
     }
 }
