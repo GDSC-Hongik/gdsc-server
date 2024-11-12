269c7668f22df2b033494cec780f8685f2d144ee
chosw1002@naver.com <chosw1002@naver.com>
2024-11-12 21:15:16 +0900

    fix: post 요청으로 변경

--- src/main/java/com/gdschongik/gdsc/domain/study/api/MentorStudyHistoryController.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/api/MentorStudyHistoryController.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,14 +20,14 @@ public class MentorStudyHistoryController {
     private final MentorStudyHistoryService mentorStudyHistoryService;
 
     @Operation(summary = "스터디 수료 처리", description = "스터디 수료 처리합니다.")
-    @PatchMapping("/complete")
+    @PostMapping("/complete")
     public ResponseEntity<Void> completeStudy(@RequestBody StudyCompleteRequest request) {
         mentorStudyHistoryService.completeStudy(request);
         return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "스터디 수료 철회", description = "스터디 수료 처리를 철회합니다.")
-    @PatchMapping("/withdraw-completion")
+    @PostMapping("/withdraw-completion")
     public ResponseEntity<Void> withdrawStudyCompletion(@RequestBody StudyCompleteRequest request) {
         mentorStudyHistoryService.withdrawStudyCompletion(request);
         return ResponseEntity.ok().build();
