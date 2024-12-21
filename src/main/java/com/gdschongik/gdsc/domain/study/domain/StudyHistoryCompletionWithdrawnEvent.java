252019d5ce074fbc280b015e873da448b9749d94
Jaehyun Ahn <desktop3711@gmail.com>
2024-12-22 01:56:29 +0900

    docs: 오타 수정

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyHistoryCompletionWithdrawnEvent.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyHistoryCompletionWithdrawnEvent.java
@@ -1,5 +1,5 @@
 package com.gdschongik.gdsc.domain.study.domain;
 
 public record StudyHistoryCompletionWithdrawnEvent(long studyHistoryId) {
-    // TODO: 이벤트 내부 필드의 식별자 값은 항상 not null이므로, primitive 타입으로 사용하도록 변경1
+    // TODO: 이벤트 내부 필드의 식별자 값은 항상 not null이므로, primitive 타입으로 사용하도록 변경
 }
