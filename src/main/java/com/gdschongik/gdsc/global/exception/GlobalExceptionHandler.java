a12c395f85835569529f71301a015162f964c90c
Everdu <kckc0608@gmail.com>
2025-01-14 14:34:18 +0900

    chore: log level info 로 변경 및 stack trace 제거 (#838)

--- src/main/java/com/gdschongik/gdsc/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gdschongik/gdsc/global/exception/GlobalExceptionHandler.java
@@ -17,19 +17,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        log.error("CustomException : {}", e.getMessage(), e);
+        log.info("CustomException : {}", e.getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(ErrorResponse.of(e.getErrorCode()));
     }
 
     @ExceptionHandler(CustomPaymentException.class)
     public ResponseEntity<ErrorResponse> handleCustomPaymentException(CustomPaymentException e) {
-        log.error("CustomPaymentException : {}, {}", e.getCode(), e.getMessage());
+        log.info("CustomPaymentException : {}, {}", e.getCode(), e.getMessage());
         return ResponseEntity.status(e.getStatus()).body(ErrorResponse.of(e.getCode(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("INTERNAL_SERVER_ERROR : {}", e.getMessage(), e);
+        log.error("INTERNAL_SERVER_ERROR : {}", e.getMessage());
         return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
                 .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
     }
@@ -37,7 +37,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        log.error("METHOD_ARGUMENT_NOT_VALID : {}", e.getMessage(), e);
+        log.info("METHOD_ARGUMENT_NOT_VALID : {}", e.getMessage());
         String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         return ResponseEntity.status(status.value())
                 .body(ErrorResponse.of(ErrorCode.METHOD_ARGUMENT_NOT_VALID, errorMessage));
