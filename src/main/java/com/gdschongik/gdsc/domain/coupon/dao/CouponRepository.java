d2df1e2db76a935312729422fd04633704455148
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2025-01-28 20:59:34 +0900

    feat: 스터디 수료 쿠폰을 스터디 당 하나만 가지도록 변경 (#851)
    * feat: 스터디 수료 쿠폰을 스터디 당 하나만 가지도록 변경
    
    * chore: todo 추가
    
    * feat: 쿠폰 unique constraint 추가

--- src/main/java/com/gdschongik/gdsc/domain/coupon/dao/CouponRepository.java
+++ src/main/java/com/gdschongik/gdsc/domain/coupon/dao/CouponRepository.java
@@ -1,6 +1,11 @@
 package com.gdschongik.gdsc.domain.coupon.dao;
 
 import com.gdschongik.gdsc.domain.coupon.domain.Coupon;
+import com.gdschongik.gdsc.domain.coupon.domain.CouponType;
+import com.gdschongik.gdsc.domain.study.domain.Study;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CouponRepository extends JpaRepository<Coupon, Long> {}
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Optional<Coupon> findByCouponTypeAndStudy(CouponType couponType, Study study);
+}
