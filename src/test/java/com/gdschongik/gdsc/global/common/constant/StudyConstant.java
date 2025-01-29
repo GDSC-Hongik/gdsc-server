87caf45d6501ef18ffde69444121a61d2f9e83be
Jaehyun Ahn <desktop3711@gmail.com>
2025-01-29 21:46:27 +0900

    feat: V2 전용 상수 추가

--- src/test/java/com/gdschongik/gdsc/global/common/constant/StudyConstant.java
+++ src/test/java/com/gdschongik/gdsc/global/common/constant/StudyConstant.java
@@ -1,6 +1,8 @@
 package com.gdschongik.gdsc.global.common.constant;
 
+import com.gdschongik.gdsc.domain.common.model.SemesterType;
 import com.gdschongik.gdsc.domain.common.vo.Period;
+import com.gdschongik.gdsc.domain.common.vo.Semester;
 import com.gdschongik.gdsc.domain.study.domain.StudyType;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
@@ -10,12 +12,20 @@ public class StudyConstant {
     private StudyConstant() {}
 
     public static final String STUDY_TITLE = "스터디 제목";
+    public static final String STUDY_DESCRIPTION = "스터디 설명";
+    public static final String STUDY_DESCRIPTION_NOTION_LINK = "https://gdschongik.com/2025-1-backend-study";
+    public static final Semester STUDY_SEMESTER = Semester.of(2025, SemesterType.FIRST);
     public static final Long TOTAL_WEEK = 8L;
+    public static final int TOTAL_ROUND = 8;
     public static final StudyType ONLINE_STUDY = StudyType.ONLINE;
     public static final StudyType ASSIGNMENT_STUDY = StudyType.ASSIGNMENT;
     public static final DayOfWeek DAY_OF_WEEK = DayOfWeek.FRIDAY;
     public static final LocalTime STUDY_START_TIME = LocalTime.of(19, 0, 0);
     public static final LocalTime STUDY_END_TIME = LocalTime.of(20, 0, 0);
+    public static final Period STUDY_APPLICATION_PERIOD =
+            Period.of(LocalDateTime.of(2025, 3, 1, 0, 0), LocalDateTime.of(2025, 3, 14, 23, 59));
+    public static final String STUDY_DISCORD_CHANNEL_ID = "12345678";
+    public static final String STUDY_DISCORD_ROLE_ID = "12345678";
 
     // StudyDetail
     public static final String ATTENDANCE_NUMBER = "1234";
