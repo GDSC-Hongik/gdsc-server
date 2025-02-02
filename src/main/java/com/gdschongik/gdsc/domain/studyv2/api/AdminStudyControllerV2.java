1f4bc902c0f056b151d0730430a2c15496f79059
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2025-02-02 19:36:19 +0900

    feat: 스터디 개설 V2 API 구현 (#863)
    * feat: 랜덤 출석번호 생성기를 컴포넌트로 등록
    
    * feat: 스터디 개설 v2 API 구현
    
    * test: 스터디 개설 v2 테스트 추가
    
    * feat: 스터디회차 페치조인해서 조회하는 메서드 추가
    
    * fix: 지연로딩 문제로 인한 페치조인 조회하는 메서드 테스트에 적용
    
    * fix: 누락된 where절 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/AdminStudyControllerV2.java
@@ -0,0 +1,29 @@
+package com.gdschongik.gdsc.domain.studyv2.api;
+
+import com.gdschongik.gdsc.domain.studyv2.application.AdminStudyServiceV2;
+import com.gdschongik.gdsc.domain.studyv2.dto.request.StudyCreateRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Admin Study V2", description = "스터디 V2 어드민 API입니다.")
+@RestController
+@RequestMapping("/admin/studies/v2")
+@RequiredArgsConstructor
+public class AdminStudyControllerV2 {
+
+    private final AdminStudyServiceV2 adminStudyServiceV2;
+
+    @Operation(summary = "스터디 개설", description = "스터디를 개설합니다. 빈 스터디회차를 함께 생성합니다.")
+    @PostMapping
+    public ResponseEntity<Void> createStudy(@Valid @RequestBody StudyCreateRequest request) {
+        adminStudyServiceV2.createStudy(request);
+        return ResponseEntity.ok().build();
+    }
+}
