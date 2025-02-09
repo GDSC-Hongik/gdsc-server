a3995752b2a5d26b7addf30e47cc46dd1a34e1b7
Jaehyun Ahn <desktop3711@gmail.com>
2025-02-09 19:08:57 +0900

    feat: DTO 정적 팩토리 추가

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/dto/StudyManagerDto.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/dto/StudyManagerDto.java
@@ -3,6 +3,7 @@ package com.gdschongik.gdsc.domain.studyv2.dto.dto;
 import com.gdschongik.gdsc.domain.common.vo.Period;
 import com.gdschongik.gdsc.domain.common.vo.Semester;
 import com.gdschongik.gdsc.domain.study.domain.StudyType;
+import com.gdschongik.gdsc.domain.studyv2.domain.StudyV2;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
@@ -23,4 +24,22 @@ public record StudyManagerDto(
         Period applicationPeriod,
         String discordChannelId,
         String discordRoleId,
-        Long mentorId) {}
+        Long mentorId) {
+    public static StudyManagerDto from(StudyV2 study) {
+        return new StudyManagerDto(
+                study.getId(),
+                study.getType(),
+                study.getTitle(),
+                study.getDescription(),
+                study.getDescriptionNotionLink(),
+                study.getSemester(),
+                study.getTotalRound(),
+                study.getDayOfWeek(),
+                study.getStartTime(),
+                study.getEndTime(),
+                study.getApplicationPeriod(),
+                study.getDiscordChannelId(),
+                study.getDiscordRoleId(),
+                study.getMentor().getId());
+    }
+}
--- src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/dto/StudySessionManagerDto.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/dto/StudySessionManagerDto.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.studyv2.dto.dto;
 
 import com.gdschongik.gdsc.domain.common.vo.Period;
+import com.gdschongik.gdsc.domain.studyv2.domain.StudySessionV2;
 
 /**
  * 스터디 회차 관리자 DTO입니다. 출결번호가 포함되어 있습니다.
@@ -14,4 +15,17 @@ public record StudySessionManagerDto(
         Period lessonPeriod,
         String assignmentDescriptionLink,
         Period assignmentPeriod,
-        Long studyId) {}
+        Long studyId) {
+    public static StudySessionManagerDto from(StudySessionV2 studySession) {
+        return new StudySessionManagerDto(
+                studySession.getId(),
+                studySession.getPosition(),
+                studySession.getTitle(),
+                studySession.getDescription(),
+                studySession.getLessonAttendanceNumber(),
+                studySession.getLessonPeriod(),
+                studySession.getAssignmentDescriptionLink(),
+                studySession.getAssignmentPeriod(),
+                studySession.getStudyV2().getId());
+    }
+}
