5b80d5b18b6c6d0f8e13373c1c294c0882d34726
Jaehyun Ahn <desktop3711@gmail.com>
2025-02-11 21:21:00 +0900

    feat: 수정 요청 DTO에 커맨드로 매핑하는 로직 추가

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/request/StudyUpdateRequest.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/request/StudyUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.studyv2.dto.request;
 
 import com.gdschongik.gdsc.domain.common.vo.Period;
+import com.gdschongik.gdsc.domain.studyv2.domain.StudyUpdateCommand;
 import jakarta.annotation.Nullable;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
@@ -21,4 +22,25 @@ public record StudyUpdateRequest(
             Period lessonPeriod,
             @Nullable String assignmentDescriptionLink,
             @Nullable Period assignmentPeriod) {}
+
+    public static StudyUpdateCommand toCommand(StudyUpdateRequest request) {
+        var sessionCommands = request.studySessions().stream()
+                .map(studySession -> new StudyUpdateCommand.Session(
+                        studySession.studySessionId(),
+                        studySession.title(),
+                        studySession.description(),
+                        studySession.lessonPeriod(),
+                        studySession.assignmentDescriptionLink(),
+                        studySession.assignmentPeriod()))
+                .toList();
+
+        return new StudyUpdateCommand(
+                request.title(),
+                request.description(),
+                request.descriptionNotionLink(),
+                request.dayOfWeek(),
+                request.startTime(),
+                request.endTime(),
+                sessionCommands);
+    }
 }
