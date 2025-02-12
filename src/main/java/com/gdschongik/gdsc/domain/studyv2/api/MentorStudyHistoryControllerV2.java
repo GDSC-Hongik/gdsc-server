c4694dcc30e84dda2c911ca209162e3f5c26ccf4
chosw1002@naver.com <chosw1002@naver.com>
2025-02-12 21:16:53 +0900

    docs: description 수정

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/api/MentorStudyHistoryControllerV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/MentorStudyHistoryControllerV2.java
@@ -19,14 +19,14 @@ public class MentorStudyHistoryControllerV2 {
 
     private final MentorStudyHistoryServiceV2 mentorStudyHistoryServiceV2;
 
-    @Operation(summary = "스터디 수료 처리", description = "스터디 수료 처리합니다.")
+    @Operation(summary = "스터디 수료 처리", description = "스터디 수료 처리하고 스터디 수료 발급쿠폰을 발급합니다.")
     @PostMapping("/complete")
     public ResponseEntity<Void> completeStudy(@RequestBody StudyCompleteRequest request) {
         mentorStudyHistoryServiceV2.completeStudy(request);
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "스터디 수료 철회", description = "스터디 수료 처리를 철회합니다.")
+    @Operation(summary = "스터디 수료 철회", description = "스터디 수료 처리를 철회하고 스터디 수료 발급쿠폰을 회수합니다.")
     @PostMapping("/withdraw-completion")
     public ResponseEntity<Void> withdrawStudyCompletion(@RequestBody StudyCompleteRequest request) {
         mentorStudyHistoryServiceV2.withdrawStudyCompletion(request);
