e979b76a2235100ee6fa0b4479c357f70e8b929c
chosw1002@naver.com <chosw1002@naver.com>
2025-02-12 21:28:52 +0900

    feat: 역할 검증 추가

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyValidatorV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyValidatorV2.java
@@ -10,6 +10,16 @@ import com.gdschongik.gdsc.global.exception.CustomException;
 public class StudyValidatorV2 {
 
     public void validateStudyMentor(Member currentMember, StudyV2 study) {
+        // 어드민인 경우 검증 통과
+        if (currentMember.isAdmin()) {
+            return;
+        }
+
+        // 멘토인지 검증
+        if (!currentMember.isMentor()) {
+            throw new CustomException(STUDY_ACCESS_NOT_ALLOWED);
+        }
+
         // 해당 스터디의 담당 멘토인지 검증
         if (!currentMember.getId().equals(study.getMentor().getId())) {
             throw new CustomException(STUDY_MENTOR_INVALID);
