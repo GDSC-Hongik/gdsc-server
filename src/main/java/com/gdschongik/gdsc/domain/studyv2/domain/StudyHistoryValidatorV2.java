850990f8ce0a96cc9a6c54925699cbd53dfaecfc
Jaehyun Ahn <desktop3711@gmail.com>
2025-02-22 21:27:17 +0900

    refactor: null-satefy한 비교를 위해 `Objects.equals` 사용

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyHistoryValidatorV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyHistoryValidatorV2.java
@@ -5,6 +5,7 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import java.util.Objects;
 
 @DomainService
 public class StudyHistoryValidatorV2 {
@@ -19,7 +20,7 @@ public class StudyHistoryValidatorV2 {
         // 레포지토리 소유자가 현 멤버가 아닌 경우
         String currentMemberGithubId = currentMember.getOauthId();
 
-        if (!repositoryOwnerGithubId.equals(currentMemberGithubId)) {
+        if (!Objects.equals(repositoryOwnerGithubId, currentMemberGithubId)) {
             throw new CustomException(STUDY_HISTORY_REPOSITORY_NOT_UPDATABLE_OWNER_MISMATCH);
         }
     }
