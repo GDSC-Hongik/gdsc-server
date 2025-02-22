903e0973d9a9b5822ccc3d1bced631f25fe21d01
Jaehyun Ahn <desktop3711@gmail.com>
2025-02-22 21:23:46 +0900

    feat: 레포지터리 소유자 검증 로직 추가

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyHistoryValidatorV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyHistoryValidatorV2.java
@@ -2,6 +2,7 @@ package com.gdschongik.gdsc.domain.studyv2.domain;
 
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
+import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
 
@@ -11,6 +12,15 @@ public class StudyHistoryValidatorV2 {
     public void validateAppliedToStudy(long countStudyHistory, int studentCount) {
         if (countStudyHistory != studentCount) {
             throw new CustomException(STUDY_HISTORY_NOT_APPLIED_STUDENT_EXISTS);
+        }
+    }
+
+    public void validateUpdateRepository(String repositoryOwnerGithubId, Member currentMember) {
+        // 레포지토리 소유자가 현 멤버가 아닌 경우
+        String currentMemberGithubId = currentMember.getOauthId();
+
+        if (!repositoryOwnerGithubId.equals(currentMemberGithubId)) {
+            throw new CustomException(STUDY_HISTORY_REPOSITORY_NOT_UPDATABLE_OWNER_MISMATCH);
         }
     }
 }
