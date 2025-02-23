76ff56371dcd97e3e86a7493be771ef9876d138a
Jaehyun Ahn <91878695+uwoobeat@users.noreply.github.com>
2025-02-23 20:21:54 +0900

    feat: 스터디 수강이력 레포지터리 입력 API 구현 (#911)
    * feat: 레포지터리 소유자 ID 조회 기능 추가
    
    * feat: 학생 및 스터디로 수강이력 조회 메서드 추가
    
    * feat: 레포지터리 소유자 검증 로직 추가
    
    * feat: 레포지터리 입력 기능 구현
    
    * refactor: null-satefy한 비교를 위해 `Objects.equals` 사용
    
    * refactor: 엔드포인트 및 요청 방식 수정
    
    * fix: 오타 수정
    
    * refactor: 메서드명 수정
    
    * docs: 로그 수정

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyHistoryValidatorV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyHistoryValidatorV2.java
@@ -2,8 +2,10 @@ package com.gdschongik.gdsc.domain.studyv2.domain;
 
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
+import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.annotation.DomainService;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import java.util.Objects;
 
 @DomainService
 public class StudyHistoryValidatorV2 {
@@ -11,6 +13,15 @@ public class StudyHistoryValidatorV2 {
     public void validateAppliedToStudy(long countStudyHistory, int studentCount) {
         if (countStudyHistory != studentCount) {
             throw new CustomException(STUDY_HISTORY_NOT_APPLIED_STUDENT_EXISTS);
+        }
+    }
+
+    public void validateUpdateRepository(String repositoryOwnerGithubId, Member currentMember) {
+        // 레포지토리 소유자가 현 멤버가 아닌 경우
+        String currentMemberGithubId = currentMember.getOauthId();
+
+        if (!Objects.equals(repositoryOwnerGithubId, currentMemberGithubId)) {
+            throw new CustomException(STUDY_HISTORY_REPOSITORY_NOT_UPDATABLE_OWNER_MISMATCH);
         }
     }
 }
