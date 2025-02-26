70ffe0eeb054784254900cf974c2a5d202244ee2
chosw1002@naver.com <chosw1002@naver.com>
2025-02-26 12:22:50 +0900

    feat: 스터디 공지 관련 api 구현

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/api/MentorStudyControllerV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/MentorStudyControllerV2.java
@@ -1,15 +1,19 @@
 package com.gdschongik.gdsc.domain.studyv2.api;
 
+import com.gdschongik.gdsc.domain.study.dto.request.StudyAnnouncementCreateUpdateRequest;
 import com.gdschongik.gdsc.domain.studyv2.application.MentorStudyServiceV2;
 import com.gdschongik.gdsc.domain.studyv2.dto.request.StudyUpdateRequest;
 import com.gdschongik.gdsc.domain.studyv2.dto.response.StudyManagerResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +38,29 @@ public class MentorStudyControllerV2 {
     @PutMapping("/{studyId}")
     public ResponseEntity<Void> updateStudy(@PathVariable Long studyId, @RequestBody StudyUpdateRequest request) {
         mentorStudyServiceV2.updateStudy(studyId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "스터디 공지 생성", description = "스터디의 공지사항을 생성합니다.")
+    @PostMapping("/{studyId}/announcements")
+    public ResponseEntity<Void> createStudyAnnouncement(
+            @PathVariable Long studyId, @Valid @RequestBody StudyAnnouncementCreateUpdateRequest request) {
+        mentorStudyServiceV2.createStudyAnnouncement(studyId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "스터디 공지 수정", description = "스터디의 공지사항을 수정합니다.")
+    @PutMapping("/announcements/{studyAnnouncementId}")
+    public ResponseEntity<Void> updateStudyAnnouncement(
+            @PathVariable Long studyAnnouncementId, @Valid @RequestBody StudyAnnouncementCreateUpdateRequest request) {
+        mentorStudyServiceV2.updateStudyAnnouncement(studyAnnouncementId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "스터디 공지 삭제", description = "스터디의 공지사항을 삭제합니다.")
+    @DeleteMapping("/announcements/{studyAnnouncementId}")
+    public ResponseEntity<Void> deleteStudyAnnouncement(@PathVariable Long studyAnnouncementId) {
+        mentorStudyServiceV2.deleteStudyAnnouncement(studyAnnouncementId);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/gdschongik/gdsc/domain/studyv2/application/MentorStudyServiceV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/application/MentorStudyServiceV2.java
@@ -3,7 +3,10 @@ package com.gdschongik.gdsc.domain.studyv2.application;
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.study.dto.request.StudyAnnouncementCreateUpdateRequest;
+import com.gdschongik.gdsc.domain.studyv2.dao.StudyAnnouncementV2Repository;
 import com.gdschongik.gdsc.domain.studyv2.dao.StudyV2Repository;
+import com.gdschongik.gdsc.domain.studyv2.domain.StudyAnnouncementV2;
 import com.gdschongik.gdsc.domain.studyv2.domain.StudyUpdateCommand;
 import com.gdschongik.gdsc.domain.studyv2.domain.StudyV2;
 import com.gdschongik.gdsc.domain.studyv2.domain.StudyValidatorV2;
@@ -48,5 +51,49 @@ public class MentorStudyServiceV2 {
         studyV2Repository.save(study);
 
         log.info("[MentoryStudyServiceV2] 스터디 정보 수정 완료: studyId={}", studyId);
+    }
+
+    @Transactional
+    public void createStudyAnnouncement(Long studyId, StudyAnnouncementCreateUpdateRequest request) {
+        Member currentMember = memberUtil.getCurrentMember();
+        StudyV2 study = studyV2Repository.findById(studyId).orElseThrow(() -> new CustomException(STUDY_NOT_FOUND));
+
+        studyValidatorV2.validateStudyMentor(currentMember, study);
+
+        StudyAnnouncementV2 studyAnnouncement = StudyAnnouncementV2.create(request.title(), request.link(), study);
+        studyAnnouncementV2Repository.save(studyAnnouncement);
+
+        log.info("[MentoryStudyServiceV2] 스터디 공지 생성: studyAnnouncementId={}", studyAnnouncement.getId());
+    }
+
+    @Transactional
+    public void updateStudyAnnouncement(Long studyAnnouncementId, StudyAnnouncementCreateUpdateRequest request) {
+        Member currentMember = memberUtil.getCurrentMember();
+        StudyAnnouncementV2 studyAnnouncement = studyAnnouncementV2Repository
+                .findById(studyAnnouncementId)
+                .orElseThrow(() -> new CustomException(STUDY_ANNOUNCEMENT_NOT_FOUND));
+        StudyV2 study = studyAnnouncement.getStudy();
+
+        studyValidatorV2.validateStudyMentor(currentMember, study);
+
+        studyAnnouncement.update(request.title(), request.link());
+        studyAnnouncementV2Repository.save(studyAnnouncement);
+
+        log.info("[MentoryStudyServiceV2] 스터디 공지 수정 완료: studyAnnouncementId={}", studyAnnouncement.getId());
+    }
+
+    @Transactional
+    public void deleteStudyAnnouncement(Long studyAnnouncementId) {
+        Member currentMember = memberUtil.getCurrentMember();
+        StudyAnnouncementV2 studyAnnouncement = studyAnnouncementV2Repository
+                .findById(studyAnnouncementId)
+                .orElseThrow(() -> new CustomException(STUDY_ANNOUNCEMENT_NOT_FOUND));
+        StudyV2 study = studyAnnouncement.getStudy();
+
+        studyValidatorV2.validateStudyMentor(currentMember, study);
+
+        studyAnnouncementV2Repository.delete(studyAnnouncement);
+
+        log.info("[MentoryStudyServiceV2] 스터디 공지 삭제 완료: studyAnnouncementId={}", studyAnnouncement.getId());
     }
 }
