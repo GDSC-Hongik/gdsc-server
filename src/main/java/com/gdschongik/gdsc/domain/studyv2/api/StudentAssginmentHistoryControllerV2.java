f339f85b67cb6518621c052c898be649c9469e73
Jaehyun Ahn <desktop3711@gmail.com>
2025-02-26 23:09:44 +0900

    feat: 과제 제출하기 V2 API 구현

---
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/StudentAssginmentHistoryControllerV2.java
@@ -0,0 +1,26 @@
+package com.gdschongik.gdsc.domain.studyv2.api;
+
+import com.gdschongik.gdsc.domain.studyv2.application.StudentAssignmentHistoryServiceV2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Student Assignment History V2", description = "학생 과제 제출이력 API입니다.")
+@RestController
+@RequestMapping("/v2/assignment-histories")
+@RequiredArgsConstructor
+public class StudentAssginmentHistoryControllerV2 {
+
+    private final StudentAssignmentHistoryServiceV2 studentAssigmentHistoryServiceV2;
+
+    @Operation(summary = "내 과제 제출하기", description = "나의 과제를 제출합니다. 제출된 과제는 채점되어 제출내역에 반영됩니다.")
+    @PostMapping("/submit")
+    public ResponseEntity<Void> submitMyAssignment(@RequestParam(name = "studySessionId") Long studySessionId) {
+        studentAssigmentHistoryServiceV2.submitAssignment(studySessionId);
+    }
+}
