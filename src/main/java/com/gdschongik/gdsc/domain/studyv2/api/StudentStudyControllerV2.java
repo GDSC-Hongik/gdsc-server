733bfd0ea09db3e4869b6adaef61c827c3d16b35
kckc0608 <kckc0608@gmail.com>
2025-02-27 11:30:56 +0900

    chore: spotless 적용

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/api/StudentStudyControllerV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/StudentStudyControllerV2.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
