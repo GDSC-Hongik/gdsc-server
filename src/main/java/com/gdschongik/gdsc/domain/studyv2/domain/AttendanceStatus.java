b28ab77fdd1d283c453880064d0441168d75605a
Jaehyun Ahn <desktop3711@gmail.com>
2025-02-28 21:49:30 +0900

    feat: 응답용 출석상태 추가

---
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/AttendanceStatus.java
@@ -0,0 +1,35 @@
+package com.gdschongik.gdsc.domain.studyv2.domain;
+
+import com.gdschongik.gdsc.domain.study.domain.StudyType;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AttendanceStatus {
+    NOT_LIVE("미실시"),
+    BEFORE_ATTENDANCE("출석 전"),
+    NOT_ATTENDED("미출석"),
+    ATTENDED("출석"),
+    ;
+
+    private final String value;
+
+    public static AttendanceStatus of(
+            StudySessionV2 studySession, StudyType type, boolean isAttended, LocalDateTime now) {
+        if (!type.isLive()) {
+            return NOT_LIVE;
+        }
+
+        if (studySession.getLessonPeriod().getStartDate().isAfter(now)) {
+            return BEFORE_ATTENDANCE;
+        }
+
+        if (isAttended) {
+            return ATTENDED;
+        } else {
+            return NOT_ATTENDED;
+        }
+    }
+}
