9da70f88860426bdc620e99e48be5956f47ed4d8
Jaehyun Ahn <desktop3711@gmail.com>
2025-03-03 16:51:58 +0900

    feat: 세션 DTO 매핑 로직 구현

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/dto/StudySessionStudentDto.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/dto/StudySessionStudentDto.java
@@ -1,6 +1,7 @@
 package com.gdschongik.gdsc.domain.studyv2.dto.dto;
 
 import com.gdschongik.gdsc.domain.common.vo.Period;
+import com.gdschongik.gdsc.domain.studyv2.domain.StudySessionV2;
 
 /**
  * 스터디 회차 학생 DTO입니다. 출결번호가 포함되어 있지 않습니다.
@@ -13,4 +14,16 @@ public record StudySessionStudentDto(
         Period lessonPeriod,
         String assignmentDescriptionLink,
         Period assignmentPeriod,
-        Long studyId) {}
+        Long studyId) {
+    public static StudySessionStudentDto of(StudySessionV2 studySession) {
+        return new StudySessionStudentDto(
+                studySession.getId(),
+                studySession.getPosition(),
+                studySession.getTitle(),
+                studySession.getDescription(),
+                studySession.getLessonPeriod(),
+                studySession.getAssignmentDescriptionLink(),
+                studySession.getAssignmentPeriod(),
+                studySession.getStudy().getId());
+    }
+}
