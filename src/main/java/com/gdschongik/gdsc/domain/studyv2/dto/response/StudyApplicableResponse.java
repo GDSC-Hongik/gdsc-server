8c649ca31de359046d02b1e17dc4905f69c1e0da
kwon <kckc0608@naver.com>
2025-03-05 22:57:15 +0900

    chore: spotless 적용

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/response/StudyApplicableResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/response/StudyApplicableResponse.java
@@ -3,18 +3,14 @@ package com.gdschongik.gdsc.domain.studyv2.dto.response;
 import com.gdschongik.gdsc.domain.studyv2.domain.StudyHistoryV2;
 import com.gdschongik.gdsc.domain.studyv2.domain.StudyV2;
 import com.gdschongik.gdsc.domain.studyv2.dto.dto.StudyStudentDto;
-
 import java.util.List;
 
-public record StudyApplicableResponse(
-        List<Long> appliedStudyIds,
-        List<StudyStudentDto> applicableStudies
-) {
+public record StudyApplicableResponse(List<Long> appliedStudyIds, List<StudyStudentDto> applicableStudies) {
     public static StudyApplicableResponse of(List<StudyHistoryV2> studyHistories, List<StudyV2> applicableStudies) {
         return new StudyApplicableResponse(
-                studyHistories.stream().map(studyHistory -> studyHistory.getStudy().getId()).toList(),
-                applicableStudies.stream().map(StudyStudentDto::from).toList()
-        );
+                studyHistories.stream()
+                        .map(studyHistory -> studyHistory.getStudy().getId())
+                        .toList(),
+                applicableStudies.stream().map(StudyStudentDto::from).toList());
     }
-
 }
