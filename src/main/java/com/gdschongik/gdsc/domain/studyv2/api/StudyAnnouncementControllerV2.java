25cf626db7678054e5da1acb37965123bb476406
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2025-03-10 21:20:53 +0900

    feat: 스터디 공지 목록 조회 V2 API 추가 (#966)
    * feat: 스터디 공지 목록 조회 api 추가
    
    * rename: 변수명 수정
    
    * rename: 변수명 수정
    
    * refactor: 응답 래핑 제거
    
    * rename: 서비스명 수정

---
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/StudyAnnouncementControllerV2.java
@@ -0,0 +1,29 @@
+package com.gdschongik.gdsc.domain.studyv2.api;
+
+import com.gdschongik.gdsc.domain.studyv2.application.StudyAnnouncementServiceV2;
+import com.gdschongik.gdsc.domain.studyv2.dto.dto.StudyAnnouncementDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Study Announcement V2", description = "스터디 공지 V2 API입니다.")
+@RestController
+@RequestMapping("/v2/study-announcements")
+@RequiredArgsConstructor
+public class StudyAnnouncementControllerV2 {
+
+    private final StudyAnnouncementServiceV2 studyAnnouncementServiceV2;
+
+    @Operation(summary = "스터디 공지 목록 조회", description = "스터디 공지 목록을 조회합니다.")
+    @GetMapping("/{studyId}")
+    public ResponseEntity<List<StudyAnnouncementDto>> getStudyAnnouncements(@PathVariable Long studyId) {
+        var response = studyAnnouncementServiceV2.getStudyAnnouncements(studyId);
+        return ResponseEntity.ok(response);
+    }
+}
