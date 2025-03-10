ec92a3e80e9d05b2635393e01b93fcf6f3f816b9
chosw1002@naver.com <chosw1002@naver.com>
2025-03-10 20:30:26 +0900

    rename: 변수명 수정

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/response/StudyAnnouncementResponse.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/dto/response/StudyAnnouncementResponse.java
@@ -4,7 +4,7 @@ import com.gdschongik.gdsc.domain.studyv2.domain.StudyAnnouncementV2;
 import com.gdschongik.gdsc.domain.studyv2.dto.dto.StudyAnnouncementDto;
 import java.util.List;
 
-public record StudyAnnouncementResponse(List<StudyAnnouncementDto> announcements) {
+public record StudyAnnouncementResponse(List<StudyAnnouncementDto> studyAnnouncements) {
     public static StudyAnnouncementResponse from(List<StudyAnnouncementV2> studyAnnouncements) {
         return new StudyAnnouncementResponse(
                 studyAnnouncements.stream().map(StudyAnnouncementDto::from).toList());
