28facdd30a5e90441ad2279ce8e2ea39bbb2d951
kckc0608 <kckc0608@naver.com>
2025-03-26 21:05:59 +0900

    chore: 불필요 코드 삭제

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyV2.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/domain/StudyV2.java
@@ -209,7 +209,6 @@ public class StudyV2 extends BaseEntity {
     }
 
     public LocalDateTime getOpeningDate() {
-        System.out.println();
         return studySessions.stream()
                 .filter(studySession -> studySession.getPosition() == 1)
                 .findFirst()
