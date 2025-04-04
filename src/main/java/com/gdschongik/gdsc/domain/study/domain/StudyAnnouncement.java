c27027ab661131a6ce36c9d4725da8b871c6a6a5
Jaehyun Ahn <desktop3711@gmail.com>
2025-04-05 00:14:09 +0900

    feat: 공지 V2 엔티티에서 생성 이벤트 발행하도록 변경

--- src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyAnnouncement.java
+++ src/main/java/com/gdschongik/gdsc/domain/study/domain/StudyAnnouncement.java
@@ -1,7 +1,6 @@
 package com.gdschongik.gdsc.domain.study.domain;
 
 import com.gdschongik.gdsc.domain.common.model.BaseEntity;
-import com.gdschongik.gdsc.domain.studyv2.domain.StudyAnnouncementCreatedEvent;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PostPersist;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,11 +38,6 @@ public class StudyAnnouncement extends BaseEntity {
         this.title = title;
         this.link = link;
         this.study = study;
-    }
-
-    @PostPersist
-    public void onPostPersist() {
-        registerEvent(new StudyAnnouncementCreatedEvent(id));
     }
 
     public static StudyAnnouncement create(String title, String link, Study study) {
