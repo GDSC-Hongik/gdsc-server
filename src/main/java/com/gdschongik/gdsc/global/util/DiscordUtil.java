7530dbffa174a587d56b6963d3a8817429d60f3a
Jaehyun Ahn <desktop3711@gmail.com>
2025-04-13 16:47:26 +0900

    fix: 임베드 시간 kst-utc 불일치 문제 수정

--- src/main/java/com/gdschongik/gdsc/global/util/DiscordUtil.java
+++ src/main/java/com/gdschongik/gdsc/global/util/DiscordUtil.java
@@ -6,7 +6,9 @@ import com.gdschongik.gdsc.global.exception.CustomException;
 import com.gdschongik.gdsc.global.property.DiscordProperty;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -20,8 +22,7 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 @RequiredArgsConstructor
 public class DiscordUtil {
 
-    public static final String IMAGE_GENERATOR_URL =
-            "https://image.wawoo.dev/api/v1/study-announcement";
+    public static final String IMAGE_GENERATOR_URL = "https://image.wawoo.dev/api/v1/study-announcement";
 
     private final JDA jda;
     private final DiscordProperty discordProperty;
@@ -97,11 +98,18 @@ public class DiscordUtil {
                 .appendDescription(studyName + " 공지가 업로드 되었어요.\n")
                 .appendDescription("공지는 [와우클래스](<https://study.wawoo.dev/landing>)에서도 확인 가능해요.\n")
                 .appendDescription(String.format("## [► 공지 확인하러 가기](<%s>)\n", link))
-                .setTimestamp(createdAt)
+                .setTimestamp(convertKstToUtc(createdAt))
                 .setImage(buildImageUrl(studyName, title, createdAt, theme))
                 .build();
 
         channel.sendMessageEmbeds(embed).queue();
+    }
+
+    private Instant convertKstToUtc(LocalDateTime kstCreatedAt) {
+        return kstCreatedAt
+                .atZone(ZoneId.of("Asia/Seoul"))
+                .withZoneSameInstant(ZoneId.of("UTC"))
+                .toInstant();
     }
 
     private String buildImageUrl(String studyName, String title, LocalDateTime dateTime, String theme) {
