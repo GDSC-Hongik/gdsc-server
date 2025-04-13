3c9c00beb9d2da081f843253e8d9b8d7ace1d934
kimsh1017 <81338287+kimsh1017@users.noreply.github.com>
2025-04-13 19:17:54 +0900

    refactor: 테스트용 게스트 회원 생성시 `GithubClient` 사용하도록 변경 (#1046)
    refactor: 테스트 멤버 생성시 GithubClient 사용하도록 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/application/TestMemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/TestMemberService.java
@@ -5,27 +5,23 @@ import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
 import com.gdschongik.gdsc.global.exception.CustomException;
-import java.util.Optional;
+import com.gdschongik.gdsc.infra.github.client.GithubClient;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestClient;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TestMemberService {
 
     private final MemberRepository memberRepository;
-    private final RestClient restClient;
-
-    public TestMemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-        this.restClient = RestClient.builder().baseUrl("https://api.github.com").build();
-    }
+    private final GithubClient githubClient;
 
     @Transactional
     public void createTestMember(String githubHandle) {
-        String githubOauthId = getGithubOauthId(githubHandle);
+        String githubOauthId = githubClient.getOauthId(githubHandle);
 
         if (memberRepository.findByOauthId(githubOauthId).isPresent()) {
             throw new CustomException(INTERNAL_ERROR);
@@ -34,16 +30,4 @@ public class TestMemberService {
         Member guestMember = Member.createGuest(githubOauthId);
         memberRepository.save(guestMember);
     }
-
-    private String getGithubOauthId(String githubHandle) {
-        return Optional.ofNullable(restClient
-                        .get()
-                        .uri("/users/{githubHandle}", githubHandle)
-                        .retrieve()
-                        .body(GithubUser.class))
-                .map(GithubUser::id)
-                .orElseThrow(() -> new CustomException(INTERNAL_ERROR));
-    }
-
-    private record GithubUser(String id) {}
 }
