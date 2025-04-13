b90d55abc3ebf56427030527f5b7c819d4da591a
Jaehyun Ahn <desktop3711@gmail.com>
2025-04-14 01:50:29 +0900

    feat: 웹훅 기반 스터디 공지 생성 기능 구현

--- src/main/java/com/gdschongik/gdsc/domain/studyv2/api/StudyWebhookController.java
+++ src/main/java/com/gdschongik/gdsc/domain/studyv2/api/StudyWebhookController.java
@@ -1,5 +1,6 @@
 package com.gdschongik.gdsc.domain.studyv2.api;
 
+import com.gdschongik.gdsc.domain.studyv2.application.MentorStudyAnnouncementServiceV2;
 import com.gdschongik.gdsc.domain.studyv2.dto.request.NotionWebhookRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StudyWebhookController {
 
+    private final MentorStudyAnnouncementServiceV2 mentorStudyAnnouncementServiceV2;
+
     @Operation(summary = "스터디 노션 웹훅 처리", description = "공지 및 과제 발행 요청 웹훅을 처리합니다.")
     @PostMapping
     public ResponseEntity<Void> handleStudyWebhook(@RequestBody NotionWebhookRequest request) {
-        System.out.println("request = " + request);
+        mentorStudyAnnouncementServiceV2.createStudyAnnouncementByWebhook(request);
         return ResponseEntity.ok().build();
     }
 }
