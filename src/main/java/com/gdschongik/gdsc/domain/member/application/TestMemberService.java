f3cbd2d25c8723736a224e84d1bd447bd6e35e12
kimsh1017 <kimsh8580@gmail.com>
2025-04-14 21:06:16 +0900

    refactor: TestMemberService로 테스트용 메서드 이동

--- src/main/java/com/gdschongik/gdsc/domain/member/application/TestMemberService.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/TestMemberService.java
@@ -2,9 +2,17 @@ package com.gdschongik.gdsc.domain.member.application;
 
 import static com.gdschongik.gdsc.global.exception.ErrorCode.*;
 
+import com.gdschongik.gdsc.domain.auth.application.JwtService;
+import com.gdschongik.gdsc.domain.auth.dto.AccessTokenDto;
+import com.gdschongik.gdsc.domain.auth.dto.RefreshTokenDto;
 import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
 import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.member.dto.request.MemberTokenByGithubHandleRequest;
+import com.gdschongik.gdsc.domain.member.dto.response.MemberTokenResponse;
+import com.gdschongik.gdsc.domain.membership.application.MembershipService;
 import com.gdschongik.gdsc.global.exception.CustomException;
+import com.gdschongik.gdsc.global.security.MemberAuthInfo;
+import com.gdschongik.gdsc.global.util.MemberUtil;
 import com.gdschongik.gdsc.infra.github.client.GithubClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class TestMemberService {
 
+    private final MembershipService membershipService;
+    private final JwtService jwtService;
     private final MemberRepository memberRepository;
     private final GithubClient githubClient;
+    private final MemberUtil memberUtil;
 
     @Transactional
     public void createTestMember(String githubHandle) {
@@ -29,5 +40,27 @@ public class TestMemberService {
 
         Member guestMember = Member.createGuest(githubOauthId);
         memberRepository.save(guestMember);
+    }
+
+    public MemberTokenResponse createTemporaryTokenByGithubHandle(MemberTokenByGithubHandleRequest request) {
+        final String oauthId = githubClient.getOauthId(request.githubHandle());
+
+        final Member member =
+                memberRepository.findByOauthId(oauthId).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+
+        AccessTokenDto accessTokenDto = jwtService.createAccessToken(MemberAuthInfo.from(member));
+        RefreshTokenDto refreshTokenDto = jwtService.createRefreshToken(member.getId());
+
+        return new MemberTokenResponse(accessTokenDto.tokenValue(), refreshTokenDto.tokenValue());
+    }
+
+    @Transactional
+    public void demoteToGuestAndRegularRequirementToUnsatisfied() {
+        Member member = memberUtil.getCurrentMember();
+        member.demoteToGuest();
+
+        membershipService.deleteMembership(member);
+
+        log.info("[AdminMemberService] 게스트로 강등: demotedMemberId={}", member.getId());
     }
 }
