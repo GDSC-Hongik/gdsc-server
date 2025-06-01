37ec658268c7a9a6b996699025563feb29169b13
Jaehyun Ahn <desktop3711@gmail.com>
2025-06-01 20:04:19 +0900

    feat: 핸들러가 서비스로 위임하도록 변경

--- src/main/java/com/gdschongik/gdsc/domain/member/application/handler/MemberEventHandler.java
+++ src/main/java/com/gdschongik/gdsc/domain/member/application/handler/MemberEventHandler.java
@@ -1,28 +1,23 @@
 package com.gdschongik.gdsc.domain.member.application.handler;
 
-import com.gdschongik.gdsc.domain.member.dao.MemberRepository;
-import com.gdschongik.gdsc.domain.member.domain.Member;
+import com.gdschongik.gdsc.domain.member.application.OnboardingMemberService;
 import com.gdschongik.gdsc.domain.member.domain.event.MemberAssociateRequirementUpdatedEvent;
-import com.gdschongik.gdsc.global.exception.CustomException;
-import com.gdschongik.gdsc.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class MemberEventHandler {
-    private final MemberRepository memberRepository;
 
-    public void advanceToAssociate(MemberAssociateRequirementUpdatedEvent memberAssociateRequirementUpdatedEvent) {
-        Member member = memberRepository
-                .findById(memberAssociateRequirementUpdatedEvent.memberId())
-                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-        try {
-            member.advanceToAssociate();
-        } catch (CustomException e) {
-            log.info("{}", e.getErrorCode());
-        }
+    private final OnboardingMemberService onboardingMemberService;
+
+    @ApplicationModuleListener
+    public void handleMemberAssociateRequirementUpdatedEvent(MemberAssociateRequirementUpdatedEvent event) {
+        log.info("[MemberEventHandler] 멤버 준회원 승급조건 업데이트 이벤트 수신: memberId={}", event.memberId());
+
+        onboardingMemberService.attemptAdvanceToAssociate(event.memberId());
     }
 }
