e0ed8a037955a0cdd13d9d110162eaa6305d65d9
Jaehyun Ahn <desktop3711@gmail.com>
2025-06-17 23:10:51 +0900

    feat: 주문 이벤트 핸들러에 `@AML` 적용

--- src/main/java/com/gdschongik/gdsc/domain/order/application/OrderEventHandler.java
+++ src/main/java/com/gdschongik/gdsc/domain/order/application/OrderEventHandler.java
@@ -6,9 +6,8 @@ import com.gdschongik.gdsc.domain.order.domain.event.OrderCompletedEvent;
 import com.gdschongik.gdsc.domain.order.domain.event.OrderCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -17,7 +16,7 @@ public class OrderEventHandler {
 
     private final MembershipService membershipService;
 
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    @ApplicationModuleListener
     public void handleOrderCreatedEvent(OrderCreatedEvent orderCreatedEvent) {
         log.info(
                 "[OrderEventHandler] 주문 생성 이벤트 수신: nanoId={}, isFree={}",
@@ -28,13 +27,13 @@ public class OrderEventHandler {
         }
     }
 
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    @ApplicationModuleListener
     public void handleOrderCompletedEvent(OrderCompletedEvent orderCompletedEvent) {
         log.info("[OrderEventHandler] 주문 완료 이벤트 수신: nanoId={}", orderCompletedEvent.nanoId());
         membershipService.verifyPaymentStatus(orderCompletedEvent.nanoId());
     }
 
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    @ApplicationModuleListener
     public void handleOrderCanceledEvent(OrderCanceledEvent orderCanceledEvent) {
         log.info("[OrderEventHandler] 주문 취소 이벤트 수신: orderId={}", orderCanceledEvent.orderId());
         membershipService.revokePaymentStatus(orderCanceledEvent.orderId());
